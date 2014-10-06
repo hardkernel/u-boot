53d72dbd217fce4a7999981ccf511c421e9637e1
Dongjin Kim <tobetter@gmail.com>
2014-10-06 16:14:30 +0900

    BACKPORT: FASTBOOT: Add fastboot to improve partition support
    These files are ported from git://git.denx.de/u-boot.git,
    HEAD commit was [7e9d844 ARMV7: tungsten: Cleanup build_server.sh].
    
    Change-Id: I0c7a3333b39a9365d50a909dd1d56fb711a48f7c
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

---
+++ board/hardkernel/odroidc/fastboot.c
@@ -0,0 +1,303 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+#include <common.h>
+#include <malloc.h>
+#include <fastboot.h>
+#include <asm/sizes.h>
+#include <asm/io.h>
+
+#if !defined(CONFIG_FASTBOOT_NO_FORMAT)
+static struct ptable the_ptable;
+
+/* in a board specific file */
+struct fbt_partition {
+	const char *name;
+	const char *type;
+	unsigned size_kb;
+};
+extern struct fbt_partition fbt_partitions[];
+
+#include <part.h>
+
+/* a bunch of this is duplicate of part_efi.c and part_efi.h
+ * we should consider putting this code in there and layer
+ * the partition info.
+ */
+#define EFI_VERSION 0x00010000
+#define EFI_ENTRIES 128
+#define EFI_NAMELEN 36
+
+static const u8 partition_type[16] = {
+	0xa2, 0xa0, 0xd0, 0xeb, 0xe5, 0xb9, 0x33, 0x44,
+	0x87, 0xc0, 0x68, 0xb6, 0xb7, 0x26, 0x99, 0xc7,
+};
+
+static const u8 random_uuid[16] = {
+	0xff, 0x1f, 0xf2, 0xf9, 0xd4, 0xa8, 0x0e, 0x5f,
+	0x97, 0x46, 0x59, 0x48, 0x69, 0xae, 0xc3, 0x4e,
+};
+
+struct efi_entry {
+	u8 type_uuid[16];
+	u8 uniq_uuid[16];
+	u64 first_lba;
+	u64 last_lba;
+	u64 attr;
+	u16 name[EFI_NAMELEN];
+};
+
+struct efi_header {
+	u8 magic[8];
+
+	u32 version;
+	u32 header_sz;
+
+	u32 crc32;
+	u32 reserved;
+
+	u64 header_lba;
+	u64 backup_lba;
+	u64 first_lba;
+	u64 last_lba;
+
+	u8 volume_uuid[16];
+
+	u64 entries_lba;
+
+	u32 entries_count;
+	u32 entries_size;
+	u32 entries_crc32;
+} __attribute__((packed));
+
+struct ptable {
+	u8 mbr[512];
+	union {
+		struct efi_header header;
+		u8 block[512];
+	};
+	struct efi_entry entry[EFI_ENTRIES];
+};
+
+static void init_mbr(u8 *mbr, u32 blocks)
+{
+	mbr[0x1be] = 0x00; // nonbootable
+	mbr[0x1bf] = 0xFF; // bogus CHS
+	mbr[0x1c0] = 0xFF;
+	mbr[0x1c1] = 0xFF;
+
+	mbr[0x1c2] = 0xEE; // GPT partition
+	mbr[0x1c3] = 0xFF; // bogus CHS
+	mbr[0x1c4] = 0xFF;
+	mbr[0x1c5] = 0xFF;
+
+	mbr[0x1c6] = 0x01; // start
+	mbr[0x1c7] = 0x00;
+	mbr[0x1c8] = 0x00;
+	mbr[0x1c9] = 0x00;
+
+	memcpy(mbr + 0x1ca, &blocks, sizeof(u32));
+
+	mbr[0x1fe] = 0x55;
+	mbr[0x1ff] = 0xaa;
+}
+
+static void start_ptbl(struct ptable *ptbl, unsigned blocks)
+{
+	struct efi_header *hdr = &ptbl->header;
+
+	memset(ptbl, 0, sizeof(*ptbl));
+
+	init_mbr(ptbl->mbr, blocks - 1);
+
+	memcpy(hdr->magic, "EFI PART", 8);
+	hdr->version = EFI_VERSION;
+	hdr->header_sz = sizeof(struct efi_header);
+	hdr->header_lba = 1;
+	hdr->backup_lba = blocks - 1;
+	hdr->first_lba = 34;
+	hdr->last_lba = blocks - 1;
+	memcpy(hdr->volume_uuid, random_uuid, 16);
+	hdr->entries_lba = 2;
+	hdr->entries_count = EFI_ENTRIES;
+	hdr->entries_size = sizeof(struct efi_entry);
+}
+
+static void end_ptbl(struct ptable *ptbl)
+{
+	struct efi_header *hdr = &ptbl->header;
+	u32 n;
+
+	n = crc32(0, 0, 0);
+	n = crc32(n, (void*) ptbl->entry, sizeof(ptbl->entry));
+	hdr->entries_crc32 = n;
+
+	n = crc32(0, 0, 0);
+	n = crc32(0, (void*) &ptbl->header, sizeof(ptbl->header));
+	hdr->crc32 = n;
+}
+
+static int add_ptn(struct ptable *ptbl, u64 first, u64 last, const char *name)
+{
+	struct efi_header *hdr = &ptbl->header;
+	struct efi_entry *entry = ptbl->entry;
+	unsigned n;
+
+	if (first < 34) {
+		printf("partition '%s' overlaps partition table\n", name);
+		return -1;
+	}
+
+	if (last > hdr->last_lba) {
+		printf("partition '%s' does not fit\n", name);
+		return -1;
+	}
+	for (n = 0; n < EFI_ENTRIES; n++, entry++) {
+		if (entry->last_lba)
+			continue;
+		memcpy(entry->type_uuid, partition_type, 16);
+		memcpy(entry->uniq_uuid, random_uuid, 16);
+		entry->uniq_uuid[0] = n;
+		entry->first_lba = first;
+		entry->last_lba = last;
+		for (n = 0; (n < EFI_NAMELEN) && *name; n++)
+			entry->name[n] = *name++;
+		return 0;
+	}
+	printf("out of partition table entries\n");
+	return -1;
+}
+
+static int do_format(void)
+{
+	struct ptable *ptbl = &the_ptable;
+	unsigned next;
+	int n;
+	block_dev_desc_t *dev_desc;
+	unsigned long blocks_to_write, result;
+
+	dev_desc = get_dev_by_name(FASTBOOT_BLKDEV);
+	if (!dev_desc) {
+		printf("error getting device %s\n", FASTBOOT_BLKDEV);
+		return -1;
+	}
+	if (!dev_desc->lba) {
+		printf("device %s has no space\n", FASTBOOT_BLKDEV);
+		return -1;
+	}
+
+	printf("blocks %lu\n", dev_desc->lba);
+
+	start_ptbl(ptbl, dev_desc->lba);
+	for (n = 0, next = 0; fbt_partitions[n].name; n++) {
+		u64 sz = fbt_partitions[n].size_kb * 2;
+		if (fbt_partitions[n].name[0] == '-') {
+			next += sz;
+			continue;
+		}
+		if (sz == 0)
+			sz = dev_desc->lba - next;
+		if (add_ptn(ptbl, next, next + sz - 1, fbt_partitions[n].name))
+			return -1;
+		next += sz;
+	}
+	end_ptbl(ptbl);
+
+	blocks_to_write = DIV_ROUND_UP(sizeof(struct ptable), dev_desc->blksz);
+	result = dev_desc->block_write(dev_desc->dev, 0, blocks_to_write, ptbl);
+	if (result != blocks_to_write) {
+		printf("\nFormat failed, block_write() returned %lu instead of %lu\n", result, blocks_to_write);
+		return -1;
+	}
+
+	printf("\nnew partition table of %lu %lu-byte blocks\n",
+		blocks_to_write, dev_desc->blksz);
+	fbt_reset_ptn();
+
+	return 0;
+}
+
+int board_fbt_oem(const char *cmdbuf)
+{
+	if (!strcmp(cmdbuf,"format"))
+		return do_format();
+	return -1;
+}
+#endif /* !CONFIG_FASTBOOT_NO_FORMAT */
+
+void board_fbt_set_reboot_type(enum fbt_reboot_type frt)
+{
+	switch(frt) {
+	case FASTBOOT_REBOOT_NORMAL:
+	  strcpy((char*)FASTBOOT_REBOOT_PARAMETER_ADDR, "normal");
+	  break;
+	case FASTBOOT_REBOOT_BOOTLOADER:
+	  strcpy((char*)FASTBOOT_REBOOT_PARAMETER_ADDR, "bootloader");
+	  break;
+	case FASTBOOT_REBOOT_RECOVERY:
+	  strcpy((char*)FASTBOOT_REBOOT_PARAMETER_ADDR, "recovery");
+	  break;
+	case FASTBOOT_REBOOT_RECOVERY_WIPE_DATA:
+	  strcpy((char*)FASTBOOT_REBOOT_PARAMETER_ADDR, "recovery:wipe_data");
+	  break;
+	default:
+	  writel(0, (void*)FASTBOOT_REBOOT_PARAMETER_ADDR);
+	  printf("unknown reboot type %d\n", frt);
+	  break;
+	}
+}
+
+enum fbt_reboot_type board_fbt_get_reboot_type(void)
+{
+	enum fbt_reboot_type frt = FASTBOOT_REBOOT_UNKNOWN;
+	const char *sar_free_p = (const char*)FASTBOOT_REBOOT_PARAMETER_ADDR;
+	if (!strcmp(sar_free_p, "recovery")) {
+		frt = FASTBOOT_REBOOT_RECOVERY;
+	} else if (!strcmp(sar_free_p, "recovery:wipe_data")) {
+		frt = FASTBOOT_REBOOT_RECOVERY_WIPE_DATA;
+	} else if (!strcmp(sar_free_p, "bootloader")) {
+		frt = FASTBOOT_REBOOT_BOOTLOADER;
+	} else if (!strcmp(sar_free_p, "normal")) {
+		frt = FASTBOOT_REBOOT_NORMAL;
+	}
+
+	/* clear before next boot */
+	writel(0, (void*)FASTBOOT_REBOOT_PARAMETER_ADDR);
+	return frt;
+}
+
+const char *board_fbt_get_partition_type(const char *partition_name)
+{
+	int i;
+	for (i = 0; fbt_partitions[i].name; i++) {
+		if (!strcmp(partition_name, fbt_partitions[i].name)) {
+			return fbt_partitions[i].type;
+		}
+	}
+	return NULL;
+}
+
---
+++ include/fastboot.h
@@ -0,0 +1,284 @@
+/*
+ * (C) Copyright 2008 - 2009
+ * Windriver, <www.windriver.com>
+ * Tom Rix <Tom.Rix@windriver.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ *
+ * The logical naming of flash comes from the Android project
+ * Thse structures and functions that look like fastboot_flash_*
+ * They come from bootloader/legacy/include/boot/flash.h
+ *
+ * The boot_img_hdr structure and associated magic numbers also
+ * come from the Android project.  They are from
+ * system/core/mkbootimg/bootimg.h
+ *
+ * Here are their copyrights
+ *
+ * Copyright (C) 2008 The Android Open Source Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#ifndef FASTBOOT_H
+#define FASTBOOT_H
+
+#include <common.h>
+#include <command.h>
+
+/* This is the interface file between the common cmd_fastboot.c and
+   the board specific support.
+
+   To use this interface, define CONFIG_FASTBOOT in your board config file.
+   An example is include/configs/omap3430labrador.h
+   ...
+   #define CONFIG_FASTBOOT	        1    / * Using fastboot interface * /
+   ...
+
+   An example of the board specific spupport for omap3 is found at
+   cpu/omap3/fastboot.c
+
+*/
+
+/* From fastboot client.. */
+#define FASTBOOT_INTERFACE_CLASS     0xff
+#define FASTBOOT_INTERFACE_SUB_CLASS 0x42
+#define FASTBOOT_INTERFACE_PROTOCOL  0x03
+
+#define FASTBOOT_SERIALNO_BOOTARG "androidboot.serialno"
+
+/* The fastboot client uses a value of 2048 for the
+   page size of it boot.img file format.
+   Reset this in your board config file as needed. */
+#ifndef CONFIG_FASTBOOT_MKBOOTIMAGE_PAGE_SIZE
+#define CONFIG_FASTBOOT_MKBOOTIMAGE_PAGE_SIZE 2048
+#endif
+
+/* Fastboot bulk packet sizes */
+#define	CONFIG_USBD_FASTBOOT_BULK_PKTSIZE_HS	512
+#define	CONFIG_USBD_FASTBOOT_BULK_PKTSIZE_FS	64
+
+/* Max size of commands from host to us */
+#define FASTBOOT_COMMAND_SIZE 64
+
+/* Max size of responses from us to host */
+#define FASTBOOT_RESPONSE_SIZE 64
+
+/* Flags */
+#define FASTBOOT_FLAG_RESPONSE  0x1
+#define FASTBOOT_FLAG_HAS_RUN   0x2
+
+#define FASTBOOT_MAX_INFO_NAMELEN    32
+#define FASTBOOT_MAX_NUM_DEVICE_INFO 32
+
+struct device_info {
+	char *name;
+	char *value;
+};
+
+struct info_partition_header {
+	u32 magic;
+	u32 num_values;
+};
+
+struct cmd_fastboot_interface {
+
+	/* A getvar string for the serial number
+	   It can have a maximum of 60 characters
+
+	   Set by board */
+	char *serial_no;
+
+	block_dev_desc_t *dev_desc;
+
+	/* Transfer buffer, for handling flash updates
+	   Should be multiple of the block size
+	   Care should be take so it does not overrun bootloader memory
+	   Controlled by the configure variable CONFIG_FASTBOOT_TRANSFER_BUFFER
+
+	   Set by board */
+	u8 *transfer_buffer;
+
+	/* Pointer to the part of the transfer_buffer to use for
+	   flashing.  Typically it's equal to transfer_buffer, but for
+	   special cases, it might be some offset into the buffer (e.g.
+	   for a multi-image binary).
+	*/
+	u8 *image_start_ptr;
+
+	/* How big is the transfer buffer
+	   Controlled by the configure variable
+	   CONFIG_FASTBOOT_TRANSFER_BUFFER_SIZE
+
+	   Set by board	*/
+	u64 transfer_buffer_size;
+
+	/* Download size, if download has to be done. This can be checked to find
+		whether next packet is a command or a data */
+	u64 d_size;
+
+	/* Data downloaded so far */
+	u64 d_bytes;
+
+	/* Upload size, if download has to be done */
+	u64 u_size;
+
+	/* Data uploaded so far */
+	u64 u_bytes;
+
+	/* response with a NULL following to stop strlen() */
+	char response[FASTBOOT_RESPONSE_SIZE];
+	char null_term;
+
+	/* Indicate response to be sent, data to be recieved */
+	unsigned int flag;
+
+	/* configuration status  */
+	unsigned int configured;
+
+	/* processing a command */
+	unsigned int executing_command;
+
+	unsigned int exit;
+
+	unsigned int unlocked;
+	unsigned long unlock_pending_start_time;
+
+	/* device specific info */
+	unsigned int dev_info_uninitialized;
+	unsigned int num_device_info;
+	struct device_info dev_info[FASTBOOT_MAX_NUM_DEVICE_INFO];
+};
+
+/* Status values */
+#define FASTBOOT_OK			0
+#define FASTBOOT_ERROR			-1
+#define FASTBOOT_DISCONNECT		1
+#define FASTBOOT_INACTIVE		2
+
+/* Android bootimage file format */
+#define FASTBOOT_BOOT_MAGIC "ANDROID!"
+#define FASTBOOT_BOOT_MAGIC_SIZE 8
+#define FASTBOOT_BOOT_NAME_SIZE 16
+#define FASTBOOT_BOOT_ARGS_SIZE 512
+
+struct fastboot_boot_img_hdr {
+	unsigned char magic[FASTBOOT_BOOT_MAGIC_SIZE];
+
+	unsigned kernel_size;  /* size in bytes */
+	unsigned kernel_addr;  /* physical load addr */
+
+	unsigned ramdisk_size; /* size in bytes */
+	unsigned ramdisk_addr; /* physical load addr */
+
+	unsigned second_size;  /* size in bytes */
+	unsigned second_addr;  /* physical load addr */
+
+	unsigned tags_addr;    /* physical addr for kernel tags */
+	unsigned page_size;    /* flash page size we assume */
+	unsigned unused[2];    /* future expansion: should be 0 */
+
+	unsigned char name[FASTBOOT_BOOT_NAME_SIZE]; /* asciiz product name */
+
+	unsigned char cmdline[FASTBOOT_BOOT_ARGS_SIZE];
+
+	unsigned id[8]; /* timestamp / checksum / sha1 / etc */
+};
+
+typedef struct sparse_header {
+  __le32	magic;		/* 0xed26ff3a */
+  __le16	major_version;	/* (0x1) - reject images with higher major versions */
+  __le16	minor_version;	/* (0x0) - allow images with higer minor versions */
+  __le16	file_hdr_sz;	/* 28 bytes for first revision of the file format */
+  __le16	chunk_hdr_sz;	/* 12 bytes for first revision of the file format */
+  __le32	blk_sz;		/* block size in bytes, must be a multiple of 4 (4096) */
+  __le32	total_blks;	/* total blocks in the non-sparse output image */
+  __le32	total_chunks;	/* total chunks in the sparse input image */
+  __le32	image_checksum; /* CRC32 checksum of the original data, counting "don't care" */
+				/* as 0. Standard 802.3 polynomial, use a Public Domain */
+				/* table implementation */
+} sparse_header_t;
+
+#define SPARSE_HEADER_MAGIC	0xed26ff3a
+
+#define CHUNK_TYPE_RAW		0xCAC1
+#define CHUNK_TYPE_FILL		0xCAC2
+#define CHUNK_TYPE_DONT_CARE	0xCAC3
+
+typedef struct chunk_header {
+  __le16	chunk_type;	/* 0xCAC1 -> raw; 0xCAC2 -> fill; 0xCAC3 -> don't care */
+  __le16	reserved1;
+  __le32	chunk_sz;	/* in blocks in output image */
+  __le32	total_sz;	/* in bytes of chunk input file including chunk header and data */
+} chunk_header_t;
+
+/* Following a Raw or Fill chunk is data.  For a Raw chunk, it's the data in chunk_sz * blk_sz.
+ *  For a Fill chunk, it's 4 bytes of the fill data.
+ */
+
+#ifdef	CONFIG_CMD_FASTBOOT
+enum fbt_reboot_type {
+	FASTBOOT_REBOOT_UNKNOWN, /* typically for a cold boot */
+	FASTBOOT_REBOOT_NORMAL,
+	FASTBOOT_REBOOT_BOOTLOADER,
+	FASTBOOT_REBOOT_RECOVERY,
+	FASTBOOT_REBOOT_NONE,
+	FASTBOOT_REBOOT_RECOVERY_WIPE_DATA,
+};
+extern void fbt_preboot(void);
+extern void fbt_reset_ptn(void);
+extern void fbt_add_ptn(disk_partition_t *ptn);
+extern int fbt_send_info(const char *info);
+
+int board_fbt_oem(const char *cmdbuf);
+void board_fbt_set_reboot_type(enum fbt_reboot_type frt);
+/* gets the reboot type, automatically clearing it for next boot */
+enum fbt_reboot_type board_fbt_get_reboot_type(void);
+int board_fbt_key_pressed(void);
+enum fbt_reboot_type board_fbt_key_command(void);
+int board_fbt_load_ptbl(void);
+void board_fbt_start(void);
+void board_fbt_end(void);
+void board_fbt_finalize_bootargs(char* args, size_t buf_sz);
+int board_fbt_handle_flash(disk_partition_t *ptn,
+			   struct cmd_fastboot_interface *priv);
+const char *board_fbt_get_partition_type(const char *partition_name);
+
+#endif /* CONFIG_FASTBOOT */
+#endif /* FASTBOOT_H */
