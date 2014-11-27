fe7afaab784e28e1e78083897ccf04683ad43e6f
Dongjin Kim <tobetter@gmail.com>
2014-11-27 15:08:14 +0900

    ODROID:cfgload: Modify to support legacy .ini format
    Change-Id: I3b6772d8346e344786f9fc4ae8ae15603aa13815
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- board/hardkernel/odroidc/cfgload.c
+++ board/hardkernel/odroidc/cfgload.c
@@ -23,27 +23,112 @@
 #include <common.h>
 #include <command.h>
 
-int do_fat_cfgload(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
+#ifdef CONFIG_SYS_HUSH_PARSER
+#include <hush.h>
+#endif
+
+static const char* magicstr = "ODROIDC-UBOOT-CONFIG";
+static char *load_cmd =
+    "if fatload mmc 0:1 $loadaddr boot.ini;"
+    " then echo Loading boot.ini from mmc0:1 (vfat);"
+    " else if ext4load mmc 0:1 $loadaddr /boot/boot.ini;"
+    " then echo Loading boot.ini from mmc0:1 (ext4);"
+    " else if ext4load mmc 0:2 $loadaddr /boot/boot.ini;"
+    " then echo Loading boot.init from mmc0:2 (ext4);"
+    " fi;fi;fi";
+
+int do_fat_cfgload(cmd_tbl_t * cmdtp, int flag, int argc, char *argv[])
 {
-	int ret;
+    unsigned char *fp;
+    unsigned long filesize = 0;
+    unsigned char cmd[512], skip = 0, first = 1;
+    unsigned int wpos = 0;
 
-	ret = run_command("fatload mmc 0:1 $loadaddr boot.ini", 0);
-	if (ret)
-		goto load_err;
+    fp = (char *)simple_strtoul(getenv("loadaddr"), NULL, 16);
+    if (0 == fp)
+        fp = CONFIG_SYS_LOAD_ADDR;
 
-	printf("boot.ini is loaded from FAT to 0x%s\n", getenv("loadaddr"));
+    setenv("filesize", "0");
 
-	ret = run_command("ini boot $loadaddr $filesize", 0);
+#ifndef CONFIG_SYS_HUSH_PARSER
+    run_command(load_cmd, 0);
+#else
+    parse_string_outer(load_cmd, FLAG_PARSE_SEMICOLON
+            | FLAG_EXIT_FROM_LOOP);
+#endif
 
-load_err:
-	return ret;
+    filesize = getenv_ulong("filesize", 16, 0);
+    if (0 == filesize) {
+        printf("Failed to load boot.init, maybe empty file?\n");
+        return 0;
+    }
+
+    if (filesize > 64 * 1024) {
+        printf("boot.ini exceeds 64kB, size=%d\n", filesize);
+        return 0;
+    }
+
+    printf("Executing the script...\n");
+
+    while (1) {
+        if (*fp == '#') {
+            skip = 1;
+        }
+        else {
+            skip = 0;
+            wpos = 0;
+            memset(cmd, 0x00, sizeof(cmd));
+        }
+
+        while (*fp != 0x0a) {
+            if ((*fp != 0x0d) && (!skip))
+                cmd[wpos++] = *fp;
+
+            fp++;
+            if (filesize)
+                filesize--;
+            else
+                break;
+        }
+
+        if (wpos) {
+            if (wpos < sizeof(cmd)) {
+                if (first) {
+                    if (!strncmp(cmd, magicstr, strlen(magicstr))) {
+                        first = 0;
+                    }
+                    else {
+                        printf("It's not boot.ini for ODROID\n");
+                        return 0;
+                    }
+                }
+                else {
+                    printf("%s\n", cmd);
+                    run_command(cmd, 0);
+                }
+            }
+            wpos = 0;
+        }
+
+        fp++;
+        if (filesize)
+            filesize--;
+        else
+            break;
+    }
+
+    return 1;
 }
 
-U_BOOT_CMD(
-	cfgload, 1, 0, do_fat_cfgload,
-	"cfgload - Load boot.ini from FAT/EXT4 partitioin in a memory card\n",
-	"    Loads boot.ini in FAT/EXT4 partition of one of below\n"
-	"        1. /boot.ini in the 1st partition (FAT)\n"
-	"        2. /boot/boot.ini in the 1st partiton (EXT4)\n"
-	"        3. /boot/boot.ini in the 2nd partiton (EXT4)\n"
-);
+U_BOOT_CMD( cfgload, 1, 0, do_fat_cfgload,
+        "cfgload - boot.ini textfile load from FAT32/ext4\n",
+        "<interface(only support mmc 0:1/0:2)>\n"
+        "   - boot.ini file load from FAT32/ext4 on 'interface'\n"
+        "Will attempt boot from: \n"
+        "   fat mmc 0:1 /boot.ini\n"
+        "   ext4 0:1 /boot/boot.ini\n"
+        "   ext4 0:2 /boot/boot.ini\n"
+        "on the above sequence\n"
+        );
+
+/* vim: set ts=4 sw=4 tw=80: */
