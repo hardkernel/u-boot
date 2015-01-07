e7d4447d551ccba5d60be8b11697aa0ab49086c4
Dongjin Kim <tobetter@gmail.com>
2015-01-08 00:14:03 +0900

    ODROIDC:armv7: temporarily set -mno-unaligned-access
    Recent compiler generates unaligned memory access in armv7 default.
    But current U-Boot does not allow unaligned memory access, so it causes
    data abort exception. This patch add compile option "-mno-unaligned-access"
    if it is available.
    
    Reference commit:
      [1] http://lists.denx.de/pipermail/u-boot/2012-July/127260.html
      [1] http://lists.denx.de/pipermail/u-boot/2012-October/136558.html
    
    Change-Id: Id774f4a783ea457feeccefb6dea00662198f153c
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- arch/arm/cpu/aml_meson/config.mk
+++ arch/arm/cpu/aml_meson/config.mk
@@ -1,6 +1,8 @@
 CROSS_COMPILE=arm-none-eabi-
 ARM_CPU=cortex-a9
-PLATFORM_CPPFLAGS += $(call cc-option,-mcpu=cortex-a9  -ffixed-r8 -mno-long-calls  -Wall -fPIC )
+PLATFORM_CPPFLAGS += $(call cc-option,-mcpu=cortex-a9 -ffixed-r8 -mno-long-calls  -Wall -fPIC )
+PF_CPPFLAGS_NO_UNALIGNED := $(call cc-option, -mno-unaligned-access,)
+PLATFORM_CPPFLAGS += $(PF_CPPFLAGS_NO_UNALIGNED)
 #USE_PRIVATE_LIBGCC=yes
 TEXT_BASE=$(CONFIG_SYS_TEXT_BASE)
 UCL_TEXT_BASE=$(CONFIG_SYS_UCL_TEXT_BASE)
