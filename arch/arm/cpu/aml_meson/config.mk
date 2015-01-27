9cb907f3e04a114e4ac4649dd3e3dd0ffdc78629
Alexis Ballier <aballier@gentoo.org>
2015-01-27 13:00:02 +0100

    Do not force CROSS_COMPILE.
    My toolchain is called: armv7a-hardfloat-linux-gnueabi-gcc & friends.

--- arch/arm/cpu/aml_meson/config.mk
+++ arch/arm/cpu/aml_meson/config.mk
@@ -1,4 +1,4 @@
-CROSS_COMPILE=arm-none-eabi-
+CROSS_COMPILE?=arm-none-eabi-
 ARM_CPU=cortex-a9
 PLATFORM_CPPFLAGS += $(call cc-option,-mcpu=cortex-a9 -ffixed-r8 -mno-long-calls  -Wall -fPIC )
 PF_CPPFLAGS_NO_UNALIGNED := $(call cc-option, -mno-unaligned-access,)
