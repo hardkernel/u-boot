cbcd9a588d4685d17289234447fdadcb09c599d2
Xiaobo Gu <xiaobo.gu@amlogic.com>
2015-05-25 23:22:01 +0800

    Add gxbaby new config p200(dev) p201(ref).
    1 ./mk gxb_p200_v1 //build gxbaby dev board
    2 ./mk gxb_p201_v1 //build gxbaby ref board
    
    Change-Id: I2c67ad7f38a616a7610fac17e930a7e1857748b6

---
+++ include/configs/gxb_p200_v1.h
@@ -0,0 +1,146 @@
+
+/*
+ * include/configs/gxb_p200_v1.h
+ *
+ * Copyright (C) 2015 Amlogic, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef __GXB_P200_V1_H__
+#define __GXB_P200_V1_H__
+
+#include <asm/arch/cpu.h>
+
+#define CONFIG_SYS_GENERIC_BOARD  1
+#ifndef CONFIG_AML_MESON
+#warning "include warning"
+#endif
+
+/* SMP Definitinos */
+#define CPU_RELEASE_ADDR		secondary_boot_func
+
+/* Serial config */
+#define CONFIG_CONS_INDEX 2
+#define CONFIG_BAUDRATE  115200
+#define CONFIG_AML_MESON_SERIAL   1
+#define CONFIG_SERIAL_MULTI		1
+
+/* args/envs */
+#define CONFIG_SYS_MAXARGS  64
+#define CONFIG_BOOTARGS "init=/init console=ttyS0,115200 earlyprintk=aml-uart,0xc81004c0 selinux=0"
+
+#define CONFIG_ENV_IS_NOWHERE  1
+#define CONFIG_ENV_SIZE   (64*1024)
+#define CONFIG_FIT 1
+#define CONFIG_OF_LIBFDT 1
+#define CONFIG_ANDROID_BOOT_IMAGE 1
+#define CONFIG_ANDROID_IMG 1
+#define CONFIG_SYS_BOOTM_LEN (64<<20) /* Increase max gunzip size*/
+
+/* ddr */
+#define CONFIG_DDR_SIZE					1024 //MB
+#define CONFIG_DDR_CLK					936  //MHz, should be multiple of 24
+#define CONFIG_DDR_TYPE					CONFIG_DDR_TYPE_DDR3
+/* DDR channel setting, please refer hardware design.
+ *    CONFIG_DDR0_RANK0_ONLY   : one channel
+ *    CONFIG_DDR0_RANK01_SAME  : one channel use two rank with same setting
+ *    CONFIG_DDR01_SHARE_AC    : two channels  */
+#define CONFIG_DDR_CHANNEL_SET			CONFIG_DDR0_RANK01_SAME
+#define CONFIG_DDR_FULL_TEST			0 //1 for ddr full test
+#define CONFIG_NR_DRAM_BANKS			1
+
+/* storage: emmc/nand/sd */
+#define	CONFIG_STORE_COMPATIBLE 1
+#define CONFIG_AML_SD_EMMC 1
+#ifdef	CONFIG_AML_SD_EMMC
+	#define CONFIG_GENERIC_MMC 1
+	#define CONFIG_CMD_MMC 1
+#endif
+#define CONFIG_AML_NAND	1
+#ifdef CONFIG_AML_NAND
+#endif
+#define	CONFIG_PARTITIONS 1
+#define CONFIG_SYS_NO_FLASH  1
+
+/* vpu */
+#define CONFIG_AML_VPU 1
+#ifdef CONFIG_AML_VPU
+#define CONFIG_VPU_PRESET 1
+#endif
+
+/* USB
+ * Enable CONFIG_MUSB_HCD for Host functionalities MSC, keyboard
+ * Enable CONFIG_MUSB_UDD for Device functionalities.
+ */
+/* #define CONFIG_MUSB_UDC		1 */
+#define CONFIG_CMD_USB 1
+#if defined(CONFIG_CMD_USB)
+	#define CONFIG_M8_USBPORT_BASE_A	0xC9000000
+	#define CONFIG_M8_USBPORT_BASE_B	0xC9100000
+	#define CONFIG_USB_STORAGE      1
+	#define CONFIG_USB_DWC_OTG_HCD  1
+	#define CONFIG_USB_DWC_OTG_294	1
+#endif //#if defined(CONFIG_CMD_USB)
+#define CONFIG_AML_TINY_USBTOOL 1
+
+/* net */
+#define CONFIG_CMD_NET   1
+#if defined(CONFIG_CMD_NET)
+	#define CONFIG_AML_ETHERNET 1
+	#define CONFIG_NET_MULTI 1
+	#define CONFIG_CMD_PING 1
+	#define CONFIG_CMD_DHCP 1
+	#define CONFIG_CMD_RARP 1
+	//#define CONFIG_NET_RGMII
+	//	//#define CONFIG_NET_RMII_CLK_EXTERNAL //use external 50MHz clock source
+	#define CONFIG_AML_ETHERNET    1                   /*to link /driver/net/aml_ethernet.c*/
+	#define IP101PHY    1                   /*to link /driver/net/aml_ethernet.c*/
+	#define CONFIG_HOSTNAME        arm_gxbb
+	#define CONFIG_ETHADDR         00:15:18:01:81:31   /* Ethernet address */
+	#define CONFIG_IPADDR          10.18.9.97          /* Our ip address */
+	#define CONFIG_GATEWAYIP       10.18.9.1           /* Our getway ip address */
+	#define CONFIG_SERVERIP        10.18.9.113         /* Tftp server ip address */
+	#define CONFIG_NETMASK         255.255.255.0
+#endif /* (CONFIG_CMD_NET) */
+
+/* other devices */
+#define CONFIG_EFUSE 1
+#define CONFIG_SYS_I2C_AML 1
+#define CONFIG_SYS_I2C_SPEED     400000
+
+/* commands */
+#define CONFIG_CMD_CACHE 1
+#define CONFIG_CMD_BOOTI 1
+#define CONFIG_CMD_EFUSE 1
+#define CONFIG_CMD_I2C 1
+#define CONFIG_CMD_MEMORY 1
+#define CONFIG_CMD_FAT 1
+
+/*file system*/
+#define CONFIG_DOS_PARTITION 1
+#define CONFIG_MMC 1
+#define CONFIG_FS_FAT 1
+#define CONFIG_FS_EXT4 1
+
+/* Cache Definitions */
+//#define CONFIG_SYS_DCACHE_OFF
+//#define CONFIG_SYS_ICACHE_OFF
+
+/* other functions */
+#define CONFIG_NEED_BL301 	1
+
+#endif
+
