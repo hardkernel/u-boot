f7473d6b7746d538af7e98ea96018938651d5637
Frank Chen <frank.chen@amlogic.com>
2015-06-25 20:31:56 +0800

    GXB: remove useless bootargs in p200/p201/skt
        selinux=0 is useless option, if you want disable selinux, pls use
        androidboot.selinux=disabled, and selinux should default to enable for
        andoid
    
    Change-Id: Ib9a32c6a61ba1211a94d5da4e6a878b98976059c

--- include/configs/gxb_skt_v1.h
+++ include/configs/gxb_skt_v1.h
@@ -42,7 +42,7 @@
 #define CONFIG_SYS_MAXARGS  64
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"storeboot=\0"
-#define CONFIG_BOOTARGS "init=/init console=ttyS0,115200 no_console_suspend earlyprintk=aml-uart,0xc81004c0 selinux=0"
+#define CONFIG_BOOTARGS "init=/init console=ttyS0,115200 no_console_suspend earlyprintk=aml-uart,0xc81004c0"
 #define CONFIG_BOOTCOMMAND "run storeboot"
 
 //#define CONFIG_ENV_IS_NOWHERE  1
