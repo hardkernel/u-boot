48505cd3c2d1d2bc25e9b6878548778d6fa26db1
Yan Wang <yan.wang@amlogic.com>
2015-06-29 15:34:54 +0800

    GXB: UBOOT: update mailbox to support thermal.
      thermal get value: same with kernel SCPI API,
      thermal calibration: SMC command.
    
      BL31 commit:
      commit fe4790a424d500f4297922ddf19c53467a6b4599
      Author: Yan Wang <yan.wang@amlogic.com>
      Date:   Mon Jun 29 16:05:48 2015 +0800
        GXB:BL31: update efuse driver.
           1. BL31 send secure mailbox to BL30 for thermal init.
           2. BL31 store efuse layout.
           3. update plat/ program data types.
    
      BL30 commit:
      commit 6bd0718e92e139002f1d3602972169feb162e02f
      Author: Yan Wang <yan.wang@amlogic.com>
      Date: Mon Jun 29  16:25:10 2015 +0800
         GXB: BL30: update efuse and thermal driver.
             1. efuse remove thermal interface, only efuse R/W APIs.
    	 2. thermal add init function to store efuse layout and
    	    update thermal value into register.
    
    Change-Id: Iad42f364ca246f2253059ea2b7eb6e5b127ac70c

--- arch/arm/include/asm/arch-gxb/mailbox.h
+++ arch/arm/include/asm/arch-gxb/mailbox.h
@@ -19,7 +19,9 @@
 #ifndef __GXBB_MAILBOX_H_
 #define __GXBB_MAILBOX_H_
 
+#define SCPI_CMD_SENSOR_VALUE 0x1C
 #define SCPI_CMD_OPEN_SCP_LOG 0xC4
+#define SCPI_CMD_THERMAL_CALIB 0xC5
 
 #define LOW_PRIORITY	0
 #define HIGH_PRIORITY 1
@@ -30,6 +32,24 @@
 #define MHU_LOW_SCP_TO_AP_PAYLOAD		0x13000
 #define MHU_LOW_AP_TO_SCP_PAYLOAD		0x13200
 
+enum scpi_error_codes {
+	SCPI_SUCCESS = 0, /* Success */
+	SCPI_ERR_PARAM = 1, /* Invalid parameter(s) */
+	SCPI_ERR_ALIGN = 2, /* Invalid alignment */
+	SCPI_ERR_SIZE = 3, /* Invalid size */
+	SCPI_ERR_HANDLER = 4, /* Invalid handler/callback */
+	SCPI_ERR_ACCESS = 5, /* Invalid access/permission denied */
+	SCPI_ERR_RANGE = 6, /* Value out of range */
+	SCPI_ERR_TIMEOUT = 7, /* Timeout has occurred */
+	SCPI_ERR_NOMEM = 8, /* Invalid memory area or pointer */
+	SCPI_ERR_PWRSTATE = 9, /* Invalid power state */
+	SCPI_ERR_SUPPORT = 10, /* Not supported or disabled */
+	SCPI_ERR_DEVICE = 11, /* Device error */
+	SCPI_ERR_MAX
+};
+
 void open_scp_log(void);
+int thermal_calibration(unsigned int type, unsigned int data);
+int thermal_get_value(unsigned int sensor_id, unsigned int *value);
 
  #endif
