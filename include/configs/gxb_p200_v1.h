34743adaae04b327c42c43d4d412d1ec6fcee857
Tao Zeng <tao.zeng@amlogic.com>
2015-06-30 13:50:43 +0800

    P200/P201: reduce VDDEE to 1.0v to save power
    Old value of VDDEE is 1.1v, now change to 1.0v. QA has
    verified on p200/p201 with items including online/local
    video playing, 3D game, benchmark and normal usage.
    No stability related problems found.
    
    Change-Id: I8c8675e4acc0a1d982898106327c8e499f0ad0e2

--- include/configs/gxb_p200_v1.h
+++ include/configs/gxb_p200_v1.h
@@ -34,7 +34,7 @@
  */
 #define CONFIG_PLATFORM_POWER_INIT
 #define CONFIG_VCCK_INIT_VOLTAGE	1100
-#define CONFIG_VDDEE_INIT_VOLTAGE	1100
+#define CONFIG_VDDEE_INIT_VOLTAGE	1000
 
 /* SMP Definitinos */
 #define CPU_RELEASE_ADDR		secondary_boot_func
