ebdb8cb298d519eaa1f4234a7d8183d77b8a9727
Xing Xu <xing.xu@amlogic.com>
2015-07-16 13:40:55 +0800

    Revert "fix for bad mode"
    This reverts commit d6aee588b87616554942744fbcac7c279ad0231e.
    
    Change-Id: I0ec64aebd03e00ba45793d15e25bc23d7dceb58e

--- arch/arm/cpu/armv8/common/firmware/plat/gxb/bl2_plat_setup.c
+++ arch/arm/cpu/armv8/common/firmware/plat/gxb/bl2_plat_setup.c
@@ -42,7 +42,6 @@
 #include <xlat_tables.h>
 #include <io.h>
 #include "storage.h"
-#include <asm/arch/secure_apb.h>
 
 /*******************************************************************************
  * Declarations of linker defined symbols which will help us find the layout
@@ -204,7 +203,6 @@ void bl2_plat_flush_bl31_params(void)
  ******************************************************************************/
 void bl2_plat_arch_setup(void)
 {
-	writel(readl(SYS_CPU_MISC) | (0x3 << 20),SYS_CPU_MISC);
 	return;
 }
 
