e29aa51edb9519c2f2e0f246034bf6cd6c949600
Dongjin Kim <tobetter@gmail.com>
2015-09-01 14:20:26 +0900

    ODROID-C2: add fastboot partition details of memory card
    Change-Id: I8846a7575c56b40fb10cb1dee90710f422bf038a
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

---
+++ board/hardkernel/odroid-c2/odroid-c2-fastboot.c
@@ -0,0 +1,200 @@
+/*
+ * Copyright (C) 2015 Hardkernel Co,. Ltd
+ * Dongjin Kim <tobetter@gmail.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <linux/sizes.h>
+#include <linux/string.h>
+#include <usb/fastboot.h>
+
+/* FIXME: Is block size always 512? */
+#define lba_to_bytes(x)		(int)((size_t)x * 512)
+#define bytes_to_lba(x)		((size_t)x / 512)
+#define kbytes_to_lba(x)	bytes_to_lba(x * SZ_1K)
+#define bytes_to_kb(x)		((size_t)x / SZ_1K)
+
+static struct fbt_partition {
+	const char *name;
+	unsigned int size_kb;
+} partitions[] = {
+	{
+		.name = "bootloader",
+		.size_kb = bytes_to_kb(720 * SZ_1K),
+	},
+#if 0	/* FIXME: 'bcb' partition is required for Android recovery. */
+	{
+		.name = "bcb",			/* Bootloader * control * block */
+		.size_kb = bytes_to_kb(SZ_4K)
+	},
+#endif
+	{
+		.name = "env",			/* "environment" */
+		.size_kb = bytes_to_kb(CONFIG_ENV_SIZE)
+	}, {
+		.name = "dtb",			/* Device * Tree */
+		.size_kb = bytes_to_kb(SZ_64K)
+	}, {
+		.name = "boot",			/* Boot * image */
+		.size_kb = bytes_to_kb(10 * SZ_1M)
+	}, {
+		.name = "recovery",		/* Recovery Image */
+		.size_kb = bytes_to_kb(12 * SZ_1M)
+	}, {
+		.name = "logo",			/* Logo */
+		.size_kb = bytes_to_kb(2 * SZ_1M)
+	}
+};
+
+static struct dos_partition {
+	const char *name;
+	int part;
+	u8 type;
+	size_t size_kb;
+} dos_partitions[] = {
+	{
+		.name = "cache",
+		.part = 2,
+		.type = 0x83,
+		.size_kb = bytes_to_kb(2 * SZ_1G),
+	}, {
+		.name = "system",
+		.part = 3,
+		.type = 0x83,
+		.size_kb = bytes_to_kb(2 * SZ_1G),
+	}, {
+		.name = "userdata",
+		.part = 4,
+		.type = 0x83,
+		.size_kb = bytes_to_kb(3 * SZ_1G),
+	}, {
+		.name = "vfat",
+		.part = 1,
+		.type = 0x0c,
+		.size_kb = -1,
+	},
+};
+
+static int n = 0;
+
+static int valid_partition_number(int part)
+{
+	return (1 <= part) && (part <= ARRAY_SIZE(dos_partitions));
+}
+
+/*
+ * Initiate dos partition index and return the first sector (lba)
+ */
+lbaint_t board_dos_partition_start(void)
+{
+	int n;
+	lbaint_t next = 0;
+
+	for (n = 0 ; n < ARRAY_SIZE(partitions); n++)
+		next += kbytes_to_lba(partitions[n].size_kb);
+
+	return next;
+}
+
+/*
+ * Get the partition detail, partition number and its type, as well as
+ * return the number of sectors to allocate for the partition.
+ */
+lbaint_t board_dos_partition_next(int *part, u8 *type)
+{
+	if (!valid_partition_number(n + 1))
+		return 0;
+
+	struct dos_partition *p = &dos_partitions[n++];
+
+	*part = p->part;	/* partition number */
+	*type = p->type;	/* partition type */
+
+	/* Use remained sectors for this partition */
+	if (p->size_kb == -1)
+		return -1;
+
+	return kbytes_to_lba(p->size_kb);
+}
+
+/*
+ * Return the partition name of given partiton number. Since DOS partition
+ * does not support name on its parition table, the partition names are
+ * predefined upon each partition number.
+ */
+char *board_dos_partition_name(int part, char* name)
+{
+	int i;
+
+	if (!valid_partition_number(part))
+		return NULL;
+
+	for (i = 0; i < ARRAY_SIZE(dos_partitions); i++) {
+		/* Partition number is same with given to seek */
+		if (dos_partitions[i].part == part) {
+			strcpy(name, dos_partitions[i].name);
+			return name;
+		}
+	}
+
+	return NULL;
+}
+
+/*
+ * Initiate the fastboot partition entries with internal system partitions and
+ * DOS partition table.
+ */
+int board_partition_init(void)
+{
+	struct mmc *mmc;
+	fastboot_ptentry ptn;
+	lbaint_t next = 0;
+	lbaint_t len;
+	int n = 0;
+
+	fastboot_flash_reset_ptn();
+
+	mmc = find_mmc_device(CONFIG_FASTBOOT_FLASH_MMC_DEV);
+
+	for (n = 0 ; mmc && n < ARRAY_SIZE(partitions); n++) {
+		len = kbytes_to_lba(partitions[n].size_kb);
+
+		/* Skip to add the partition if start with '-', but move forward
+		 * to next position as much as its size
+		 */
+		if (partitions[n].name[0] == '-') {
+			next += len;
+			continue;
+		}
+
+		/* 'env' partition contains U-boot's environment fields, it
+		 * could be damaged by fastboot if its offset is invalid with
+		 * defined by CONFIG_ENV_OFFSET.
+		 */
+		if (!strcmp(partitions[n].name, "env") &&
+				(bytes_to_lba(CONFIG_ENV_OFFSET) != next)) {
+			printf("WARNING!!: Invalid offset of 'env' partition,"
+					" it must be " LBAFU " but " LBAFU "\n",
+					next, bytes_to_lba(CONFIG_ENV_OFFSET));
+		}
+
+		if (len == 0)
+			len = mmc->block_dev.lba - next;
+
+		strncpy((char*)&ptn.name, partitions[n].name, sizeof(ptn.name));
+
+		ptn.start = next;
+		ptn.length = len;
+
+		/* Add the partition to fastboot partition entry table */
+		fastboot_flash_add_ptn(&ptn);
+
+		next += len;
+	}
+
+	fastboot_load_dos_partition();
+
+	return 0;
+}
