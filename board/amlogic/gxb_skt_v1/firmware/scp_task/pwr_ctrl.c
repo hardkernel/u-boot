5f70ff3857722ad88308a40a7f43d5cf6b094dbb
Xiaobo Gu <xiaobo.gu@amlogic.com>
2015-10-22 01:34:01 -0800

    Revert "PD#111987: GXB: BL30/BL301: SCP enter WFI after suspend."
    Reason: Need fix suspend issue.
    
    P200 suspend fail, temp revert.
    
    This reverts commit 6b3f533968da54fd5323e1abf36c549a14448cd1.
    
    Change-Id: Ifaab4ab82cd5eeac8cfed67168f09e83f8e0c3a8

--- board/amlogic/gxb_skt_v1/firmware/scp_task/pwr_ctrl.c
+++ board/amlogic/gxb_skt_v1/firmware/scp_task/pwr_ctrl.c
@@ -1,5 +1,3 @@
-#include <gpio-gxbb.h>
-
 static void power_off_at_clk81(void)
 {
 }
@@ -21,28 +19,10 @@ static void power_on_at_32k(void)
 {
 }
 
-void get_wakeup_source(void *response, unsigned int suspend_from)
-{
-	struct wakeup_info *p = (struct wakeup_info *)response;
-	unsigned val;
-
-	p->status = RESPONSE_OK;
-	val = REMOTE_WAKEUP_SRC;
-#ifdef CONFIG_CEC_WAKEUP
-	if (suspend_from != SYS_POWEROFF)
-		val |= CEC_WAKEUP_SRC;
-#endif
-	p->sources = val;
-	p->gpio_info_count = 0;
-}
 
 static unsigned int detect_key(unsigned int suspend_from)
 {
 	int exit_reason = 0;
-	unsigned *irq = (unsigned *)SECURE_TASK_SHARE_IRQ;
-	/* unsigned *wakeup_en = (unsigned *)SECURE_TASK_RESPONSE_WAKEUP_EN; */
-
-	/* setup wakeup resources*/
 	init_remote();
 #ifdef CONFIG_CEC_WAKEUP
 	if (hdmi_cec_func_config & 0x1) {
@@ -50,41 +30,25 @@ static unsigned int detect_key(unsigned int suspend_from)
 		cec_node_init();
 	}
 #endif
-
-	/* *wakeup_en = 1;*/
 	do {
-		switch (*irq) {
-#ifdef CONFIG_CEC_WAKEUP
-		case IRQ_AO_CEC_NUM:
-			if (suspend_from == SYS_POWEROFF)
-				break;
-			if (cec_msg.log_addr) {
-				if (hdmi_cec_func_config & 0x1) {
-					cec_handler();
-					if (cec_msg.cec_power == 0x1) {
-						/*cec power key*/
-						exit_reason = CEC_WAKEUP;
-						break;
-					}
+	#ifdef CONFIG_CEC_WAKEUP
+		if (cec_msg.log_addr) {
+			if (hdmi_cec_func_config & 0x1) {
+				cec_handler();
+				if (cec_msg.cec_power == 0x1) {  //cec power key
+					exit_reason = CEC_WAKEUP;
+					break;
 				}
-			} else if (hdmi_cec_func_config & 0x1)
-				cec_node_init();
-		break;
-#endif
-		case IRQ_AO_IR_DEC_NUM:
-			if (remote_detect_key())
-				exit_reason = REMOTE_WAKEUP;
-			break;
-		default:
+			}
+		} else if (hdmi_cec_func_config & 0x1) {
+			cec_node_init();
+		}
+	#endif
+		if (remote_detect_key()) {
+			exit_reason = REMOTE_WAKEUP;
 			break;
 		}
-		*irq = 0xffffffff;
-		if (exit_reason)
-			break;
-		else
-			asm volatile("wfi");
 	} while (1);
-
 	return exit_reason;
 }
 
@@ -98,5 +62,4 @@ static void pwr_op_init(struct pwr_op *pwr_op)
 	pwr_op->power_on_at_32k = power_on_at_32k;
 
 	pwr_op->detect_key = detect_key;
-	pwr_op->get_wakeup_source = get_wakeup_source;
 }
