a26d6a0e2ef0d4a238545dff615d829e3f58fe45
ckkim <ckkim@hardkernel.com>
2015-11-12 17:40:16 +0900

    Exynos4412:Support Sandisk emmc 5.1 TLC
    Change-Id: Ibd35f44fb4c93d6c439eb468e946910eb5a6e59d

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -971,13 +971,14 @@ static int mmc_read_ext_csd(struct mmc *host)
 
 	ext_csd_struct = ext_csd[EXT_CSD_REV];
 	host->ext_csd.boot_size_multi = ext_csd[BOOT_SIZE_MULTI];
-	
+#if !defined(CONFIG_HKDK4412)
 	if (ext_csd_struct > 7) {
 		printf("unrecognised EXT_CSD structure "
 			"version %d\n", ext_csd_struct);
 		err = -1;
 		goto out;
 	}
+#endif
 
 	if (ext_csd_struct >= 2) {
 		host->ext_csd.sectors =
