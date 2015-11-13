13c7781f3ab7b44a65c2570bd9a76a2253b9c8de
Xiaobo Gu <xiaobo.gu@amlogic.com>
2015-11-13 00:12:30 -0800

    Revert "PD#115139: remote: support more key to wake up"
    gxb_skt_v1 compile error. push again when fixed.
    
    This reverts commit 6e9b5a3fd7a3d2311ec58f9d43e6f5deda446630.
    
    Change-Id: Ie73426ea1aa086f442a02bbee1e5effbebffcc41

--- arch/arm/cpu/armv8/gxb/firmware/scp_task/scp_remote.c
+++ arch/arm/cpu/armv8/gxb/firmware/scp_task/scp_remote.c
@@ -1,5 +1,4 @@
 #include "config.h"
-#include <config.h>
 #include "registers.h"
 #include "task_apis.h"
 
@@ -21,10 +20,7 @@ enum{
 
 #define IR_POWER_KEY_MASK 0xffffffff
 static unsigned int kk[] = {
-	CONFIG_IR_REMOTE_POWER_UP_KEY_VAL1,
-	CONFIG_IR_REMOTE_POWER_UP_KEY_VAL2,
-	CONFIG_IR_REMOTE_POWER_UP_KEY_VAL3,
-	CONFIG_IR_REMOTE_POWER_UP_KEY_VAL4,
+	0xe51afb04,
 };
 static int init_remote(void)
 {
@@ -36,22 +32,11 @@ static int init_remote(void)
 static int remote_detect_key(void)
 {
 	unsigned power_key;
-	int j;
-#if 0
 	if (((readl(AO_IR_DEC_STATUS))>>3) & 0x1) {
 		power_key = readl(AO_IR_DEC_FRAME);
-		if ((power_key&IR_POWER_KEY_MASK) == kk[j])
+		if ((power_key&IR_POWER_KEY_MASK) == kk[DECODEMODE_NEC])
 			return 1;
 
-	}
-#endif
-	if (((readl(AO_IR_DEC_STATUS))>>3) & 0x1) {
-	power_key = readl(AO_IR_DEC_FRAME);
-	for (j = 0 ; j < CONFIG_IR_REMOTE_POWER_UP_KEY_CNT; j++) {
-		if ((power_key&IR_POWER_KEY_MASK) == kk[j]) {
-		return 1;
-		}
-	}
 	}
 	return 0;
 }
