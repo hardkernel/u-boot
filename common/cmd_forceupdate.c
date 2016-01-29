ce71ac80cf1f918f7b62b863493eea71e77d0fe8
Xuhua Zhang <xuhua.zhang@amlogic.com>
2016-01-29 11:39:26 +0800

    PD#118624: adckey: press enter key force update
    Change-Id: I75140b42a9a2780f799c2f3393450c1c0c29b441

---
+++ common/cmd_forceupdate.c
@@ -0,0 +1,47 @@
+#include <common.h>
+#include <errno.h>
+#include <environment.h>
+#include <asm/saradc.h>
+
+inline int get_source_key(int channel)
+{
+	int adc_chan = channel;
+    int adc_val = get_adc_sample_gxbb(adc_chan);
+    //printf("get_source_key (%d) at channel (%d)\n", adc_val,adc_chan);
+    return adc_val;
+}
+
+static void check_auto_update(void)
+{
+	int source_key_value = -1;
+	int times=40;
+	while (times--) {
+		udelay(100000);
+		source_key_value = get_source_key(2);
+		if ((source_key_value >= 0) && (source_key_value < 40)) {
+			//printk("press update key!\n");
+		} else {
+			return ;
+		}
+	}
+	run_command ("run update", 0);
+}
+
+static void press_key_into_update(void)
+{
+	saradc_enable();
+	check_auto_update();
+	//saradc_disable();
+}
+
+static int do_forceupdate(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
+{
+	press_key_into_update();
+	return 0;
+}
+
+U_BOOT_CMD(
+	forceupdate,	1,	1,	do_forceupdate,
+	"forceupdate",
+	"forceupdate - press adc key before power on\n"
+);
