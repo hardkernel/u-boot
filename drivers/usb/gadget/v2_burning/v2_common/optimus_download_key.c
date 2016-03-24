259a9ac1c9cd5d948a4b2e4b83d558677638973c
Sam Wu <yihui.wu@amlogic.com>
2016-03-24 20:57:40 +0800

    PD#122441: factory_burning: support aml_hdcp_key2.2
    1,add support hdcp2.2 rx key burning.
    
    Change-Id: Ic3fc0328c2a3616c4c3dabe71a88d03480b15406

--- drivers/usb/gadget/v2_burning/v2_common/optimus_download_key.c
+++ drivers/usb/gadget/v2_burning/v2_common/optimus_download_key.c
@@ -22,6 +22,93 @@
 #define CMD_BUFF_SIZE (512)
 #endif// #ifndef CMD_BUFF_SIZE
 
+
+#ifndef __HDCP22_HEY_H__
+#define __HDCP22_HEY_H__
+
+typedef unsigned int    __u32;
+typedef signed int      __s32;
+typedef unsigned char   __u8;
+typedef signed char     __s8;
+
+#define IH_MAGIC	0x27051956	/* Image Magic Number		*/
+#define IH_NMLEN		32	/* Image Name Length		*/
+
+
+#define AML_RES_IMG_ITEM_ALIGN_SZ   16
+#define AML_RES_IMG_V1_MAGIC_LEN    8
+#define AML_RES_IMG_V1_MAGIC        "AML_HDK!"//8 chars
+#define AML_RES_IMG_HEAD_SZ         (24)//64
+#define AML_RES_ITEM_HEAD_SZ        (48)//64
+
+#define AML_RES_IMG_VERSION_V1      (0x01)
+
+#pragma pack(push, 1)
+typedef struct pack_header{
+	unsigned int	totalSz;/* Item Data total Size*/
+	unsigned int	dataSz;	/* Item Data used  Size*/
+	unsigned int	dataOffset;	/* Item data offset*/
+	unsigned char   type;	/* Image Type, not used yet*/
+	unsigned char 	comp;	/* Compression Type	*/
+    unsigned short  reserv;
+	char 	name[IH_NMLEN];	/* Image Name		*/
+}AmlResItemHead_t;
+#pragma pack(pop)
+
+//typedef for amlogic resource image
+#pragma pack(push, 4)
+typedef struct {
+    __u32   crc;    //crc32 value for the resouces image
+    __s32   version;//0x01 means 'AmlResItemHead_t' attach to each item , 0x02 means all 'AmlResItemHead_t' at the head
+
+    __u8    magic[AML_RES_IMG_V1_MAGIC_LEN];  //resources images magic
+
+    __u32   imgSz;  //total image size in byte
+    __u32   imgItemNum;//total item packed in the image
+
+}AmlResImgHead_t;
+#pragma pack(pop)
+
+/*The Amlogic resouce image is consisted of a AmlResImgHead_t and many
+ *
+ * |<---AmlResImgHead_t-->|<--AmlResItemHead_t-->---...--|<--AmlResItemHead_t-->---...--|....
+ *
+ */
+#endif//#ifndef __HDCP22_HEY_H__
+
+#define _AML_HDCP22_RX_KEY_NAME     "aml_hdcp_key2.2"
+static struct {
+    const char* keyName;
+    int         isEncrypt;
+}
+_amlHdcp22RxKeys[] = {
+    [0] = {.keyName = "hdcp22_rx_private",  .isEncrypt = 1},
+    [1] = {.keyName = "hdcp22_rx_fw",       .isEncrypt = 0},
+    [2] = {.keyName = "hdcp2_rx",           .isEncrypt = 0},
+};
+
+static char generalDataChange(const char input)
+{
+    int i;
+    char result = 0;
+
+    for (i=0; i<8; i++) {
+        if ((input & (1<<i)) != 0)
+            result |= (1<<(7-i));
+        else
+            result &= ~(1<<(7-i));
+    }
+
+    return result;
+}
+
+static void hdcp2DataDecryption(const unsigned len, const char *input, char *out)
+{
+    int i = 0;
+    for (i=0; i<len; i++)
+        out[i] = generalDataChange(input[i]);
+}
+
 /*
  *This fucntion called by mwrite command, mread= bulkcmd "download key .." + n * download transfer, for key n==1
  *Attentions: "return value is the key length" if burn sucess
@@ -37,10 +124,44 @@ unsigned v2_key_burn(const char* keyName, const u8* keyVal, const unsigned keyVa
     unsigned writtenLen = 0;
 
     DWN_DBG("to write key[%s] in len=%d\n", keyName, keyValLen);
-    ret = key_manage_write(keyName, keyVal, keyValLen);
-    if (ret) {
-        DWN_ERR("Fail to write key[%s] in len=%d\n", keyName, keyValLen);
-        return 0;
+    if (!strcmp(keyName, _AML_HDCP22_RX_KEY_NAME))
+    {
+        const AmlResImgHead_t*  packedImgHead = (AmlResImgHead_t*)keyVal;
+        const AmlResItemHead_t* packedImgItem = (AmlResItemHead_t*)(packedImgHead + 1);
+        int i = 0;
+
+        const unsigned gensum = add_sum(keyVal + 4, keyValLen - 4);
+        if (packedImgHead->crc != gensum) {
+            DWN_ERR("crc chcked failed, origsum[%8x] != gensum[%8x]\n", packedImgHead->crc, gensum);
+            return 0;
+        }
+
+        for (i = 0; i < packedImgHead->imgItemNum; ++i)
+        {
+            const AmlResItemHead_t* pItem = packedImgItem + i;
+            const char* itemN = _amlHdcp22RxKeys[i].keyName;
+            u8*         itembuf = (u8*)keyVal + pItem->dataOffset;
+            int       itemSz  = pItem->dataSz;
+
+            if (_amlHdcp22RxKeys[i].isEncrypt) {
+                DWN_MSG("key[%s] at[%d] isEncrypted\n", itemN, i);
+                hdcp2DataDecryption(itemSz, (char*)itembuf, (char*)itembuf);
+            }
+            DWN_MSG("burnkey[%s] at sz[%d]\n", itemN, itemSz);
+            ret = key_manage_write(itemN, itembuf, itemSz);
+            if (ret) {
+                DWN_ERR("Fail to write key[%s] in len=%d\n", itemN, itemSz);
+                return 0;
+            }
+        }
+    }
+    else
+    {
+        ret = key_manage_write(keyName, keyVal, keyValLen);
+        if (ret) {
+            DWN_ERR("Fail to write key[%s] in len=%d\n", keyName, keyValLen);
+            return 0;
+        }
     }
 
     writtenLen = ret >=0 ? keyValLen : 0;
@@ -114,6 +235,8 @@ int v2_key_command(const int argc, char * const argv[], char *info)
         const char* queryKey = subCmd_argv[1];
         int keyIsBurned = 0;
 
+        if (!strcmp(_AML_HDCP22_RX_KEY_NAME, queryKey)) queryKey = _amlHdcp22RxKeys[0].keyName;
+
         rcode = key_manage_query_exist(queryKey, &keyIsBurned);
         if (rcode) {
             sprintf(info, "failed to query key state, rcode %d\n", rcode);
@@ -134,6 +257,8 @@ int v2_key_command(const int argc, char * const argv[], char *info)
         const char* queryKey = subCmd_argv[1];
         int exist = 0;
         int canOverWrite = 0;
+
+        if (!strcmp(_AML_HDCP22_RX_KEY_NAME, queryKey)) queryKey = _amlHdcp22RxKeys[0].keyName;
 
         rcode = key_manage_query_canOverWrite(queryKey, &canOverWrite);
         if (rcode) {
