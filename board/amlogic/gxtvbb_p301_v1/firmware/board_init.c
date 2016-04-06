0ed3962fbf191ae4b3206adb69aaf97512f1e888
Evoke Zhang <evoke.zhang@amlogic.com>
2016-04-06 13:27:54 +0800

    PD#122667: lcd: add panel power init in BL21 board_init
    verified on platform p301.
    compile pass for all board config.
    
    Change-Id: Ib7fca9ef3c3617f7086a30346f40e5452f870f4d

--- board/amlogic/gxtvbb_p301_v1/firmware/board_init.c
+++ board/amlogic/gxtvbb_p301_v1/firmware/board_init.c
@@ -22,6 +22,48 @@
 #include "power.c"
 
 /* bl2 customer code */
+
+#define PNL_AO_GPIO_O_EN_N                0x09
+#define PNL_AO_GPIO_I                     0x0a
+
+#define PNL_PREG_PAD_GPIO1_EN_N           0x0f
+#define PNL_PREG_PAD_GPIO1_O              0x10
+#define PNL_PREG_PAD_GPIO1_I              0x11
+
+#define PNL_PREG_PAD_GPIO4_EN_N           0x18
+#define PNL_PREG_PAD_GPIO4_O              0x19
+#define PNL_PREG_PAD_GPIO4_I              0x1a
+
+#define PNL_REG_BASE               (0xc8834400L)
+#define PNL_REG(reg)               (PNL_REG_BASE + (reg << 2))
+#define PNL_REG_R(_reg)            (*(volatile unsigned int *)PNL_REG(_reg))
+#define PNL_REG_W(_reg, _value)    *(volatile unsigned int *)PNL_REG(_reg) = (_value);
+void panel_power_init(void)
+{
+	serial_puts("init panel power\n");
+	/* GPIOX_3 */
+	PNL_REG_W(PNL_PREG_PAD_GPIO4_O,
+		(PNL_REG_R(PNL_PREG_PAD_GPIO4_O) & ~(1 << 3)));
+	PNL_REG_W(PNL_PREG_PAD_GPIO4_EN_N,
+		(PNL_REG_R(PNL_PREG_PAD_GPIO4_EN_N) & ~(1 << 3)));
+	/* GPIOH_0 */
+/*	PNL_REG_W(PNL_PREG_PAD_GPIO1_O,
+		(PNL_REG_R(PNL_PREG_PAD_GPIO1_O) & ~(1 << 20)));
+	PNL_REG_W(PNL_PREG_PAD_GPIO1_EN_N,
+		(PNL_REG_R(PNL_PREG_PAD_GPIO1_EN_N) & ~(1 << 20)));
+*/
+	/* GPIOAO_4 */
+	PNL_REG_W(PNL_AO_GPIO_O_EN_N,
+		(PNL_REG_R(PNL_AO_GPIO_O_EN_N) & ~(1 << 20)));
+	PNL_REG_W(PNL_AO_GPIO_O_EN_N,
+		(PNL_REG_R(PNL_AO_GPIO_O_EN_N) & ~(1 << 4)));
+	/* GPIOY_13 */
+	PNL_REG_W(PNL_PREG_PAD_GPIO1_O,
+		(PNL_REG_R(PNL_PREG_PAD_GPIO1_O) & ~(1 << 13)));
+	PNL_REG_W(PNL_PREG_PAD_GPIO1_EN_N,
+		(PNL_REG_R(PNL_PREG_PAD_GPIO1_EN_N) & ~(1 << 13)));
+}
+
 void board_init(void)
 {
 	power_init(0);
@@ -38,4 +80,6 @@ void board_init(void)
 	//aml_update_bits(P_AO_GPIO_O_EN_N, 1 << 3, 0);
 	//aml_update_bits(P_AO_GPIO_O_EN_N, 1 << 19, 1 << 19);
 	writel((readl(P_AO_GPIO_O_EN_N) | (1 << 19)),P_AO_GPIO_O_EN_N);
+
+	panel_power_init();
 }
