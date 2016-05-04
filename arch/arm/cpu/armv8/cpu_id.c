dd8e331a8517636e0e6b785d3bc65112bf4deb80
Jets Yan <jets.yan@amlogic.com>
2016-05-04 17:36:54 +0800

    cpu_id: accurate and enlarged package_id
    Change-Id: I465a12316d47c21a5a0bc4381531612009bb33c8

--- arch/arm/cpu/armv8/cpu_id.c
+++ arch/arm/cpu/armv8/cpu_id.c
@@ -25,7 +25,7 @@ cpu_id_t get_cpu_id(void) {
 	cpu_id_t cpu_id;
 	unsigned int cpu_id_reg = readl(P_AO_SEC_SD_CFG8);
 	cpu_id.family_id = (cpu_id_reg >> 24) & (0XFF);
-	cpu_id.package_id = (cpu_id_reg >> 16) & (0XFF);
+	cpu_id.package_id = (cpu_id_reg >> 16) & (0XF0);
 	cpu_id.chip_rev = (cpu_id_reg >> 8) & (0XFF);
 	cpu_id.layout_ver = (cpu_id_reg) & (0XF);
 	return cpu_id;
