b1fe33e6cc5c1892dcff03f603c7094a13d3b299
Xiaobo Gu <xiaobo.gu@amlogic.com>
2016-07-29 17:03:26 +0800

    GXL/GXM/TXL: DDR add rdbi function
    Change-Id: I2ac1cc564fa567af49cbf359ac44c1e5e4a5e888
    Signed-off-by: Xiaobo Gu <xiaobo.gu@amlogic.com>

--- arch/arm/include/asm/arch-gxl/ddr_define.h
+++ arch/arm/include/asm/arch-gxl/ddr_define.h
@@ -111,11 +111,18 @@
 #endif
 #define DDR_FUNC_DDR_PLL_BYPASS				(CONFIG_DDR_PLL_BYPASS<<5)
 
+/* ddr rdbi function */
+#ifndef CONFIG_DDR_FUNC_RDBI
+#define CONFIG_DDR_FUNC_RDBI				0
+#endif
+#define DDR_FUNC_RDBI						(CONFIG_DDR_FUNC_RDBI<<6)
+
 #define DDR_FUNC							(DDR_FUNC_D2PLL					| \
 											DDR_FUNC_LP						| \
 											DDR_FUNC_ZQ_PD					| \
 											DDR_FUNC_EXT_VREF				| \
 											DDR_FUNC_DDR4_TIMING_TEST		| \
 											DDR_FUNC_DDR_PLL_BYPASS			| \
+											DDR_FUNC_RDBI					| \
 											(1 << 31) 						\
 											)
