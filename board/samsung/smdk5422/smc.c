f1a354e451fd954ad5496a812cbdf51c897397b0
Marcus Haehnel <marcus@mh-development.info>
2016-08-18 17:45:53 +0200

    Fix broken gcc version checking

--- board/samsung/smdk5422/smc.c
+++ board/samsung/smdk5422/smc.c
@@ -22,7 +22,7 @@ static inline u32 exynos_smc(u32 cmd, u32 arg1, u32 arg2, u32 arg3)
 	register u32 reg3 __asm__("r3") = arg3;
 
 	__asm__ volatile (
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 6
+#if __GNUC__ >= 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 		".arch_extension sec\n"
 #endif
 		"smc	0\n"
@@ -39,7 +39,7 @@ static inline u32 exynos_smc_read(u32 cmd)
 	register u32 reg1 __asm__("r1") = 0;
 
 	__asm__ volatile (
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 6
+#if __GNUC__ >= 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 		".arch_extension sec\n"
 #endif
 		"smc	0\n"
