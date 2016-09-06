b1c1496de600749dbc34aa46070a7aa3c77f75e2
Larson Jiang <larson.jiang@amlogic.com>
2016-09-06 15:58:58 +0800

    PD#131038: q201 bt wake up
    Change-Id: Idf8193b53b9806780d9f054e7e406394d4259a5c

--- board/amlogic/gxm_q201_v1/firmware/scp_task/pwr_ctrl.c
+++ board/amlogic/gxm_q201_v1/firmware/scp_task/pwr_ctrl.c
@@ -150,9 +150,11 @@ void get_wakeup_source(void *response, unsigned int suspend_from)
 	struct wakeup_info *p = (struct wakeup_info *)response;
 	unsigned val;
 	struct wakeup_gpio_info *gpio;
-
+	p->gpio_info_count = 0;
 	p->status = RESPONSE_OK;
-	val = (POWER_KEY_WAKEUP_SRC | AUTO_WAKEUP_SRC | REMOTE_WAKEUP_SRC);
+	//val = (POWER_KEY_WAKEUP_SRC | AUTO_WAKEUP_SRC | REMOTE_WAKEUP_SRC);
+	val = (POWER_KEY_WAKEUP_SRC | AUTO_WAKEUP_SRC | REMOTE_WAKEUP_SRC |
+	       BT_WAKEUP_SRC);
 #ifdef CONFIG_CEC_WAKEUP
 	if (suspend_from != SYS_POWEROFF)
 		val |= CEC_WAKEUP_SRC;
@@ -169,7 +171,18 @@ void get_wakeup_source(void *response, unsigned int suspend_from)
 	gpio->irq = IRQ_AO_GPIO0_NUM;
 	gpio->trig_type = GPIO_IRQ_FALLING_EDGE;
 
-	p->gpio_info_count = 1;
+	//p->gpio_info_count = 1;
+	p->gpio_info_count ++;
+
+	gpio = &(p->gpio_info[1]);
+	gpio->wakeup_id = BT_WAKEUP_SRC;
+	gpio->gpio_in_idx = GPIOX_18;
+	gpio->gpio_in_ao = 0;
+	gpio->gpio_out_idx = -1;
+	gpio->gpio_out_ao = -1;
+	gpio->irq = IRQ_GPIO0_NUM;
+	gpio->trig_type	= GPIO_IRQ_RISING_EDGE;
+	p->gpio_info_count ++;
 }
 void wakeup_timer_setup(void)
 {
@@ -255,6 +268,10 @@ static unsigned int detect_key(unsigned int suspend_from)
 			if ((readl(AO_GPIO_I) & (1<<2)) == 0)
 				exit_reason = POWER_KEY_WAKEUP;
 		}
+		if (irq[IRQ_GPIO0] == IRQ_GPIO0_NUM) {
+		    irq[IRQ_GPIO0] = 0xFFFFFFFF;
+			if ((readl(PREG_PAD_GPIO4_I) & (0x01 << 18)))
+			    exit_reason = BT_WAKEUP;}
 		if (exit_reason)
 			break;
 		else
