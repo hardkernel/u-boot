eb2df0c7ad8412673d7b629bf300f502041d27b0
Xingyu Chen <xingyu.chen@amlogic.com>
2016-10-24 18:17:19 +0800

    PD#133572: saradc: support to parse fdt
    1. support to parse flattened device tree(fdt) which share with the kernel,
    and get the register base address by reading the '/saradc' node in the fdt.
    2. remove the useless code
    
    Change-Id: Ie7246e6856c0754d533b773b1683a1a87f0489fc
    Signed-off-by: Xingyu Chen <xingyu.chen@amlogic.com>

--- arch/arm/include/asm/saradc.h
+++ arch/arm/include/asm/saradc.h
@@ -15,27 +15,9 @@ enum{AML_ADC_NO_AVG = 0,  AML_ADC_SIMPLE_AVG_1, AML_ADC_SIMPLE_AVG_2,
 #define AML_ADC_CHAN_XN	AML_ADC_CHAN_2
 #define AML_ADC_CHAN_YN	AML_ADC_CHAN_3
 
-
-
-typedef struct adckey_info{
-	const char *key;
-	int   value;	/* voltage/3.3v * 1023 */
-	int   tolerance;
-}adckey_info_t;
-
-typedef struct adc_info{
-	char * tint;
-	int    chan;
-	int    adc_type;
-	void * adc_data;
-}adc_info_t;
-
-struct adc_device{
-	adc_info_t * adc_device_info;
-	unsigned dev_num;
-};
-void saradc_enable(void);
+int saradc_probe(void);
+int saradc_enable(void);
 int saradc_disable(void);
-int  get_adc_sample_gxbb(int chan);
-int  get_adc_sample_gxbb_12bit(int chan);
+int get_adc_sample_gxbb(int chan);
+int get_adc_sample_gxbb_12bit(int chan);
 #endif /*__AML_SARADC_H__*/
--- drivers/adc/saradc.c
+++ drivers/adc/saradc.c
@@ -24,73 +24,55 @@
 #include <asm/arch/io.h>
 #include <asm/saradc.h>
 #include <asm/cpu_id.h>
+#ifdef CONFIG_OF_LIBFDT
+#include <libfdt.h>
+#endif
 
 //#define ENABLE_DYNAMIC_POWER
 
-#define CONFIG_GXBB 1
-static int flag_12bit;
-
-#if defined(CONFIG_GXBB)
-
-#define WRITE_REG(reg, val) writel(val, reg)
-#define READ_REG(reg)       readl(reg)
-
 #define AML_ADC_SAMPLE_DEBUG 0
+#define FDT_DEFAULT_ADDRESS  0x01000000
 
 #ifdef  CONFIG_TARGET_MESON_GXTV
-#define GXBB_CLK_REG                (volatile unsigned int *)0xc8100090
-#define P_SAR_SAR_ADC_REG0		    (volatile unsigned int *)0xc8100600
-#define P_SAR_ADC_CHAN_LIST		    (volatile unsigned int *)0xc8100604
-#define P_SAR_ADC_AVG_CNTL		    (volatile unsigned int *)0xc8100608
-#define P_SAR_ADC_REG3				(volatile unsigned int *)0xc810060c
-#define P_SAR_ADC_DELAY			    (volatile unsigned int *)0xc8100610
-#define P_SAR_ADC_LAST_RD			(volatile unsigned int *)0xc8100614
-#define P_SAR_ADC_FIFO_RD			(volatile unsigned int *)0xc8100618
-#define P_SAR_ADC_AUX_SW			(volatile unsigned int *)0xc810061c
-#define P_SAR_ADC_CHAN_10_SW		(volatile unsigned int *)0xc8100620
-#define P_SAR_ADC_DETECT_IDLE_SW	(volatile unsigned int *)0xc8100624
-#define P_SAR_ADC_DELTA_10	        (volatile unsigned int *)0xc8100628
-#define P_SAR_ADC_DELTA_11          (volatile unsigned int *)0xc810062c
-#define P_SAR_ADC_REG13				(volatile unsigned int *)0xc8100634
-
+#define GXBB_CLK_REG                	0xc8100090
+#define P_SAR_ADC_BASE             		0xc8100600
 #else
-#define GXBB_CLK_REG                (volatile unsigned int *)0xc883c3d8
-#define P_SAR_SAR_ADC_REG0		    (volatile unsigned int *)0xc1108680
-#define P_SAR_ADC_CHAN_LIST		    (volatile unsigned int *)0xc1108684
-#define P_SAR_ADC_AVG_CNTL		    (volatile unsigned int *)0xc1108688
-#define P_SAR_ADC_REG3				(volatile unsigned int *)0xc110868c
-#define P_SAR_ADC_DELAY			    (volatile unsigned int *)0xc1108690
-#define P_SAR_ADC_LAST_RD			(volatile unsigned int *)0xc1108694
-#define P_SAR_ADC_FIFO_RD			(volatile unsigned int *)0xc1108698
-#define P_SAR_ADC_AUX_SW			(volatile unsigned int *)0xc110869c
-#define P_SAR_ADC_CHAN_10_SW		(volatile unsigned int *)0xc11086a0
-#define P_SAR_ADC_DETECT_IDLE_SW	(volatile unsigned int *)0xc11086a4
-#define P_SAR_ADC_DELTA_10	        (volatile unsigned int *)0xc11086a8
-#define P_SAR_ADC_DELTA_11          (volatile unsigned int *)0xc11086ac
-#define P_SAR_ADC_REG13				(volatile unsigned int *)0xc11086b4
-
+#define GXBB_CLK_REG                	0xc883c3d8
+#define P_SAR_ADC_BASE		   			0xc1108680
 #endif
-/*
-#define P_SAR_ADC_DELTA_11		    (volatile unsigned int *)0xc11086a8
-#define P_SAR_ADC_TEMP_SES		    (volatile unsigned int *)0xc11086ac
-#define P_SAR_ADC_CLOCK			    (volatile unsigned int *)0xc883c3d8
-#define P_SAR_FIFO_READ			    (volatile unsigned int *)0xc1108698
-*/
 
-#define PP_SAR_ADC_REG0					P_SAR_SAR_ADC_REG0
-#define PP_SAR_ADC_CHAN_LIST 			P_SAR_ADC_CHAN_LIST
-#define PP_SAR_ADC_AVG_CNTL				P_SAR_ADC_AVG_CNTL
-#define PP_SAR_ADC_REG3					P_SAR_ADC_REG3
-#define PP_SAR_ADC_DELAY				P_SAR_ADC_DELAY
-#define PP_SAR_ADC_LAST_RD				P_SAR_ADC_LAST_RD
-#define PP_SAR_ADC_FIFO_RD				P_SAR_ADC_FIFO_RD
-#define PP_SAR_ADC_AUX_SW				P_SAR_ADC_AUX_SW
-#define PP_SAR_ADC_CHAN_10_SW			P_SAR_ADC_CHAN_10_SW
-#define PP_SAR_ADC_DETECT_IDLE_SW	    P_SAR_ADC_DETECT_IDLE_SW
-#define PP_SAR_ADC_DELTA_10				P_SAR_ADC_DELTA_10
-#define PP_SAR_ADC_DELTA_11				P_SAR_ADC_DELTA_11
-#define PP_SAR_ADC_REG13				P_SAR_ADC_REG13
+#define P_SAR_ADC_REG0(base)		    (base + (0x0))
+#define P_SAR_ADC_CHAN_LIST(base)		(base + (0x1))
+#define P_SAR_ADC_AVG_CNTL(base)		(base + (0x2))
+#define P_SAR_ADC_REG3(base)			(base + (0x3))
+#define P_SAR_ADC_DELAY(base)			(base + (0x4))
+#define P_SAR_ADC_LAST_RD(base)			(base + (0x5))
+#define P_SAR_ADC_FIFO_RD(base)			(base + (0x6))
+#define P_SAR_ADC_AUX_SW(base)			(base + (0x7))
+#define P_SAR_ADC_CHAN_10_SW(base)		(base + (0x8))
+#define P_SAR_ADC_DETECT_IDLE_SW(base)	(base + (0x9))
+#define P_SAR_ADC_DELTA_10(base)		(base + (0xa))
+#define P_SAR_ADC_DELTA_11(base)		(base + (0xb))
+#define P_SAR_ADC_REG13(base)			(base + (0xd))
 
+#define SARADC_STATE_IDLE 0
+#define SARADC_STATE_BUSY 1
+#define SARADC_STATE_SUSPEND 2
+
+#define FLAG_INITIALIZED    (1<<28)
+//#define FLAG_BUSY (1<<29)
+#define FLAG_BUSY_KERNEL    (1<<14) /* for kernel:SARADC_DELAY 14bit */
+#define FLAG_BUSY_KERNEL_BIT  14
+#define FLAG_BUSY_BL30      (1<<15) /* for bl30:SARADC_DELAY 15bit*/
+
+typedef struct {
+	unsigned char channel;
+	unsigned char adc_type; /*1:12bit;0:10bit*/
+	u32 __iomem *saradc_clk_addr;
+	u32 __iomem *saradc_base_addr;
+}saradc_info;
+
+static saradc_info saradc_dev;
 
 static __inline__ void aml_set_reg32_bits(volatile unsigned int *_reg, const uint32_t _value, const uint32_t _start, const uint32_t _len)
 {
@@ -109,74 +91,162 @@ static __inline__ uint32_t aml_read_reg32(volatile unsigned int *_reg)
 	return readl((volatile unsigned int *)_reg);
 };
 
-
-#define set_bits	aml_set_reg32_bits
-#define get_bits	aml_get_reg32_bits
-#define set_reg	    aml_write_reg32
-#define get_reg	    aml_read_reg32
-
-
-#endif
-
-#define SARADC_STATE_IDLE 0
-#define SARADC_STATE_BUSY 1
-#define SARADC_STATE_SUSPEND 2
-
-#define FLAG_INITIALIZED (1<<28)
-//#define FLAG_BUSY (1<<29)
-#define FLAG_BUSY_KERNEL    (1<<14) /* for kernel:SARADC_DELAY 14bit */
-#define FLAG_BUSY_KERNEL_BIT  14
-#define FLAG_BUSY_BL30      (1<<15) /* for bl30:SARADC_DELAY 15bit*/
-
-//static u8 g_chan_mux[AML_ADC_SARADC_CHAN_NUM] = {0,1,2,3,4,5,6,7};
+saradc_info *saradc_dev_get(void)
+{
+	return &saradc_dev;
+}
 
 /*
-* 1:enable
-* 0:disable
-*/
-void saradc_clock_switch(int onoff)
+  * description: used to enable and disable the clock of the SARADC
+  * onoff: 1: enable ; 0: disable
+  */
+void saradc_clock_switch( int onoff)
 {
+	saradc_info *saradc = saradc_dev_get();
+
 	if (onoff) {
 		if (get_cpu_id().family_id >= MESON_CPU_MAJOR_ID_GXBB) /*GXBB's clock from the clock tree.*/
-			aml_set_reg32_bits(GXBB_CLK_REG,1,8,1);
+			aml_set_reg32_bits(saradc->saradc_clk_addr,1,8,1);
 		else
-			aml_set_reg32_bits(PP_SAR_ADC_REG3,1,30,1);
+			aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),1,30,1);
 	} else {
 		if (get_cpu_id().family_id >= MESON_CPU_MAJOR_ID_GXBB) /*GXBB's clock from the clock tree.*/
-			aml_set_reg32_bits(GXBB_CLK_REG,0,8,1);
+			aml_set_reg32_bits(saradc->saradc_clk_addr,0,8,1);
 		else
-			aml_set_reg32_bits(PP_SAR_ADC_REG3,0,30,1);
+			aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),0,30,1);
 	}
 }
 
+/*
+  * description: used to set the DIV of the clock
+  */
 void saradc_clock_set(unsigned char val)
 {
+	saradc_info *saradc = saradc_dev_get();
+
 	if (get_cpu_id().family_id >= MESON_CPU_MAJOR_ID_GXBB) { /*bit0-bit7*/
 		val = val & 0xff;
-		aml_write_reg32(GXBB_CLK_REG, (0<<9) | (val << 0));
-	} else {                                               /*bit10-bit15*/
+		aml_write_reg32(saradc->saradc_clk_addr, (0<<9) | (val << 0));
+	} else {                                                 /*bit10-bit15*/
 		val = val & 0x3f;
-		aml_set_reg32_bits(PP_SAR_ADC_REG3,val,10,5);
+		aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),val,10,5);
 	}
 }
+
 static inline void saradc_power_control(int on)
 {
-	//struct saradc_reg3 *reg3 = (struct saradc_reg3 *)&adc->regs->reg3;
+	saradc_info *saradc = saradc_dev_get();
 
 	if (on) {
-		aml_set_reg32_bits(PP_SAR_ADC_DELTA_11,1,13,1);
-		aml_set_reg32_bits(PP_SAR_ADC_DELTA_11,3,5,2);
-		aml_set_reg32_bits(PP_SAR_ADC_REG3,1,21,1);
+		aml_set_reg32_bits(P_SAR_ADC_DELTA_11(saradc->saradc_base_addr),1,13,1);
+		aml_set_reg32_bits(P_SAR_ADC_DELTA_11(saradc->saradc_base_addr),3,5,2);
+		aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),1,21,1);
 
 		udelay(5);
 		saradc_clock_switch(1);
 	}	else {
 		saradc_clock_switch(0);
-		aml_set_reg32_bits(PP_SAR_ADC_REG3,0,30,1);
-		/*aml_set_reg32_bits(PP_SAR_ADC_DELTA_11,0,13,1);*//* disable bandgap */
-		aml_set_reg32_bits(PP_SAR_ADC_DELTA_11,0,5,2);
+		aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),0,30,1);
+		/*aml_set_reg32_bits(PP_SAR_ADC_DELTA_11(saradc->saradc_base_addr),0,13,1);*//* disable bandgap */
+		aml_set_reg32_bits(P_SAR_ADC_DELTA_11(saradc->saradc_base_addr),0,5,2);
 	}
 }
+
+void saradc_hw_init(void)
+{
+	saradc_info *saradc = saradc_dev_get();
+
+	if (get_cpu_id().family_id <= MESON_CPU_MAJOR_ID_GXTVBB)
+		saradc->adc_type = 0;
+	else
+		saradc->adc_type = 1;
+
+	aml_write_reg32(P_SAR_ADC_REG0(saradc->saradc_base_addr), 0x84004040);
+	aml_write_reg32(P_SAR_ADC_CHAN_LIST(saradc->saradc_base_addr), 0);
+	/* REG2: all chanel set to 8-samples & median averaging mode */
+	aml_write_reg32(P_SAR_ADC_AVG_CNTL(saradc->saradc_base_addr), 0);
+	aml_write_reg32(P_SAR_ADC_REG3(saradc->saradc_base_addr), 0x9388000a);
+	aml_write_reg32(P_SAR_ADC_DELAY(saradc->saradc_base_addr), 0x10a000a);
+	aml_write_reg32(P_SAR_ADC_AUX_SW(saradc->saradc_base_addr), 0x3eb1a0c);
+	aml_write_reg32(P_SAR_ADC_CHAN_10_SW(saradc->saradc_base_addr), 0x8c000c);
+	aml_write_reg32(P_SAR_ADC_DETECT_IDLE_SW(saradc->saradc_base_addr), 0xc000c);
+
+	if (saradc->adc_type)
+		aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),0x1,27,1);
+
+	saradc_clock_set(20);
+
+#if AML_ADC_SAMPLE_DEBUG
+	printf("ADCREG reg0 =%x\n",   aml_read_reg32(P_SAR_ADC_REG0(saradc->saradc_base_addr)));
+	printf("ADCREG ch list =%x\n",aml_read_reg32(P_SAR_ADC_CHAN_LIST(saradc->saradc_base_addr)));
+	printf("ADCREG avg  =%x\n",   aml_read_reg32(P_SAR_ADC_AVG_CNTL(saradc->saradc_base_addr)));
+	printf("ADCREG reg3 =%x\n",   aml_read_reg32(P_SAR_ADC_REG3(saradc->saradc_base_addr)));
+	printf("ADCREG ch72 sw =%x\n",aml_read_reg32(P_SAR_ADC_AUX_SW(saradc->saradc_base_addr)));
+	printf("ADCREG ch10 sw =%x\n",aml_read_reg32(P_SAR_ADC_CHAN_10_SW(saradc->saradc_base_addr)));
+	printf("ADCREG detect&idle=%x\n",aml_read_reg32(P_SAR_ADC_DETECT_IDLE_SW(saradc->saradc_base_addr)));
+	printf("ADCREG GXBB_CLK_REG=%x\n",aml_read_reg32(saradc->saradc_clk_addr));
+#endif //AML_ADC_SAMPLE_DEBUG
+
+}
+
+int saradc_probe(void)
+{
+	int ret;
+	int len;
+	int parent_offset;
+	char *fdt_addr;
+	u32 *reg_addr;
+	int saradc_fdt_ready = 0;
+	unsigned long temp_addr;
+	saradc_info *saradc = saradc_dev_get();
+
+#ifdef CONFIG_OF_LIBFDT
+#ifdef CONFIG_DTB_MEM_ADDR
+	fdt_addr = (char *)CONFIG_DTB_MEM_ADDR;
+#else
+	fdt_addr = (char *)FDT_DEFAULT_ADDRESS;
+#endif
+	if ((ret = fdt_check_header((const void *)fdt_addr)) < 0) {
+		printf("saradc: check dts: %s, load default parameters\n",
+			fdt_strerror(ret));
+	}else{
+		saradc_fdt_ready = 1;
+	}
+#endif
+
+	if (saradc_fdt_ready) {
+#ifdef CONFIG_OF_LIBFDT
+		parent_offset = fdt_path_offset(fdt_addr, "/saradc");
+		if (parent_offset < 0) {
+			printf("saradc: not find the node /saradc: %s\n",fdt_strerror(parent_offset));
+			return -1;
+		}
+
+		reg_addr = (u32 *)fdt_getprop(fdt_addr, parent_offset, "reg", &len);
+		if (NULL == reg_addr) {
+			printf("saradc: failed to get /saradc\n");
+			return -1;
+		}
+	   /*
+		*  To avoid error "-Werror=int-to-pointer" when this code is compiled,
+		*  and use the variable of type 'unsigned long' to save the address, then cast 'unsigned long' to 'pointer' type."
+		*/
+		temp_addr = fdt32_to_cpu(reg_addr[5]); /*big-endian to little-endian*/
+		saradc->saradc_clk_addr = (u32 __iomem *)(temp_addr & 0xffffffff);
+		temp_addr = fdt32_to_cpu(reg_addr[1]); /*big-endian to little-endian*/
+		saradc->saradc_base_addr = (u32 __iomem *)(temp_addr & 0xffffffff);
+#endif
+	} else {
+		saradc->saradc_clk_addr =  (u32 __iomem *)GXBB_CLK_REG;
+		saradc->saradc_base_addr = (u32 __iomem *)P_SAR_ADC_BASE;
+	}
+
+	saradc_hw_init();
+
+	return 0;
+
+}
+
 static void saradc_internal_cal_12bit(void)
 {
 	int val[5]/*, nominal[5] = {0, 1024, 2048, 3072, 4096}*/;
@@ -184,12 +254,13 @@ static void saradc_internal_cal_12bit(void)
 	int abs_val = 4096;
 	unsigned int abs_num = 0;
 	unsigned int abs_tmp = 0;
+	saradc_info *saradc = saradc_dev_get();
 
 	/* set CAL_CNTL: 3/4 VDD*/
-	aml_set_reg32_bits(PP_SAR_ADC_REG3,3,23,3);
+	aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),3,23,3);
 
 	for (i = 0; i < 64; i++) {
-		aml_set_reg32_bits(PP_SAR_ADC_REG13,i,8,6);
+		aml_set_reg32_bits(P_SAR_ADC_REG13(saradc->saradc_base_addr),i,8,6);
 		udelay(5);
 		val[0] = get_adc_sample_gxbb_12bit(7);
 		abs_tmp = abs(3000 - val[0]);
@@ -198,68 +269,25 @@ static void saradc_internal_cal_12bit(void)
 			abs_num = i;
 		}
 	}
-	aml_set_reg32_bits(PP_SAR_ADC_REG13,abs_num,8,6);
+	aml_set_reg32_bits(P_SAR_ADC_REG13(saradc->saradc_base_addr),abs_num,8,6);
 }
 //#define NOT_USE_SINA_DETECT 1
 //#define CLK_DIV 19
 
-void saradc_enable(void)
-{
-	if (get_cpu_id().family_id <= MESON_CPU_MAJOR_ID_GXTVBB)
-		flag_12bit = 0;
-	else
-		flag_12bit = 1;
-
-	printf("get_cpu_id flag_12bit=%d\n",flag_12bit);
-	set_reg(P_SAR_SAR_ADC_REG0, 0x84004040);
-	set_reg(PP_SAR_ADC_CHAN_LIST, 0);
-	/* REG2: all chanel set to 8-samples & median averaging mode */
-	set_reg(PP_SAR_ADC_AVG_CNTL, 0);
-
-	if (flag_12bit)
-		set_reg(PP_SAR_ADC_REG3, 0x9b88000a);
-	else
-		set_reg(PP_SAR_ADC_REG3, 0x9388000a);
-	aml_set_reg32_bits(PP_SAR_ADC_REG3,0x14,10,5);
-
-	/*gxl change vdd
-	set_reg(PP_SAR_ADC_REG13, );
-	gxl change sampling mode */
-	if (flag_12bit)
-		aml_set_reg32_bits(PP_SAR_ADC_REG3,0x1,27,1);
-
-	saradc_clock_set(20);
-
-	set_reg(PP_SAR_ADC_DELAY, 0x10a000a);
-	set_reg(PP_SAR_ADC_AUX_SW, 0x3eb1a0c);
-	set_reg(PP_SAR_ADC_CHAN_10_SW, 0x8c000c);
-	set_reg(PP_SAR_ADC_DETECT_IDLE_SW, 0xc000c);
-
-#if AML_ADC_SAMPLE_DEBUG
-	printf("ADCREG reg0 =%x\n",   get_reg(PP_SAR_ADC_REG0));
-	printf("ADCREG ch list =%x\n",get_reg(PP_SAR_ADC_CHAN_LIST));
-	printf("ADCREG avg  =%x\n",   get_reg(PP_SAR_ADC_AVG_CNTL));
-	printf("ADCREG reg3 =%x\n",   get_reg(PP_SAR_ADC_REG3));
-	printf("ADCREG ch72 sw =%x\n",get_reg(PP_SAR_ADC_AUX_SW));
-	printf("ADCREG ch10 sw =%x\n",get_reg(PP_SAR_ADC_CHAN_10_SW));
-	printf("ADCREG detect&idle=%x\n",get_reg(PP_SAR_ADC_DETECT_IDLE_SW));
-	printf("ADCREG GXBB_CLK_REG=%x\n",get_reg(GXBB_CLK_REG));
-#endif //AML_ADC_SAMPLE_DEBUG
-
-	saradc_power_control(1);
-	saradc_internal_cal_12bit();
-}
-
-/*use_10bit_num=1,10 bit
-  use_10bit_num=0,12 bit*/
+/*
+  * description: used to get sampling value
+  * ch: set channel
+  * use_10bit_num: set the bits of the sampling value(1:10bit; 0:12bit)
+  */
 int get_adc_sample_gxbb_early(int ch, int use_10bit_num)
 {
 	int value, count, sum;
 	//unsigned long flags;
 	//int adc_state = SARADC_STATE_BUSY;
+	saradc_info *saradc = saradc_dev_get();
 
 	count = 0;
-	while (aml_read_reg32(PP_SAR_ADC_DELAY) & FLAG_BUSY_BL30) {
+	while (aml_read_reg32(P_SAR_ADC_DELAY(saradc->saradc_base_addr)) & FLAG_BUSY_BL30) {
 		udelay(100);
 		if (++count > 100) {
 			printf("bl30 busy error\n");
@@ -271,21 +299,21 @@ int get_adc_sample_gxbb_early(int ch, int use_10bit_num)
 	saradc_clock_set(0xa0);
 	saradc_clock_switch(1);
 
-	aml_set_reg32_bits(PP_SAR_ADC_DELAY,1,FLAG_BUSY_KERNEL_BIT,1);
-	aml_set_reg32_bits(PP_SAR_ADC_REG3,1,29,1);
+	aml_set_reg32_bits(P_SAR_ADC_DELAY(saradc->saradc_base_addr),1,FLAG_BUSY_KERNEL_BIT,1);
+	aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),1,29,1);
 
-    set_reg(PP_SAR_ADC_CHAN_LIST, ch);
-    set_reg(PP_SAR_ADC_DETECT_IDLE_SW, (0xc000c | (ch<<23) | (ch<<7)));
-    aml_set_reg32_bits(PP_SAR_ADC_REG0, 1,0,1);
-    aml_set_reg32_bits(PP_SAR_ADC_REG0, 1,2,1);
+	aml_write_reg32(P_SAR_ADC_CHAN_LIST(saradc->saradc_base_addr), ch);
+	aml_write_reg32(P_SAR_ADC_DETECT_IDLE_SW(saradc->saradc_base_addr), (0xc000c | (ch<<23) | (ch<<7)));
+	aml_set_reg32_bits(P_SAR_ADC_REG0(saradc->saradc_base_addr), 1,0,1);
+	aml_set_reg32_bits(P_SAR_ADC_REG0(saradc->saradc_base_addr), 1,2,1);
 
-    count = 0;
+	count = 0;
 	do {
 		udelay(10);
-		if (!(aml_read_reg32(P_SAR_SAR_ADC_REG0) & 0x70000000))
+		if (!(aml_read_reg32(P_SAR_ADC_REG0(saradc->saradc_base_addr)) & 0x70000000))
 			break;
 		else if (++count > 10000) {
-			printf("busy error=%x\n", aml_read_reg32(P_SAR_SAR_ADC_REG0));
+			printf("busy error=%x\n", aml_read_reg32(P_SAR_ADC_REG0(saradc->saradc_base_addr)));
 			value = -1;
 			goto end;
 		}
@@ -293,20 +321,20 @@ int get_adc_sample_gxbb_early(int ch, int use_10bit_num)
 
 	count = 0;
 	sum = 0;
-	while (aml_get_reg32_bits(PP_SAR_ADC_REG0,21,5) && (count < 32)) {
-		if (aml_get_reg32_bits(PP_SAR_ADC_REG0,26,1))
+	while (aml_get_reg32_bits(P_SAR_ADC_REG0(saradc->saradc_base_addr),21,5) && (count < 32)) {
+		if (aml_get_reg32_bits(P_SAR_ADC_REG0(saradc->saradc_base_addr),26,1))
 			printf("fifo_count, but fifo empty\n");
 
-        value = aml_read_reg32(PP_SAR_ADC_FIFO_RD);
+		value = aml_read_reg32(P_SAR_ADC_FIFO_RD(saradc->saradc_base_addr));
 		if (((value>>12) & 0x07) == ch) {
 			if (use_10bit_num) {
 				//printf("10bit val~\n");
-				if (flag_12bit) {
+				if (saradc->adc_type) {
 					value &= 0xffc;
 					value >>= 2;
 				} else
 					value &= 0x3ff;
-			} else {
+		} else {
 				//printf("12bit val~\n");
 				value &= 0xfff;
 			}
@@ -316,7 +344,7 @@ int get_adc_sample_gxbb_early(int ch, int use_10bit_num)
 		}	else
 			printf("chanel error\n");
 	}
-	if (!aml_get_reg32_bits(PP_SAR_ADC_REG0,26,1))
+	if (!aml_get_reg32_bits(P_SAR_ADC_REG0(saradc->saradc_base_addr),26,1))
 		printf("fifo_count=0, but fifo not empty\n");
 	if (!count) {
 		value = -1;
@@ -326,23 +354,24 @@ int get_adc_sample_gxbb_early(int ch, int use_10bit_num)
 	//printf("before cal: %d, count=%d\n", value, count);
 	//value = saradc_get_cal_value(adc, value);
 end:
-	aml_set_reg32_bits(PP_SAR_ADC_REG0,1,14,1);
-	aml_set_reg32_bits(PP_SAR_ADC_REG0,0,0,1);
+	aml_set_reg32_bits(P_SAR_ADC_REG0(saradc->saradc_base_addr),1,14,1);
+	aml_set_reg32_bits(P_SAR_ADC_REG0(saradc->saradc_base_addr),0,0,1);
 
 	saradc_clock_switch(0);
 	saradc_clock_set(20);
 	saradc_clock_switch(1);
 
 end1:
-	aml_set_reg32_bits(PP_SAR_ADC_REG3,0,29,1);
+	aml_set_reg32_bits(P_SAR_ADC_REG3(saradc->saradc_base_addr),0,29,1);
 	//adc_state = SARADC_STATE_IDLE;
-	aml_set_reg32_bits(PP_SAR_ADC_DELAY,0,FLAG_BUSY_KERNEL_BIT,1);
+	aml_set_reg32_bits(P_SAR_ADC_DELAY(saradc->saradc_base_addr),0,FLAG_BUSY_KERNEL_BIT,1);
 	return value;
 }
 
 int get_adc_sample_gxbb(int ch)
 {
 	int val;
+
 	val = get_adc_sample_gxbb_early(ch, 1);
 	return val;
 }
@@ -350,11 +379,19 @@ int get_adc_sample_gxbb(int ch)
 int get_adc_sample_gxbb_12bit(int ch)
 {
 	int val;
+
 	val = get_adc_sample_gxbb_early(ch, 0);
 	return val;
 
 }
 
+int saradc_enable(void)
+{
+	saradc_probe();
+	saradc_power_control(1);
+	saradc_internal_cal_12bit();
+	return 0;
+}
 
 int saradc_disable(void)
 {
