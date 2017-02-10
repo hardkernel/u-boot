09d420de9f5d1b7f4e374b69cf48e655406f02b5
Joy Cho <joy.cho@hardkernel.com>
2017-02-10 15:21:34 +0900

    ODROID-C2: Clear osd layer during initial boot process
    Change-Id: Ia155a075f6a678667cc661c7adc20bb0a867839b

--- common/cmd_showlogo.c
+++ common/cmd_showlogo.c
@@ -62,6 +62,11 @@ static int display_logo(const char* mode, const char* bmp_width, const char* bmp
 	int ret = 0;
 	int i = 0;
 
+	if (!strcmp("custombuilt", mode)) {
+		run_command("osd clear", 0);
+		return 1;
+	}
+
 	for (i = 0; i < ARRAY_SIZE(c2_res_list); ++i) {
 		if (!strcmp(c2_res_list[i].name, mode)) {
 			setenv("display_width", simple_itoa(c2_res_list[i].display_width));
