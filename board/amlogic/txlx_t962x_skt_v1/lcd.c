57e439faa7f691b4e66c93b3facfa1074307665b
Evoke Zhang <evoke.zhang@amlogic.com>
2017-03-16 13:53:04 +0800

    PD#139181: lcd: update lcd.c for txlx socket
    Change-Id: I32f7890f8be24956d0e05f78585631ffed5f707d
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- board/amlogic/txlx_t962x_skt_v1/lcd.c
+++ board/amlogic/txlx_t962x_skt_v1/lcd.c
@@ -35,18 +35,6 @@ static struct lcd_power_step_s lcd_power_off_step[] = {
 	//{LCD_POWER_TYPE_CPU,   0,0,100,}, /* power off */
 	{LCD_POWER_TYPE_MAX,   0,0,0,},   /* ending flag */
 };
-static struct lcd_power_step_s lcd_power_on_step_3d_disable[] = {
-	//{LCD_POWER_TYPE_CPU,   0,1,20,}, /* power on */
-	//{LCD_POWER_TYPE_CPU,   1,0,10,}, /* 3d_disable */
-	{LCD_POWER_TYPE_SIGNAL,0,0,0,},  /* signal */
-	{LCD_POWER_TYPE_MAX,   0,0,0,},  /* ending flag */
-};
-static struct lcd_power_step_s lcd_power_off_step_3d_disable[] = {
-	{LCD_POWER_TYPE_SIGNAL,0,0,30,},  /* signal */
-	//{LCD_POWER_TYPE_CPU,   1,2,0,},   /* 3d_disable */
-	//{LCD_POWER_TYPE_CPU,   0,0,100,}, /* power off */
-	{LCD_POWER_TYPE_MAX,   0,0,0,},   /* ending flag */
-};
 
 static char lcd_bl_gpio[BL_GPIO_NUM_MAX][LCD_CPU_GPIO_NAME_MAX] = {
 	//"GPIOAO_4",
@@ -78,14 +66,14 @@ struct ext_lcd_config_s ext_lcd_config[LCD_NUM_MAX] = {
 	/* basic timing */
 	3840,2160,4400,2250,33,477,0,6,81,0,
 	/* clk_attr */
-	0,0,1,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
+	2,0,1,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
 	/* vbyone_attr */
 	8,2,4,4,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
 	/* power step */
 	lcd_power_on_step, lcd_power_off_step,
 	/* backlight */
 	60,255,10,128,128,
-	BL_CTRL_PWM,0,1,0,200,200,
+	BL_CTRL_MAX,0,1,0,200,200,
 	BL_PWM_POSITIVE,BL_PWM_B,180,100,25,1,0,
 	Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
 	Rsv_val,Rsv_val,Rsv_val,Rsv_val,
@@ -98,48 +86,12 @@ struct ext_lcd_config_s ext_lcd_config[LCD_NUM_MAX] = {
 	/* clk_attr */
 	2,0,1,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
 	/* vbyone_attr */
-	8,2,4,4,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
-	/* power step */
-	lcd_power_on_step, lcd_power_off_step,
-	/* backlight */
-	60,255,10,128,128,
-	BL_CTRL_PWM,0,1,0,200,200,
-	BL_PWM_POSITIVE,BL_PWM_B,180,100,25,1,0,
-	Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
-	Rsv_val,Rsv_val,Rsv_val,Rsv_val,
-	10,10,Rsv_val},
-
-	{/*INL: V580DJ2: 3840x2160@60hz 8lane */
-	"vbyone_2",LCD_VBYONE,10,
-	/* basic timing */
-	3840,2160,4400,2250,33,477,0,6,81,0,
-	/* clk_attr */
-	2,0,1,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
-	/* vbyone_attr */
 	8,1,4,4,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
 	/* power step */
 	lcd_power_on_step, lcd_power_off_step,
 	/* backlight */
 	60,255,10,128,128,
-	BL_CTRL_PWM,0,1,0,200,200,
-	BL_PWM_POSITIVE,BL_PWM_B,180,100,25,1,0,
-	Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
-	Rsv_val,Rsv_val,Rsv_val,Rsv_val,
-	10,10,Rsv_val},
-
-	{/*BOE: HV550QU2: 3840x2160@60hz 8lane */
-	"vbyone_3",LCD_VBYONE,10,
-	/* basic timing */
-	3840,2160,4400,2250,33,477,1,6,81,0,
-	/* clk_attr */
-	2,0,1,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
-	/* vbyone_attr */
-	8,2,4,4,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
-	/* power step */
-	lcd_power_on_step_3d_disable, lcd_power_off_step_3d_disable,
-	/* backlight */
-	60,255,10,128,128,
-	BL_CTRL_PWM,0,1,0,200,200,
+	BL_CTRL_MAX,0,1,0,200,200,
 	BL_PWM_POSITIVE,BL_PWM_B,180,100,25,1,0,
 	Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,Rsv_val,
 	Rsv_val,Rsv_val,Rsv_val,Rsv_val,
