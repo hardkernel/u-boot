7c073b2912f19f8b3759fe74a3dd9c20df67d12a
Jian Hu <jian.hu@amlogic.com>
2017-05-04 13:27:49 +0800

    PD#139181: txlx: add display configuration for r321
    1.open hdmitx and close lcd macro
    2.fix r321 Kconfig errors
    
    Change-Id: Ic2173565058768c7e452bd88d5d97382df1197e6
    Signed-off-by: Jian Hu <jian.hu@amlogic.com>

--- board/amlogic/configs/txlx_t962e_r321_v1.h
+++ board/amlogic/configs/txlx_t962e_r321_v1.h
@@ -317,7 +317,7 @@
 #define CONFIG_AML_VPU 1
 
 /* DISPLAY & HDMITX */
-//#define CONFIG_AML_HDMITX20 1
+#define CONFIG_AML_HDMITX20 1
 #define CONFIG_AML_CANVAS 1
 #define CONFIG_AML_VOUT 1
 #define CONFIG_AML_OSD 1
@@ -325,12 +325,12 @@
 #define CONFIG_CMD_BMP 1
 
 #if defined(CONFIG_AML_VOUT)
-//#define CONFIG_AML_CVBS 1
+#define CONFIG_AML_CVBS 1
 #endif
 
-#define CONFIG_AML_LCD    1
-#define CONFIG_AML_LCD_TV 1
-#define CONFIG_AML_LCD_TABLET 1
+//#define CONFIG_AML_LCD    1
+//#define CONFIG_AML_LCD_TV 1
+//#define CONFIG_AML_LCD_TABLET 1
 
 /* USB
  * Enable CONFIG_MUSB_HCD for Host functionalities MSC, keyboard
