0fa4df45d507045600d6d6ec32dd3aac12471824
Long Yu <long.yu@amlogic.com>
2017-05-14 21:54:27 +0800

    PD#142470: emmc: support emmc for axg
    Merge to trunk:
    conflict drivers/mmc/aml_sd_emmc.c, use trunk code, add axg support
    
    Change-Id: Ibf085c042116f3db18952639dc628a2aa3d3eed9

--- drivers/mmc/aml_sd_emmc.c
+++ drivers/mmc/aml_sd_emmc.c
@@ -119,7 +119,7 @@ void aml_sd_cfg_swth(struct mmc *mmc)
 						(clk_src << Cfg_src) |
 						(clk_div << Cfg_div));
 
-	if (cpu_id.family_id == MESON_CPU_MAJOR_ID_TXLX) {
+	if (cpu_id.family_id >= MESON_CPU_MAJOR_ID_TXLX) {
 		if (aml_is_emmc_tsd(mmc)) {
 			sd_emmc_clkc &= ~(3 << Cfg_co_phase);
 			sd_emmc_clkc |= (3 << Cfg_co_phase);
