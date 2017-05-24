cd79ab044d69f6a947b84e405ccb6ddf48c83864
Haixiang Bao <haixiang.bao@amlogic.com>
2017-05-24 11:09:27 +0800

    [UBOOT][AXG]: support secure boot (v3 only)(20170524)
    1. This reverts commit
    a39b48de95bdb13ef607906df4c4a2bd0b91f4f7.
    
    2. update BL2 with multi-copy support
    commit 93e571ca6415da792566b9ce6bc14ba05d09830d
    Author: Yonghui Yu <yonghui.yu@amlogic.com>
    Date: Wed May 24 10:27:36 2017 +0800
    PD#142470: storage: enable robust boot for fip-v3
    
    3. verify pass with S420 board for multi-copy feature
    
    Change-Id: I4a1f44e31316328828b71c5ee7a79f8fcf15d71e
    Signed-off-by: Haixiang Bao <haixiang.bao@amlogic.com>

--- arch/arm/include/asm/arch-axg/cpu.h
+++ arch/arm/include/asm/arch-axg/cpu.h
@@ -69,4 +69,8 @@
 #define CONFIG_AML_BL32_COMPRESS_DISABLE 1
 #define CONFIG_AML_BL33_COMPRESS_DISABLE 1
 
+
+//2017.05.19 AXG only support v3 format for normal & secure boot
+#define CONFIG_AML_SECURE_BOOT_V3 1
+
 #endif /* _CPU_H */
