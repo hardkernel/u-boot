d1afae36a32b6b733bfa52bfe9ca2127c205dd3e
Joseph Chen <chenjh@rock-chips.com>
2017-09-12 11:31:04 +0800

    rk3288: arch_cpu_init: add some settings
    1. disable lvds phy;
    2. select EDP clock source 24M;
    3. set read latency;
    4. set vop qos to highest priority.
    
    Change-Id: I374d4717b721b10d1d6eb20ad6ac5fbea7c7b467
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- arch/arm/mach-rockchip/rk3288/rk3288.c
+++ arch/arm/mach-rockchip/rk3288/rk3288.c
@@ -7,6 +7,18 @@
 #include <asm/arch/hardware.h>
 
 #define GRF_SOC_CON2 0xff77024c
+#define GRF_SOC_CON7 0xff770260
+
+#define VIO0_VOP_QOS_BASE	0xffad0408
+#define VIO1_VOP_QOS_BASE	0xffad0008
+#define BUS_MSCH0_QOS_BASE	0xffac0014
+#define BUS_MSCH1_QOS_BASE	0xffac0094
+
+#define READLATENCY_VAL		0x34
+#define CRU_CLKSEL_CON28	0Xff7600d0
+
+#define CPU_AXI_QOS_PRIORITY_LEVEL(h, l) \
+	((((h) & 3) << 8) | (((h) & 3) << 2) | ((l) & 3))
 
 int arch_cpu_init(void)
 {
@@ -14,6 +26,20 @@ int arch_cpu_init(void)
 
 	/* Use rkpwm by default */
 	rk_setreg(GRF_SOC_CON2, 1 << 0);
+
+	/* Disable LVDS phy */
+	rk_setreg(GRF_SOC_CON7, 1 << 15);
+
+	/* Select EDP clock source 24M */
+	rk_setreg(CRU_CLKSEL_CON28, 1 << 15);
+
+	/* Read latency configure */
+	writel(READLATENCY_VAL, BUS_MSCH0_QOS_BASE);
+	writel(READLATENCY_VAL, BUS_MSCH1_QOS_BASE);
+
+	/* Set vop qos to highest priority */
+	writel(CPU_AXI_QOS_PRIORITY_LEVEL(2, 2), VIO0_VOP_QOS_BASE);
+	writel(CPU_AXI_QOS_PRIORITY_LEVEL(2, 2), VIO1_VOP_QOS_BASE);
 
 	return 0;
 }
