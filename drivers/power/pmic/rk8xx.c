6a367335743118ba7c38d91108c40043ed123397
Joseph Chen <chenjh@rock-chips.com>
2017-09-18 20:22:29 +0800

    power: pmic: rk8xx: bind rk818 fuel gauge
    Change-Id: Iad4d6f66d48e824a2020ba78fa81e30cf52230ca
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- drivers/power/pmic/rk8xx.c
+++ drivers/power/pmic/rk8xx.c
@@ -27,6 +27,11 @@ static const struct pmic_child_info power_key_info[] = {
 	{ },
 };
 
+static const struct pmic_child_info fuel_gauge_info[] = {
+	{ .prefix = "battery", .driver = "rk818_fg"},
+	{ },
+};
+
 static int rk8xx_reg_count(struct udevice *dev)
 {
 	return RK808_NUM_OF_REGS;
@@ -79,6 +84,10 @@ static int rk8xx_bind(struct udevice *dev)
 		debug("%s: %s - no child found\n", __func__, dev->name);
 
 	children = pmic_bind_children(dev, dev->node, power_key_info);
+	if (!children)
+		debug("%s: %s - no child found\n", __func__, dev->name);
+
+	children = pmic_bind_children(dev, dev->node, fuel_gauge_info);
 	if (!children)
 		debug("%s: %s - no child found\n", __func__, dev->name);
 
