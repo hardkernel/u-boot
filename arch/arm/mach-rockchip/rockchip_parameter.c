fb11f32b79b7fcbac77e4d9fddb18aa94b37ecad
Andy Yan <andy.yan@rock-chips.com>
2017-09-18 20:28:14 +0800

    rockchip: add support to parse partition from rockchip parameter
    This patch add support to parse block device partiton from
    rockchip parameter.
    
    Change-Id: I563211ba58cf333ae4a732dfa5432f30b8587c61
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

---
+++ arch/arm/mach-rockchip/rockchip_parameter.c
@@ -0,0 +1,104 @@
+/*
+ * (C) Copyright 2017 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#include <common.h>
+#include <malloc.h>
+#include "rockchip_parameter.h"
+#include "rockchip_blk.h"
+
+#define MAX_PARAM_SIZE			(1024 * 64)
+
+struct rockchip_param {
+	u32 tag;
+	u32 length;
+	char params[1];
+	u32 crc;
+};
+
+static LIST_HEAD(parts_head);
+
+static int rockchip_param_parse(char *param)
+{
+	struct blk_part *part;
+	const char *cmdline = strstr(param, "CMDLINE:");
+	const char *blkdev_parts = strstr(cmdline, "mtdparts");
+	const char *blkdev_def = strchr(blkdev_parts, ':') + 1;
+	char *next = (char *)blkdev_def;
+	char *pend;
+	int len;
+	unsigned long size, from;
+
+	debug("%s", cmdline);
+
+	env_set("bootargs", cmdline);
+
+	while (*next) {
+		if (*next == '-') {
+			size = (~0UL);
+			next++;
+		} else {
+			size = simple_strtoul(next, &next, 16);
+		}
+		next++;
+		from = simple_strtoul(next, &next, 16);
+		next++;
+		pend =  strchr(next, ')');
+		if (!pend)
+			break;
+		len = min_t(int, pend - next, PART_NAME_SIZE);
+		part = malloc(sizeof(*part));
+		if (!part) {
+			printf("out of memory\n");
+			break;
+		}
+		part->from = from + RK_BLK_OFFSET;
+		part->size = size;
+		strncpy(part->name, next, len);
+		part->name[len] = '\0';
+		next = strchr(next, ',');
+		next++;
+		list_add_tail(&part->node, &parts_head);
+		debug("0x%lx@0x%lx(%s)\n", part->size, part->from, part->name);
+	}
+
+	return 0;
+}
+
+static int rockchip_init_param(void)
+{
+	struct rockchip_param *param;
+
+	param = memalign(ARCH_DMA_MINALIGN, MAX_PARAM_SIZE);
+	if (!param) {
+		printf("out of memory\n");
+		return -ENOMEM;
+	}
+
+	blkdev_read(param, RK_BLK_OFFSET, MAX_PARAM_SIZE >> 9);
+
+	rockchip_param_parse(param->params);
+
+	return 0;
+}
+
+struct blk_part *rockchip_get_blk_part(const char *name)
+{
+	struct blk_part *part;
+	struct list_head *node;
+
+	if (list_empty(&parts_head))
+		rockchip_init_param();
+
+	list_for_each(node, &parts_head) {
+		part = list_entry(node, struct blk_part, node);
+		if (!strcmp(part->name, name))
+			return part;
+	}
+
+	return NULL;
+}
+
+
---
+++ arch/arm/mach-rockchip/rockchip_parameter.h
@@ -0,0 +1,25 @@
+/*
+ * (C) Copyright 2017 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef _ROCKCHIP_PARAMS_H_
+#define _ROCKCHIP_PARAMS_H_
+
+#include <linux/list.h>
+
+#define RK_BLK_OFFSET			0x2000
+#define PART_NAME_SIZE			32
+#define RK_BLK_SIZE			(1 << 9)
+
+struct blk_part {
+	char name[PART_NAME_SIZE];
+	unsigned long from;
+	unsigned long size;
+	struct list_head node;
+};
+
+struct blk_part *rockchip_get_blk_part(const char *name);
+
+#endif
