1ccc17a74d7a8097cb0c501a70c401947e20ae1f
Wu Liang feng <wulf@rock-chips.com>
2017-09-29 14:22:27 +0800

    include: usb: add phy rockchip inno usb2 header file
    The rockchip USB 2.0 PHY can support USB BC1.2, but for the
    time being, the framework doesn't provide a method to get
    the USB charger type for the power module, so this header
    file provides an rockchip_chg_get_type() API to get the USB
    charger type.
    
    Change-Id: I2076981e75fac7b129b17188be49188693294ef1
    Signed-off-by: Wu Liang feng <wulf@rock-chips.com>

---
+++ include/linux/usb/phy-rockchip-inno-usb2.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2017 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier:    GPL-2.0+
+ */
+
+#ifndef _PHY_ROCKCHIP_INNO_USB2_H
+#define _PHY_ROCKCHIP_INNO_USB2_H
+
+extern int rockchip_chg_get_type(void);
+
+#endif /* _PHY_ROCKCHIP_INNO_USB2_H */
