5cf14c032d4296e96a5c04adda07a1c63c8ed027
Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
2017-09-29 19:27:55 +0200

    rockchip: bootrom: add definitions to retrieve BROM boot-source
    The Rockchip BROM allows reading where it booted from from SRAM.
    This adds the necessary definitions (as received from Kever) for
    the location of this information in the RK3399's SRAM and naming
    for the constants used.
    
    Change-Id: Iac090fa74837959ffc4bb42c75328016ee671be1
    Signed-off-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>
    (cherry picked from commit 3b4f87735d093463e3b227a07a62996fc0b47b53)

--- arch/arm/include/asm/arch-rockchip/bootrom.h
+++ arch/arm/include/asm/arch-rockchip/bootrom.h
@@ -39,4 +39,23 @@ enum rockchip_bootrom_cmd {
 };
 
 void back_to_bootrom(enum rockchip_bootrom_cmd brom_cmd);
+
+/**
+ * Boot-device identifiers as used by the BROM
+ */
+enum {
+	BROM_BOOTSOURCE_NAND = 1,
+	BROM_BOOTSOURCE_EMMC = 2,
+	BROM_BOOTSOURCE_SPINOR = 3,
+	BROM_BOOTSOURCE_SPINAND = 4,
+	BROM_BOOTSOURCE_SD = 5,
+	BROM_BOOTSOURCE_USB = 10,
+	BROM_LAST_BOOTSOURCE = BROM_BOOTSOURCE_USB
+};
+
+/**
+ * Locations of the boot-device identifier in SRAM
+ */
+#define RK3399_BROM_BOOTSOURCE_ID_ADDR   0xff8c0010
+
 #endif
