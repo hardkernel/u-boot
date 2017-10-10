38242c3bc847a43dc36b960425b4526257f1f737
Joy Cho <joy.cho@hardkernel.com>
2017-10-10 15:35:29 +0900

    Set default boot parameters for ODROID-N1
    Change-Id: I774dc891151f54b548ff1e6461966713e0e09b0e

--- include/configs/odroidn1.h
+++ include/configs/odroidn1.h
@@ -34,4 +34,12 @@
 #define CONFIG_SUPPORT_RAW_INITRD
 #define CONFIG_ENV_VARS_UBOOT_CONFIG
 
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"fdt_addr_r=0x01f00000\0" \
+	"kernel_addr_r=0x02000000\0" \
+	"ramdisk_addr_r=0x04000000\0" \
+	"bootargs=earlycon=uart8250,mmio32,0xff1a0000 swiotlb=1 androidboot.baseband=N/A androidboot.selinux=permissive androidboot.hardware=odroidn1 androidboot.console=ttyFIQ0 init=/init\0" \
+	"bootcmd=cfgload; fatload mmc 0 ${kernel_addr} Image; fatload mmc 0 ${ramdisk_addr_r} ramdisk.img; fatload mmc 0 ${fdt_addr_r} rk3399-odroidn1-rev0.dtb; booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}\0"
+
 #endif
