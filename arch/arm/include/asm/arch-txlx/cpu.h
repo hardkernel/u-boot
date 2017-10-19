2cfb2580cab7d97a92bba1e58973c3534fd074f1
Haixiang Bao <haixiang.bao@amlogic.com>
2017-10-20 07:54:57 +0800

    [BL33][TXLX]: v3 secure boot work (3/3)
    1. TXLX only support v3 format and not support v2 any more
    
    2. secure & normal boot work (SD boot)
    
    3. usb boot for above work
    
    4. bl2 commit:
       commit 44c8c44a6890dd8c779d49d989137555933a58e7
       Author: Haixiang Bao <haixiang.bao@amlogic.com>
       Date:   Thu Oct 19 15:01:59 2017 +0800
        [BL2][TXLX]: v3 secure boot work (1/3)
    
    5. bl31 commit:
       commit 1e6adc8bd720ebbd5b21d314f0f3c06a30d0e4df
       Author: Haixiang Bao <haixiang.bao@amlogic.com>
       Date:   Thu Oct 19 11:05:03 2017 +0800
        [BL31][v1.3][TXLX]: v3 secure boot work (2/3)
    
    Signed-off-by: Haixiang Bao <haixiang.bao@amlogic.com>
    
    Conflicts:
    	Makefile
    	fip/txlx/bl2.bin
    	fip/txlx/bl31.img
    
    Change-Id: Iefc575c0f678b5b848fcbf5065d237d364ff3e8e

--- arch/arm/include/asm/arch-txlx/cpu.h
+++ arch/arm/include/asm/arch-txlx/cpu.h
@@ -65,4 +65,7 @@
 //2017.05.11 new compress solution, only support BL33 LZ4 compress
 #define CONFIG_AML_BL33_COMPRESS_ENABLE 1
 
+//2017.10.19 TXLX only support v3 format for normal & secure boot
+#define CONFIG_AML_SECURE_BOOT_V3 1
+
 #endif /* _CPU_H */
