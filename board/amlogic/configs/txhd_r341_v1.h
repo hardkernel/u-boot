287705fac7854d01d2c121872906cfce486c6b8b
Zhongfu Luo <zhongfu.luo@amlogic.com>
2017-11-01 16:23:24 +0800

    PD#151264: txhd: change vddee for txhd
    The value will be updated after vccktest.
    
    Change-Id: I9d7e384cb8bde7e2ccd380e6e16e32a175b3b97c
    Signed-off-by: Bo Yang <bo.yang@amlogic.com>

--- board/amlogic/configs/txhd_r341_v1.h
+++ board/amlogic/configs/txhd_r341_v1.h
@@ -34,8 +34,8 @@
  * platform power init config
  */
 #define CONFIG_PLATFORM_POWER_INIT
-#define CONFIG_VCCK_INIT_VOLTAGE	1100
-#define CONFIG_VDDEE_INIT_VOLTAGE	950		// voltage for power up
+#define CONFIG_VCCK_INIT_VOLTAGE	1050
+#define CONFIG_VDDEE_INIT_VOLTAGE	1050		// voltage for power up
 #define CONFIG_VDDEE_SLEEP_VOLTAGE	 850		// voltage for suspend
 
 /* configs for CEC */
