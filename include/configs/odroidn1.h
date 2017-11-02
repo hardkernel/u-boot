b75300c0dfabc4266c2cbc190f26ea036291ea28
Joy Cho <joy.cho@hardkernel.com>
2017-11-02 10:34:00 +0900

    Revert "Fix a redundant setting error of storagemedia in bootargs"
    This reverts commit 9224015f810b681344b2875534ec315be57b9f0c.
    
    Change-Id: I4b6d51a00847b53036bbb3ef68c8b12a15ff0982

--- include/configs/odroidn1.h
+++ include/configs/odroidn1.h
@@ -46,10 +46,9 @@
 	"fdt_addr_r=0x04800000\0" \
 	"kernel_addr_r=0x02000000\0" \
 	"ramdisk_addr_r=0x04000000\0" \
-	"storagemedia=na\0" \
 	"bootargs=earlycon=uart8250,mmio32,0xff1a0000 swiotlb=1 "ANDROID_OPT	\
 		" root=/dev/mmcblk0p2 rw rootfstype=ext4 init=/init "	\
-		MTDPARTS_DOS" SecureBootCheckOk=0 storagemedia=${storagemedia}\0"	\
+		MTDPARTS_DOS" SecureBootCheckOk=0\0"	\
 	"bootcmd=cfgload; mmc dev ${bootdev}; movi read kernel 0 ${kernel_addr_r}; "	\
 		"movi read dtb 0 ${fdt_addr_r}; "	\
 		"booti ${kernel_addr_r} - ${fdt_addr_r}\0"
