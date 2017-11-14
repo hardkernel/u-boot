0cd6d3b96ff337995ff290b22e24325c43229bad
Joseph Chen <chenjh@rock-chips.com>
2017-11-14 21:07:53 +0800

    dm: charge display: add interface to get and update fdt parameter
    Change-Id: Ie2ab5c8c68fb629fd7628310f8a67084ef751f21
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- drivers/power/charge-display-uclass.c
+++ drivers/power/charge-display-uclass.c
@@ -29,6 +29,16 @@ int charge_display_get_power_on_voltage(struct udevice *dev)
 	return ops->get_power_on_voltage(dev);
 }
 
+int charge_display_get_screen_on_voltage(struct udevice *dev)
+{
+	const struct dm_charge_display_ops *ops = dev_get_driver_ops(dev);
+
+	if (!ops || !ops->get_screen_on_voltage)
+		return -ENOSYS;
+
+	return ops->get_screen_on_voltage(dev);
+}
+
 int charge_display_show(struct udevice *dev)
 {
 	const struct dm_charge_display_ops *ops = dev_get_driver_ops(dev);
@@ -37,6 +47,36 @@ int charge_display_show(struct udevice *dev)
 		return -ENOSYS;
 
 	return ops->show(dev);
+}
+
+int charge_display_set_power_on_soc(struct udevice *dev, int val)
+{
+	const struct dm_charge_display_ops *ops = dev_get_driver_ops(dev);
+
+	if (!ops || !ops->set_power_on_soc)
+		return -ENOSYS;
+
+	return ops->set_power_on_soc(dev, val);
+}
+
+int charge_display_set_power_on_voltage(struct udevice *dev, int val)
+{
+	const struct dm_charge_display_ops *ops = dev_get_driver_ops(dev);
+
+	if (!ops || !ops->set_power_on_voltage)
+		return -ENOSYS;
+
+	return ops->set_power_on_voltage(dev, val);
+}
+
+int charge_display_set_screen_on_voltage(struct udevice *dev, int val)
+{
+	const struct dm_charge_display_ops *ops = dev_get_driver_ops(dev);
+
+	if (!ops || !ops->set_screen_on_voltage)
+		return -ENOSYS;
+
+	return ops->set_screen_on_voltage(dev, val);
 }
 
 UCLASS_DRIVER(charge_display) = {
--- include/power/charge_display.h
+++ include/power/charge_display.h
@@ -10,11 +10,20 @@
 struct dm_charge_display_ops {
 	int (*get_power_on_soc)(struct udevice *dev);
 	int (*get_power_on_voltage)(struct udevice *dev);
+	int (*get_screen_on_voltage)(struct udevice *dev);
+	int (*set_power_on_soc)(struct udevice *dev, int val);
+	int (*set_power_on_voltage)(struct udevice *dev, int val);
+	int (*set_screen_on_voltage)(struct udevice *dev, int val);
 	int (*show)(struct udevice *dev);
 };
 
 int charge_display_get_power_on_soc(struct udevice *dev);
 int charge_display_get_power_on_voltage(struct udevice *dev);
+int charge_display_get_screen_on_voltage(struct udevice *dev);
+int charge_display_set_power_on_soc(struct udevice *dev, int val);
+int charge_display_set_power_on_voltage(struct udevice *dev, int val);
+int charge_display_set_screen_on_voltage(struct udevice *dev, int val);
+
 int charge_display_show(struct udevice *dev);
 
 #endif
