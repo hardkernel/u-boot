e1ec45d2ebd447dc16525156f6c03d89d05b7880
Joseph Chen <chenjh@rock-chips.com>
2017-11-29 17:18:36 +0800

    rk312x: add arch_cpu_init implementation
    1. set read latency configure;
    2. set lcdc cpu axi qos priority level;
    3. set GPIO1_C1 iomux to gpio, default sdcard_detn;
    4. disable interrupt of rk3126.
    
    Change-Id: Iebd980e68f4a9fccbf8d620bac0f103571e1d4de
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- arch/arm/mach-rockchip/rk3128/rk3128.c
+++ arch/arm/mach-rockchip/rk3128/rk3128.c
@@ -3,10 +3,37 @@
  *
  * SPDX-License-Identifier:     GPL-2.0+
  */
+#include <asm/io.h>
+#include <asm/arch/hardware.h>
+
+#define GRF_GPIO1C_IOMUX		0x200080c0
+#define SDMMC_INTMASK			0x10214024
+#define READLATENCY_VAL			0x3f
+#define BUS_MSCH_QOS_BASE		0x10128014
+#define	CPU_AXI_QOS_PRIORITY_BASE	0x1012f188
+#define CPU_AXI_QOS_PRIORITY_LEVEL(h, l) \
+	((((h) & 3) << 8) | (((h) & 3) << 2) | ((l) & 3))
 
 int arch_cpu_init(void)
 {
 	/* We do some SoC one time setting here. */
+
+	/* Read latency configure */
+	writel(READLATENCY_VAL, BUS_MSCH_QOS_BASE);
+
+	/* Set lcdc cpu axi qos priority level */
+	writel(CPU_AXI_QOS_PRIORITY_LEVEL(3, 3), CPU_AXI_QOS_PRIORITY_BASE);
+
+	/* Set GPIO1_C1 iomux to gpio, default sdcard_detn */
+	writel(0x00040000, GRF_GPIO1C_IOMUX);
+
+#ifdef CONFIG_ROCKCHIP_RK3126
+	/*
+	 * Disable interrupt, otherwise it always generates wakeup signal. This
+	 * is an IC hardware issue.
+	 */
+	writel(0, SDMMC_INTMASK);
+#endif
 
 	return 0;
 }
