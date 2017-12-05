cba37fa7b5759c8f2ca1d0ec3c6951cf6190eece
Evoke Zhang <evoke.zhang@amlogic.com>
2017-12-05 10:28:52 +0800

    PD#154260: txlx_r321: add missed hdmimode & cvbsmode for bootargs
    Change-Id: If4d762cbe575016ed714d39c663d7788da1970dc
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- board/amlogic/configs/txlx_t962e_r321_v1.h
+++ board/amlogic/configs/txlx_t962e_r321_v1.h
@@ -122,7 +122,7 @@
             "else fi;"\
             "\0"\
         "storeargs="\
-            "setenv bootargs ${initargs} logo=${display_layer},loaded,${fb_addr} vout=${outputmode},enable panel_type=${panel_type} osd_reverse=${osd_reverse} video_reverse=${video_reverse} androidboot.selinux=${EnableSelinux} androidboot.firstboot=${firstboot} jtag=${jtag}; "\
+            "setenv bootargs ${initargs} logo=${display_layer},loaded,${fb_addr} vout=${outputmode},enable panel_type=${panel_type} osd_reverse=${osd_reverse} video_reverse=${video_reverse} hdmimode=${hdmimode} cvbsmode=${cvbsmode} androidboot.selinux=${EnableSelinux} androidboot.firstboot=${firstboot} jtag=${jtag}; "\
             "setenv bootargs ${bootargs} page_trace=${page_trace};" \
 	"setenv bootargs ${bootargs} androidboot.hardware=amlogic;"\
             "run cmdline_keys;"\
