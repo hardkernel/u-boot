a0e58cf2cc5f85a68cc478474f369913a3320283
Kever Yang <kever.yang@rock-chips.com>
2017-12-07 15:02:05 +0800

    cmd: bootrkp: add a cmd for rockchip rkimg bootloader
    'bootrkp' will find the boot device and find all necessary images.
    Basically, these partitions are must:
    Kernel.img: kernel Image
    Resource.img: kernel dtb and logo
    system.img: rootfs
    
    Change-Id: Iba3df2535705fa3ed529377f44f041fa2294ce60
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

---
+++ cmd/bootrkp.c
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 2017 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#include <common.h>
+#include <android_bootloader.h>
+#include <boot_rkimg.h>
+
+static int do_boot_rockchip(cmd_tbl_t *cmdtp, int flag, int argc,
+		      char * const argv[])
+{
+	disk_partition_t part_info;
+	struct blk_desc *dev_desc;
+	int mode = 0;
+	char *boot_partname = PART_BOOT;
+	int ret = 0;
+
+	dev_desc = rockchip_get_bootdev();
+	mode = rockchip_get_boot_mode();
+	if (mode == BOOT_MODE_RECOVERY)
+		boot_partname = PART_RECOVERY;
+	ret = part_get_info_by_name(dev_desc, boot_partname, &part_info);
+
+	if(boot_rockchip_image(dev_desc, &part_info))
+		ret = CMD_RET_FAILURE;
+
+	return ret;
+}
+
+U_BOOT_CMD(
+	boot_rockchip,  CONFIG_SYS_MAXARGS,     1,      do_boot_rockchip,
+	"boot Linux Image image from rockchip partition storage",
+	""
+);
