271d85e0a8f3877563c0c7e9403810d2cafd20c7
Sam Wu <yihui.wu@amlogic.com>
2017-12-16 13:05:47 +0800

    [FactoryBurn]fix cannot find dtb when programming keys by usb boot
    PD# 157042
    
    1, replace dtb.img decrypting address from  to kernel ,
       if using  for decrytping when Multiple dtbs in dtb.img,
       then key manager driver would fail to find dtb in .
    
    Change-Id: Ib1673af68a4519472a2fefc2cd549fbaaa590466

--- common/partitions.c
+++ common/partitions.c
@@ -58,7 +58,7 @@ int check_valid_dts(unsigned char *buffer)
 	int ret = -__LINE__;
 	char *dt_addr;
 	/* fixme, a work around way */
-	unsigned char *sbuffer = (unsigned char *)0x1000000;
+	unsigned char *sbuffer = (unsigned char *)getenv_hex("loadaddr", CONFIG_DTB_MEM_ADDR + 0x100000);
 
 	if (is_dtb_encrypt(buffer)) {
 		memcpy(sbuffer, buffer, AML_DTB_IMG_MAX_SZ);
