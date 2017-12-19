cdfcfbcc927a7d1e7047774c4de2c7c6b029369b
David Wu <david.wu@rock-chips.com>
2017-12-19 20:54:15 +0800

    rockchip: rk3036: Enable tpl and optee support
    Move original spl to tpl, and add spl to load next stage firmware,
    adapt all the address and option for them. Then Enable all the
    options for TPL/SPL and OPTEE.
    
    Change-Id: I44568d84984ca2f4b019fc85e80fbcbaba16fed4
    Signed-off-by: David Wu <david.wu@rock-chips.com>

--- include/configs/kylin_rk3036.h
+++ include/configs/kylin_rk3036.h
@@ -10,11 +10,33 @@
 #include <linux/sizes.h>
 #include <configs/rk3036_common.h>
 
+/* Store env in emmc */
+#define CONFIG_SYS_MMC_ENV_DEV          0
+#define CONFIG_SYS_MMC_ENV_PART         0
+#define CONFIG_SYS_REDUNDAND_ENVIRONMENT
+#define CONFIG_SUPPORT_EMMC_RPMB
+
 #ifndef CONFIG_SPL_BUILD
+/* Enable gpt partition table */
+#undef CONFIG_PREBOOT
+#define CONFIG_PREBOOT \
+	"mmc dev 0; " \
+	"gpt guid mmc 0; " \
+	"if test $? = 1; then " \
+		"fastboot usb 0; " \
+	"fi; "
+
+#define CONFIG_SYS_BOOT_RAMDISK_HIGH
 
 /* Store env in emmc */
 #define CONFIG_SYS_MMC_ENV_DEV		0 /* emmc */
 #define CONFIG_SYS_MMC_ENV_PART		0 /* user area */
+
+/* Enable atags */
+#define CONFIG_SYS_BOOTPARAMS_LEN	(64*1024)
+#define CONFIG_INITRD_TAG
+#define CONFIG_SETUP_MEMORY_TAGS
+#define CONFIG_CMDLINE_TAG
 
 #endif
 
