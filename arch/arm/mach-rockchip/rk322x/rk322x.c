c11c067d9619ad1160428bab35e35965c5155021
Kever Yang <kever.yang@rock-chips.com>
2018-01-20 21:59:52 +0800

    rockchip: rk322x: use common board file
    Change-Id: If3ac0d99a96a784443e16112362fd54576a70c00
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- arch/arm/mach-rockchip/rk322x/rk322x.c
+++ arch/arm/mach-rockchip/rk322x/rk322x.c
@@ -4,29 +4,61 @@
  * SPDX-License-Identifier:     GPL-2.0+
  */
 #include <asm/io.h>
+#include <asm/arch/bootrom.h>
 #include <asm/arch/hardware.h>
+#include <asm/arch/grf_rk322x.h>
 
-#define GRF_SOC_CON2	0x11000408
-#define GRF_CON_IOMUX	0x11000050
+#define GRF_BASE	0x11000000
 #define CRU_MISC_CON	0x110e0134
+#define SGRF_DDR_CON0	0x10150000
 
+const char * const boot_devices[BROM_LAST_BOOTSOURCE + 1] = {
+	[BROM_BOOTSOURCE_EMMC] = "/sdhci@fe330000",
+	[BROM_BOOTSOURCE_SD] = "/dwmmc@fe320000",
+};
+#ifdef CONFIG_SPL_BUILD
 int arch_cpu_init(void)
 {
+	static struct rk322x_grf * const grf = (void *)GRF_BASE;
 	/* We do some SoC one time setting here. */
 
+	/* Disable the ddr secure region setting to make it non-secure */
+	rk_clrreg(SGRF_DDR_CON0, 0x4000);
+
 	/* PWMs select rkpwm clock source */
-	rk_setreg(GRF_SOC_CON2, 1 << 0);
+	rk_setreg(&grf->soc_con[2], 1 << 0);
 
 	/* PWM0~3 io select */
-	rk_setreg(GRF_CON_IOMUX, 0xf << 0);
+	rk_setreg(&grf->con_iomux, 0xf << 0);
 
 	/* UART1~2 io select */
-	rk_setreg(GRF_CON_IOMUX, (1 << 11) | (1 << 8));
+	rk_setreg(&grf->con_iomux, (1 << 11) | (1 << 8));
 
 	/* HDMI phy clock source select HDMIPHY clock out */
 	rk_clrreg(CRU_MISC_CON, 1 << 13);
 
+	/*
+	* The integrated macphy is enabled by default, disable it
+	* for saving power consuming.
+	*/
+	rk_clrsetreg(&grf->macphy_con[0], MACPHY_CFG_ENABLE_MASK,
+		     0 << MACPHY_CFG_ENABLE_SHIFT);
 	/* TODO: ECO version */
 
 	return 0;
+}
+#endif
+
+void board_debug_uart_init(void)
+{
+	static struct rk322x_grf * const grf = (void *)GRF_BASE;
+	/* Enable early UART2 channel 1 on the RK322x */
+	rk_clrsetreg(&grf->gpio1b_iomux,
+		     GPIO1B1_MASK | GPIO1B2_MASK,
+		     GPIO1B2_UART21_SIN << GPIO1B2_SHIFT |
+		     GPIO1B1_UART21_SOUT << GPIO1B1_SHIFT);
+	/* Set channel C as UART2 input */
+	rk_clrsetreg(&grf->con_iomux,
+		     CON_IOMUX_UART2SEL_MASK,
+		     CON_IOMUX_UART2SEL_21 << CON_IOMUX_UART2SEL_SHIFT);
 }
