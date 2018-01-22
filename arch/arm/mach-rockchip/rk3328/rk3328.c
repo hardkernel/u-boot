73d952acc8cc1ddad6652ba71895d9fe928c1e4b
Kever Yang <kever.yang@rock-chips.com>
2018-01-22 14:37:17 +0800

    rockchip: rk3328: use common board file
    Use common board file and move SoC spec setting into rk3328.c
    
    Change-Id: Ia7cf43e0096da980d744260caa61456ddc6b24eb
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- arch/arm/mach-rockchip/rk3328/rk3328.c
+++ arch/arm/mach-rockchip/rk3328/rk3328.c
@@ -5,14 +5,21 @@
  */
 
 #include <common.h>
+#include <asm/arch/bootrom.h>
 #include <asm/arch/hardware.h>
+#include <asm/arch/grf_rk3328.h>
+#include <asm/arch/uart.h>
 #include <asm/armv8/mmu.h>
 #include <asm/io.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#define GRF_SOC_CON4		0xff100410
+#define CRU_BASE		0xFF440000
+#define GRF_BASE		0xFF100000
+#define UART2_BASE		0xFF130000
+
 #define CRU_MISC_CON		0xff440084
+#define FW_DDR_CON_REG		0xff7c0040
 
 static struct mm_region rk3328_mem_map[] = {
 	{
@@ -36,28 +43,56 @@ static struct mm_region rk3328_mem_map[] = {
 
 struct mm_region *mem_map = rk3328_mem_map;
 
-int dram_init_banksize(void)
-{
-	size_t max_size = min((unsigned long)gd->ram_size, gd->ram_top);
-
-	/* Reserve 0x200000 for ATF bl31 */
-	gd->bd->bi_dram[0].start = 0x200000;
-	gd->bd->bi_dram[0].size = max_size - gd->bd->bi_dram[0].start;
-
-	return 0;
-}
-
+const char * const boot_devices[BROM_LAST_BOOTSOURCE + 1] = {
+	[BROM_BOOTSOURCE_EMMC] = "/rksdmmc@ff520000",
+	[BROM_BOOTSOURCE_SD] = "/rksdmmc@ff500000",
+};
 int arch_cpu_init(void)
 {
+#ifdef CONFIG_TPL_BUILD
+	struct rk3328_grf_regs * const grf = (void *)GRF_BASE;
 	/* We do some SoC one time setting here. */
 
+	/* Disable the ddr secure region setting to make it non-secure */
+	rk_setreg(FW_DDR_CON_REG, 0x200);
+
 	/* Enable force to jtag, jtag_tclk/tms iomuxed with sdmmc0_d2/d3 */
-	rk_setreg(GRF_SOC_CON4, 1 << 12);
+	rk_setreg(&grf->soc_con[4], 1 << 12);
 
 	/* HDMI phy clock source select HDMIPHY clock out */
 	rk_clrreg(CRU_MISC_CON, 1 << 13);
 
 	/* TODO: ECO version */
-
+#endif
 	return 0;
+}
+
+void board_debug_uart_init(void)
+{
+#ifdef CONFIG_TPL_BUILD
+	struct rk3328_grf_regs * const grf = (void *)GRF_BASE;
+	struct rk_uart * const uart = (void *)UART2_BASE;
+
+	/* uart_sel_clk default select 24MHz */
+	writel((3 << (8 + 16)) | (2 << 8), CRU_BASE + 0x148);
+
+	/* init uart baud rate 1500000 */
+	writel(0x83, &uart->lcr);
+	writel(0x1, &uart->rbr);
+	writel(0x3, &uart->lcr);
+
+	/* Enable early UART2 */
+	rk_clrsetreg(&grf->com_iomux,
+		     IOMUX_SEL_UART2_MASK,
+		     IOMUX_SEL_UART2_M1 << IOMUX_SEL_UART2_SHIFT);
+	rk_clrsetreg(&grf->gpio2a_iomux,
+		     GPIO2A0_SEL_MASK,
+		     GPIO2A0_UART2_TX_M1 << GPIO2A0_SEL_SHIFT);
+	rk_clrsetreg(&grf->gpio2a_iomux,
+		     GPIO2A1_SEL_MASK,
+		     GPIO2A1_UART2_RX_M1 << GPIO2A1_SEL_SHIFT);
+
+	/* enable FIFO */
+	writel(0x1, &uart->sfe);
+#endif
 }
