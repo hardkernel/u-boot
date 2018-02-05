c876650a87da70b4cfd38ea7728d669d700c176d
Joseph Chen <chenjh@rock-chips.com>
2018-02-05 10:45:28 +0800

    rockchip: add chip info interface
    Change-Id: I916ef9d22370bf464569bf400e0cab61d6107d6a
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

---
+++ arch/arm/include/asm/arch-rockchip/chip_info.h
@@ -0,0 +1,6 @@
+#ifndef _ROCKCHIP_CHIP_INFO_H_
+#define _ROCKCHIP_CHIP_INFO_H_
+
+int rockchip_get_chip_info(unsigned int chip_info[]);
+int rockchip_rockusb_get_chip_info(unsigned int chip_info[]);
+#endif
