57f2d95f177221281f6a604f0835ccfd1e465ba2
Xindong Xu <xindong.xu@amlogic.com>
2018-03-15 16:50:59 +0800

    fastboot: change DEVICE_PRODUCT name [2/2]
    PD# 162254
    
    change DEVICE_PRODUCT name
    
    Change-Id: Ia49a55513fe88495c4523c4024a38f728cdb0bcb

--- board/amlogic/configs/txlx_t962x_r311_v1.h
+++ board/amlogic/configs/txlx_t962x_r311_v1.h
@@ -353,7 +353,7 @@
 #define CONFIG_USBDOWNLOAD_GADGET 1
 #define CONFIG_SYS_CACHELINE_SIZE 64
 #define CONFIG_FASTBOOT_MAX_DOWN_SIZE	0x8000000
-#define CONFIG_DEVICE_PRODUCT	"txlx_r311"
+#define CONFIG_DEVICE_PRODUCT	"t962x_r311"
 
 //UBOOT Facotry usb/sdcard burning config
 #define CONFIG_AML_V2_FACTORY_BURN              1       //support facotry usb burning
