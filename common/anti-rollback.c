d0c586cbaef2a41322ccb090a095f22d7df6345e
Liqiang Jin <liqiang.jin@amlogic.com>
2018-05-14 13:40:25 +0800

    P212: anti-rollback: add AVB anti-rollback interface [2/2]
    PD#140403
    
    Problem:
    need support AVB anti-rollback API
    
    Solution:
    add AVB anti-rollback API
    
    Verify:
    Android 7.0 + P212, verify OK
    
    Change-Id: If7faa83f595e5da836da3972939f7a88e7abce96
    Signed-off-by: Liqiang Jin <liqiang.jin@amlogic.com>

--- common/anti-rollback.c
+++ common/anti-rollback.c
@@ -30,6 +30,12 @@
 
 #define FUNCID_ANTIROLLBACK_VERSION_CHECK     0xb2000010
 
+#define FUNCID_AVB_VERSION_SET                0xb2000011
+#define FUNCID_AVB_VERSION_GET                0xb2000012
+#define FUNCID_AVB_LOCK_STATE_GET             0xb2000013
+#define FUNCID_AVB_LOCK                       0xb2000014
+#define FUNCID_AVB_UNLOCK                     0xb2000015
+
 #define IMAGE_VERSION_TYPE_BOOTLOADER         0x001
 #define IMAGE_VERSION_TYPE_RECOVERY           0x002
 #define IMAGE_VERSION_TYPE_BOOT               0x003
@@ -38,7 +44,8 @@
 #define KERNEL_TYPE_BOOT                      0x01
 #define KERNEL_TYPE_RECOVERY                  0x02
 
-static uint32_t antirollback_image_version_check(uint32_t type, uint32_t version)
+static uint32_t antirollback_image_version_check(uint32_t type,
+							uint32_t version)
 {
 	register uint32_t x0 asm("x0") = FUNCID_ANTIROLLBACK_VERSION_CHECK;
 	register uint32_t x1 asm("x1") = type;
@@ -71,8 +78,9 @@ bool check_antirollback(uint32_t kernel_version)
 		}
 	}
 	else if (KERNEL_TYPE_RECOVERY == type) {
-		if (antirollback_image_version_check(IMAGE_VERSION_TYPE_RECOVERY,
-							version) != 0) {
+		if (antirollback_image_version_check(
+					IMAGE_VERSION_TYPE_RECOVERY,
+					version) != 0) {
 			printf("checking recovery.img version failed\n");
 			ret = false;
 		}
@@ -86,4 +94,99 @@ bool check_antirollback(uint32_t kernel_version)
 		printf("checking version success\n");
 
 	return ret;
+}
+
+bool set_avb_antirollback(uint32_t index, uint32_t version)
+{
+	register uint32_t x0 asm("x0") = FUNCID_AVB_VERSION_SET;
+	register uint32_t x1 asm("x1") = index;
+	register uint32_t x2 asm("x2") = version;
+
+	do {
+		asm volatile(
+			__asmeq("%0", "x0")
+			__asmeq("%1", "x0")
+			__asmeq("%2", "x1")
+			__asmeq("%3", "x2")
+			"smc	#0\n"
+			: "=r"(x0)
+			: "r"(x0), "r"(x1), "r"(x2));
+	} while (0);
+
+	return 0 == x0;
+}
+
+bool get_avb_antirollback(uint32_t index, uint32_t* version)
+{
+	register uint32_t x0 asm("x0") = FUNCID_AVB_VERSION_GET;
+	register uint32_t x1 asm("x1") = index;
+
+	do {
+		asm volatile(
+			__asmeq("%0", "x0")
+			__asmeq("%1", "x1")
+			__asmeq("%2", "x0")
+			__asmeq("%3", "x1")
+			"smc	#0\n"
+			: "=r"(x0), "=r"(x1)
+			: "r"(x0), "r"(x1));
+	} while (0);
+
+	if (0 == x0)
+		*version = x1;
+
+	return 0 == x0;
+}
+
+bool get_avb_lock_state(uint32_t* lock_state)
+{
+	register uint32_t x0 asm("x0") = FUNCID_AVB_LOCK_STATE_GET;
+	register uint32_t x1 asm("x1") = 0;
+
+	do {
+		asm volatile(
+			__asmeq("%0", "x0")
+			__asmeq("%1", "x1")
+			__asmeq("%2", "x0")
+			"smc	#0\n"
+			: "=r"(x0), "=r"(x1)
+			: "r"(x0));
+	} while (0);
+
+	if (0 == x0)
+		*lock_state = x1;
+
+	return 0 == x0;
+}
+
+bool avb_lock(void)
+{
+	register uint32_t x0 asm("x0") = FUNCID_AVB_LOCK;
+
+	do {
+		asm volatile(
+			__asmeq("%0", "x0")
+			__asmeq("%1", "x0")
+			"smc	#0\n"
+			: "=r"(x0)
+			: "r"(x0));
+	} while (0);
+
+	return 0 == x0;
+}
+
+bool avb_unlock(void)
+{
+	register uint32_t x0 asm("x0") = FUNCID_AVB_UNLOCK;
+
+	do {
+		asm volatile(
+			__asmeq("%0", "x0")
+			__asmeq("%1", "x0")
+			"smc	#0\n"
+			: "=r"(x0)
+			: "r"(x0));
+	} while (0);
+
+	return 0 == x0;
 }
--- common/anti-rollback.h
+++ common/anti-rollback.h
@@ -31,4 +31,10 @@
 
 bool check_antirollback(uint32_t kernel_version);
 
+bool set_avb_antirollback(uint32_t index, uint32_t version);
+bool get_avb_antirollback(uint32_t index, uint32_t* version);
+bool get_avb_lock_state(uint32_t* lock_state);
+bool avb_lock(void);
+bool avb_unlock(void);
+
 #endif
