21b66f625a88a7e27b4ddfa739f5942260a052fd
Evoke Zhang <evoke.zhang@amlogic.com>
2018-05-17 18:08:58 +0800

    backlight: ldim: optimize iw7027 driver [1/2]
    PD#166583
    
    Problem:
    there is abnromal when iw7027 power on
    
    Solution:
    optimize iw7027 driver
    
    Verify:
    txlx r311
    
    Change-Id: I49dc81fc16d83ff669b5fff6fa64030b41141d01
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- drivers/display/lcd/aml_ldim/iw7027.c
+++ drivers/display/lcd/aml_ldim/iw7027.c
@@ -84,14 +84,12 @@ end:
 //iw7027 register read
 static int iw7027_rreg(struct spi_slave *slave, u8 addr, u8 *val)
 {
-	u8 tbuf[4], rbuf[4];
+	u8 tbuf[4], rbuf[4], temp;
 	int ret;
 
 	/*set read flag*/
-	if (addr >= 0x80)
-		iw7027_wreg(bl_iw7027->spi, 0x78, 0x80);
-	else
-		iw7027_wreg(bl_iw7027->spi, 0x78, 0x0);
+	temp = (addr >= 0x80) ? 0x80 : 0x0;
+	iw7027_wreg(slave, 0x78, temp);
 
 	ret = spi_claim_bus(slave);
 	if (ret) {
@@ -184,52 +182,58 @@ static int iw7027_power_on_init(void)
 
 static int iw7027_hw_init_on(void)
 {
-	int i, count = 1000;
+	int i;
 	unsigned char  reg_duty_chk = 0 , reg_chk = 0;
 	struct aml_ldim_driver_s *ldim_drv = aml_ldim_get_driver();
 
-	/*iw7027 system power_on*/
+	/* step 1: system power_on */
 	LDIMPR("%s: iw7027 system power_on\n", __func__);
 	ldim_set_gpio(ldim_drv->ldev_conf->en_gpio, ldim_drv->ldev_conf->en_gpio_on);
 	ldim_set_duty_pwm(&(ldim_drv->ldev_conf->pwm_config));
+
+	/* step 2: delay for internal logic stable */
 	mdelay(10);
 
-	/*SPI Communication Check */
-	LDIMPR("%s: SPI Communication Check \n", __func__);
+	/* step 3: SPI communication check */
+	LDIMPR("%s: SPI Communication Check\n", __func__);
 	for (i = 0; i <= 10; i++) {
 		iw7027_wreg(bl_iw7027->spi, 0x00, 0x06);
 		iw7027_rreg(bl_iw7027->spi, 0x00, &reg_chk);
 		if (reg_chk == 0x06)
 			break;
-		else if(i == 10)
-			goto iw7027_smr_end;
+		if (i == 10) {
+			LDIMERR("%s: SPI communication check error\n",
+				__func__);
+		}
 	}
-	/*Write initial control registers*/
+
+	/* step 4: configure initial registers */
 	LDIMPR("%s: Write initial control registers\n", __func__);
 	iw7027_power_on_init();
 
-	/*open Vsync*/
+	/* step 5: supply stable vsync */
 	LDIMPR("%s: open Vsync\n", __func__);
 	ldim_drv->pinmux_ctrl(1);
-	mdelay(500);
 
-	/*Disable soft reset*/
-	LDIMPR("%s: Disable soft reset\n", __func__);
+	/* step 6: delay for system clock and light bar PSU stable */
+	mdelay(550);
+
+	/* step 7: start calibration */
+	LDIMPR("%s: start calibration\n", __func__);
 	iw7027_wreg(bl_iw7027->spi, 0x00, 0x07);
+	mdelay(200);
 
-	/*Calibration Done or not*/
-	LDIMPR("%s: Calibration Done or not\n", __func__);
-	while (count --) {
+	/* step 8: calibration done or not */
+	i = 0;
+	while (i++ < 1000) {
 		iw7027_rreg(bl_iw7027->spi, 0xb3, &reg_duty_chk);
 		/*VDAC statue reg :FB1=[0x5] FB2=[0x50]*/
 		/*The current platform using FB1*/
-		if ((reg_duty_chk & 0xf) == 0x05) {
+		if ((reg_duty_chk & 0xf) == 0x05)
 			break;
-		}
 		mdelay(1);
 	}
-	LDIMPR("%s: reg_duty_chk[%d] = %x\n", __func__, count, reg_duty_chk);
-iw7027_smr_end:
+	LDIMPR("%s: calibration done: [%d] = %x\n", __func__, i, reg_duty_chk);
 
 	return 0;
 }
@@ -260,7 +264,7 @@ static unsigned int iw7027_get_value(unsigned int level)
 
 static int iw7027_smr(unsigned short *buf, unsigned char len)
 {
-	unsigned char val_0[20];
+	unsigned char val[20];
 
 	if (iw7027_on_flag == 0) {
 		if (lcd_debug_print_flag)
@@ -272,48 +276,28 @@ static int iw7027_smr(unsigned short *buf, unsigned char len)
 		return -1;
 	}
 
-	val_0[0] = ((iw7027_get_value(buf[0])
-		) & 0xf00) >> 8;
-	val_0[1] = (iw7027_get_value(buf[0])
-		) & 0xff;
-	val_0[2] = ((iw7027_get_value(buf[1])
-		) & 0xf00) >> 8;
-	val_0[3] = (iw7027_get_value(buf[1])
-		) & 0xff;
-	val_0[4] = ((iw7027_get_value(buf[2])
-		) & 0xf00) >> 8;
-	val_0[5] = (iw7027_get_value(buf[2])
-		) & 0xff;
-	val_0[6] = ((iw7027_get_value(buf[3])
-		) & 0xf00) >> 8;
-	val_0[7] = (iw7027_get_value(buf[3])
-		) & 0xff;
-	val_0[8] = ((iw7027_get_value(buf[4])
-		) & 0xf00) >> 8;
-	val_0[9] = (iw7027_get_value(buf[4])
-		) & 0xff;
-	val_0[10] = ((iw7027_get_value(buf[5])
-		) & 0xf00) >> 8;
-	val_0[11] = (iw7027_get_value(buf[5])
-		) & 0xff;
-	val_0[12] = ((iw7027_get_value(buf[6])
-		) & 0xf00) >> 8;
-	val_0[13] = (iw7027_get_value(buf[6])
-		) & 0xff;
-	val_0[14] = ((iw7027_get_value(buf[7])
-		) & 0xf00) >> 8;
-	val_0[15] = (iw7027_get_value(buf[7])
-		) & 0xff;
-	val_0[16] = ((iw7027_get_value(buf[8])
-		) & 0xf00) >> 8;
-	val_0[17] = (iw7027_get_value(buf[8])
-		) & 0xff;
-	val_0[18] = ((iw7027_get_value(buf[9])
-		) & 0xf00) >> 8;
-	val_0[19] = (iw7027_get_value(buf[9])
-		) & 0xff;
+	val[0] = ((iw7027_get_value(buf[0])) & 0xf00) >> 8;
+	val[1] = (iw7027_get_value(buf[0])) & 0xff;
+	val[2] = ((iw7027_get_value(buf[1])) & 0xf00) >> 8;
+	val[3] = (iw7027_get_value(buf[1])) & 0xff;
+	val[4] = ((iw7027_get_value(buf[2])) & 0xf00) >> 8;
+	val[5] = (iw7027_get_value(buf[2])) & 0xff;
+	val[6] = ((iw7027_get_value(buf[3])) & 0xf00) >> 8;
+	val[7] = (iw7027_get_value(buf[3])) & 0xff;
+	val[8] = ((iw7027_get_value(buf[4])) & 0xf00) >> 8;
+	val[9] = (iw7027_get_value(buf[4])) & 0xff;
+	val[10] = ((iw7027_get_value(buf[5])) & 0xf00) >> 8;
+	val[11] = (iw7027_get_value(buf[5])) & 0xff;
+	val[12] = ((iw7027_get_value(buf[6])) & 0xf00) >> 8;
+	val[13] = (iw7027_get_value(buf[6])) & 0xff;
+	val[14] = ((iw7027_get_value(buf[7])) & 0xf00) >> 8;
+	val[15] = (iw7027_get_value(buf[7])) & 0xff;
+	val[16] = ((iw7027_get_value(buf[8])) & 0xf00) >> 8;
+	val[17] = (iw7027_get_value(buf[8])) & 0xff;
+	val[18] = ((iw7027_get_value(buf[9])) & 0xf00) >> 8;
+	val[19] = (iw7027_get_value(buf[9])) & 0xff;
 
-	iw7027_wregs(bl_iw7027->spi, 0x40, val_0, 20);
+	iw7027_wregs(bl_iw7027->spi, 0x40, val, 20);
 
 	return 0;
 }
@@ -364,14 +348,14 @@ int ldim_dev_iw7027_probe(void)
 	iw7027_on_flag = 0;
 
 	/* register spi */
-
-	snprintf(name, sizeof(name), "generic_%d:%d", ldim_drv->spi_dev->bus_num, ldim_drv->spi_dev->chip_select);
+	snprintf(name, sizeof(name), "generic_%d:%d",
+		ldim_drv->spi_dev->bus_num, ldim_drv->spi_dev->chip_select);
 	str = strdup(name);
 	ret = spi_get_bus_and_cs(ldim_drv->spi_dev->bus_num,
-					ldim_drv->spi_dev->chip_select,
-					ldim_drv->spi_dev->max_speed_hz,
-					ldim_drv->spi_dev->mode, "spi_generic_drv",
-					str, &dev, &ldim_drv->spi_dev->spi);
+				ldim_drv->spi_dev->chip_select,
+				ldim_drv->spi_dev->max_speed_hz,
+				ldim_drv->spi_dev->mode, "spi_generic_drv",
+				str, &dev, &ldim_drv->spi_dev->spi);
 	if (ret) {
 		LDIMERR("register ldim_dev spi driver failed\n");
 		return ret;
@@ -385,7 +369,8 @@ int ldim_dev_iw7027_probe(void)
 	bl_iw7027->cmd_size = ldim_drv->ldev_conf->cmd_size;
 	bl_iw7027->init_data = ldim_drv->ldev_conf->init_on;
 	iw7027_ldim_driver_update();
-	printf("%s:is ok\n",__func__);
+
+	printf("%s: ok\n", __func__);
 
 	return 0;
 }
