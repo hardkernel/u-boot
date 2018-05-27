0f48a8e99e1eedc991a051b034ca53d1d25a8c1a
xiaobo gu <xiaobo.gu@amlogic.com>
2018-05-27 19:52:40 +0800

    g12b: pwm: update pwmb mask and vcck config [1/1]
    PD#165090
    
    Problem:
    g12b skt board pwm and vcck need update
    
    Solution:
    update pwmb and vcck config
    
    Verify:
    test pass on g12b skt board
    
    Change-Id: I176ca8cdb059d70e660f43b4b0e820be2c279e94
    Signed-off-by: xiaobo gu <xiaobo.gu@amlogic.com>

--- board/amlogic/g12b_skt_v1/firmware/timing.c
+++ board/amlogic/g12b_skt_v1/firmware/timing.c
@@ -454,13 +454,13 @@ bl2_reg_t __bl2_reg[] = {
 	{AO_PIN_MUX_REG1,     (3 << 20),               (0xF << 20),  0, BL2_INIT_STAGE_1, 0},
 	/* PWM_AO_B */
 	/* VDDEE_VAL_REG: check PWM table */
-	{AO_PWM_PWM_B,        VDDEE_VAL_REG,           (0x7f << 16), 0, BL2_INIT_STAGE_1, 0},
+	{AO_PWM_PWM_B,        VDDEE_VAL_REG,           0xffffffff,   0, BL2_INIT_STAGE_1, 0},
 	{AO_PWM_MISC_REG_AB,  ((1 << 23) | (1 << 1)),  (0x7f << 16), 0, BL2_INIT_STAGE_1, 0},
 	{AO_PIN_MUX_REG1,     (3 << 16),               (0xF << 16),  0, BL2_INIT_STAGE_1, 0},
 	/* Enable 5V_EN */
 	{GPIO_O_EN_N_REG3,    (1 << 8),                (1 << 8),     0, BL2_INIT_STAGE_1, 0},
 	{GPIO_O_REG3,         (1 << 8),                0xffffffff,   0, BL2_INIT_STAGE_1, 0},
 	/* Enable VCCK */
-	{AO_SEC_REG0,         (1 << 0),                0xffffffff,   0, BL2_INIT_STAGE_1, 0},
-	{AO_GPIO_O,           (1 << 31),               0xffffffff,   0, BL2_INIT_STAGE_1, 0},
+	{AO_SEC_REG0,         (1 << 0),                (1 << 0),     0, BL2_INIT_STAGE_1, 0},
+	{AO_GPIO_O,           (1 << 31),               (1 << 31),    0, BL2_INIT_STAGE_1, 0},
 };
