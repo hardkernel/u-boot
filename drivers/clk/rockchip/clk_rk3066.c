11143e9c4538f7546983efd15c3fc49845f4bef3
Kever Yang <kever.yang@rock-chips.com>
2018-06-01 11:48:14 +0800

    rockchip: clk: rk3066: convert to live dt
    Use dev_read_addr_ptr to get cru base
    
    Change-Id: I22d0a98ff29d894ec777574f93f145c55383ffa0
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- drivers/clk/rockchip/clk_rk3066.c
+++ drivers/clk/rockchip/clk_rk3066.c
@@ -530,7 +530,7 @@ static int rk3066_clk_ofdata_to_platdata(struct udevice *dev)
 #if !CONFIG_IS_ENABLED(OF_PLATDATA)
 	struct rk3066_clk_priv *priv = dev_get_priv(dev);
 
-	priv->cru = (struct rk3066_cru *)devfdt_get_addr(dev);
+	priv->cru = dev_read_addr_ptr(dev);
 #endif
 
 	return 0;
