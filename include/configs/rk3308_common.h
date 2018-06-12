0958c00c0776311c62c607d699bc836e2a4ef428
Andy Yan <andy.yan@rock-chips.com>
2018-06-12 14:26:54 +0800

    rockchip: rk3308: enable lzo/lz4/gzip compressed kernel image
    Change-Id: Ic44218ddf2ee8a993b4530cad04d712d6aecfccc
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

--- include/configs/rk3308_common.h
+++ include/configs/rk3308_common.h
@@ -50,13 +50,15 @@
 	"pxefile_addr_r=0x00600000\0" \
 	"fdt_addr_r=0x01f00000\0" \
 	"kernel_addr_r=0x02080000\0" \
+	"kernel_addr_c=0x04080000\0" \
 	"ramdisk_addr_r=0x04000000\0"
 #else
 #define ENV_MEM_LAYOUT_SETTINGS \
 	"scriptaddr=0x00500000\0" \
 	"pxefile_addr_r=0x00600000\0" \
 	"fdt_addr_r=0x03200000\0" \
-	"kernel_addr_r=0x2008000\0" \
+	"kernel_addr_r=0x00058000\0" \
+	"kernel_addr_c=0x2008000\0" \
 	"ramdisk_addr_r=0x03080000\0"
 #endif
 
