543c0e7848ba8d40d5c371aa0599e22d6c32a684
Sandy Huang <hjc@rock-chips.com>
2018-07-12 15:04:30 +0800

    drm/rockchip: vop: fix VOP_REG_SUPPORT judge wrong
    If the register isn't define at rockchip_vop_reg.c, the default value
    of reg.major is 0, this will lead to judge error. so we add reg.mask
    conditions because if it's defined register, the reg.mask can't be 0.
    
    Change-Id: I368276d2d81983b5c5dbf591503def1de49ccdc1
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- drivers/video/drm/rockchip_vop.h
+++ drivers/video/drm/rockchip_vop.h
@@ -16,10 +16,11 @@
 #define VOP_MINOR(version) 	((version) & 0xff)
 
 #define VOP_REG_SUPPORT(vop, reg) \
-		(!reg.major || (reg.major == VOP_MAJOR(vop->version) && \
-		reg.begin_minor <= VOP_MINOR(vop->version) && \
-		reg.end_minor >= VOP_MINOR(vop->version) && \
-		reg.mask))
+		(reg.mask && \
+		 (!reg.major || \
+		  (reg.major == VOP_MAJOR(vop->version) && \
+		   reg.begin_minor <= VOP_MINOR(vop->version) && \
+		   reg.end_minor >= VOP_MINOR(vop->version))))
 
 #define VOP_WIN_SUPPORT(vop, win, name) \
 		VOP_REG_SUPPORT(vop, win->name)
