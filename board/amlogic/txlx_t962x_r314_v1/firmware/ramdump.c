18c701c5a34939c324d1bc36d1dc69852c5670a0
zhiguang.ouyang <zhiguang.ouyang@amlogic.com>
2018-07-17 18:51:32 +0800

    config: r314: add new config r314 [3/3]
    PD#171087
    
    Problem:
    t962x r314 with 4pcs ddr3 2layer board bringup
    
    Solution:
    add uboot config
    
    Verify:
    test pass at xiaomi_4pcs_ddr3_txlx board
    
    Change-Id: I2ff5f6faec6810f7ec0b8545a423befede5a4efb
    Signed-off-by: zhiguang ouyang <zhiguang.ouyang@amlogic.com>

---
+++ board/amlogic/txlx_t962x_r314_v1/firmware/ramdump.c
@@ -0,0 +1,45 @@
+
+#ifdef CONFIG_MDUMP_COMPRESS
+#include "ramdump.h"
+
+struct ram_compress_full __ramdump_data = {
+	.store_phy_addr = (void *)CONFIG_COMPRESSED_DATA_ADDR,
+	.full_memsize   = CONFIG_DDR_TOTAL_SIZE,
+	.section_count  = CONFIG_COMPRESS_SECTION,
+	.sections       = {
+		{
+			/* memory afer compressed data address */
+			.phy_addr      = (void *)CONFIG_COMPRESSED_DATA_ADDR,
+			.section_size  = CONFIG_DDR_TOTAL_SIZE -
+					 CONFIG_COMPRESSED_DATA_ADDR,
+			.section_index = 4,
+			.compress_type = RAM_COMPRESS_NORMAL,
+		},
+		{
+			/* memory before bl2 */
+			.phy_addr      = (void *)CONFIG_COMPRESS_START_ADDR,
+			.section_size  = CONFIG_BL2_IGNORE_ADDR -
+					 CONFIG_COMPRESS_START_ADDR,
+			.section_index = 1,
+			.compress_type = RAM_COMPRESS_NORMAL,
+		},
+		{
+			/* memory in reserved bl2 */
+			.phy_addr      = (void *)CONFIG_BL2_IGNORE_ADDR,
+			.section_size  = CONFIG_BL2_IGNORE_SIZE,
+			.section_index = 2,
+			.compress_type = RAM_COMPRESS_SET,
+			.set_value     = 0x0,
+		},
+		{
+			/* segment 4: normal compress */
+			.phy_addr      = (void *)CONFIG_SEG4_ADDR,
+			.section_size  = CONFIG_COMPRESSED_DATA_ADDR -
+					 CONFIG_SEG4_ADDR,
+			.section_index = 3,
+			.compress_type = RAM_COMPRESS_NORMAL,
+		}
+	},
+};
+#endif /* CONFIG_MDUMP_COMPRESS */
+
