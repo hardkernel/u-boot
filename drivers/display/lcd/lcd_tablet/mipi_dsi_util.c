09e2f125bff11f11491000a60209605e3239ccf9
Evoke Zhang <evoke.zhang@amlogic.com>
2018-07-19 16:31:43 +0800

    lcd: mipi-dsi: optimize power on sequence for encl and dsi [1/2]
    PD#170290
    
    Problem:
    sometimes panel display abnormal
    
    Solution:
    enable encl after dsi controller init
    
    Verify:
    g12a u200
    
    Change-Id: I66cc80e65a3a8dd7374a183b1273ffaa080354b4
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- drivers/display/lcd/lcd_tablet/mipi_dsi_util.c
+++ drivers/display/lcd/lcd_tablet/mipi_dsi_util.c
@@ -444,10 +444,10 @@ static void mipi_dcs_set(int trans_type, int req_ack, int tear_en)
 
 	if (tear_en == MIPI_DCS_ENABLE_TEAR) {
 		/* Enable Tear Interrupt if tear_en is valid */
-		lcd_vcbus_set_mask(MIPI_DSI_TOP_INTR_CNTL_STAT,
+		dsi_host_set_mask(MIPI_DSI_TOP_INTR_CNTL_STAT,
 			(0x1 << BIT_EDPITE_INT_EN));
 		/* Enable Measure Vsync */
-		lcd_vcbus_set_mask(MIPI_DSI_TOP_MEAS_CNTL,
+		dsi_host_set_mask(MIPI_DSI_TOP_MEAS_CNTL,
 			(0x1 << BIT_VSYNC_MEAS_EN) | (0x1 << BIT_TE_MEAS_EN));
 	}
 
@@ -776,7 +776,7 @@ static void set_mipi_dsi_host(unsigned int vcid, unsigned int chroma_subsample,
 		(0x1 << BIT_TO_CLK_DIV) |
 		(dsi_phy_config.lp_tesc << BIT_TX_ESC_CLK_DIV));
 	/* Packet header settings  //move to mipi_dcs_set */
-	/* dsi_host_write( MIPI_DSI_DWC_PCKHDL_CFG_OS,
+	/* dsi_host_write(MIPI_DSI_DWC_PCKHDL_CFG_OS,
 	 *	(1 << BIT_CRC_RX_EN) |
 	 *	(1 << BIT_ECC_RX_EN) |
 	 *	(0 << BIT_BTA_EN) |
@@ -1847,6 +1847,8 @@ static void mipi_dsi_link_on(struct lcd_config_s *pconf)
 			    */
 			op_mode_disp, /* DSI operation mode, video or command */
 			pconf);
+		if (op_mode_disp == MIPI_DSI_OPERATION_MODE_VIDEO)
+			lcd_vcbus_write(ENCL_VIDEO_EN, 1);
 	}
 }
 
@@ -2039,9 +2041,15 @@ static void mipi_dsi_host_on(struct lcd_config_s *pconf)
 	if (lcd_debug_print_flag)
 		LCDPR("%s\n", __func__);
 
+	/* disable encl */
+	lcd_vcbus_write(ENCL_VIDEO_EN, 0);
+	udelay(100);
+
 	mipi_dsi_config_post(pconf);
 
 	startup_mipi_dsi_host();
+
+	set_dsi_phy_config(pconf->lcd_control.mipi_config);
 
 	op_mode_init = pconf->lcd_control.mipi_config->operation_mode_init;
 	mipi_dcs_set(MIPI_DSI_CMD_TRANS_TYPE, /* 0: high speed, 1: low power */
@@ -2052,10 +2060,12 @@ static void mipi_dsi_host_on(struct lcd_config_s *pconf)
 		0, /* Chroma sub sample, only for YUV 422 or 420, even or odd */
 		op_mode_init, /* DSI operation mode, video or command */
 		pconf);
-	set_dsi_phy_config(pconf->lcd_control.mipi_config);
 
 	/* Startup transfer */
 	mipi_dsi_lpclk_ctrl(pconf->lcd_control.mipi_config);
+	if (op_mode_init == MIPI_DSI_OPERATION_MODE_VIDEO)
+		lcd_vcbus_write(ENCL_VIDEO_EN, 1);
+
 	mipi_dsi_link_on(pconf);
 
 	if (lcd_debug_print_flag)
