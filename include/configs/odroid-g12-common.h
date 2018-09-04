c2895186224d2d04a5f37cead7656506bea8debb
Dongjin Kim <tobetter@gmail.com>
2018-09-05 02:19:32 +0900

    ODROID-COMMON: add 'CONFIG_SUPPORT_RAW_INITRD' to load gziped initrd
    Change-Id: Ie4f8f94a342372eb910f028990635b23740bacea
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- include/configs/odroid-g12-common.h
+++ include/configs/odroid-g12-common.h
@@ -469,6 +469,7 @@
 #define CONFIG_CMD_CPU_TEMP			1
 #define CONFIG_SYS_MEM_TOP_HIDE			0x08000000 /* hide 128MB for kernel reserve */
 #define CONFIG_CMD_LOADB			1
+#define CONFIG_SUPPORT_RAW_INITRD		1
 
 /* debug mode defines */
 //#define CONFIG_DEBUG_MODE			1
