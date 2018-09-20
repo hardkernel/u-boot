e36a44135631ceb5a3e1c46ef2260b091610bde5
Joseph Chen <chenjh@rock-chips.com>
2018-09-20 10:34:57 +0800

    rockchip: rk3288: adjust U-Boot text base and init sp
    - set text base to 2MB offset;
    - set init sp to 4MB offset;
    
    Change-Id: I9f250c3561cbe8c0b4ee1c9da625766f7306c2f2
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- include/configs/rk3288_common.h
+++ include/configs/rk3288_common.h
@@ -24,9 +24,9 @@
 /* Bootrom will load u-boot binary to 0x0 once return from SPL */
 #define CONFIG_SYS_TEXT_BASE		0x00000000
 #else
-#define CONFIG_SYS_TEXT_BASE		0x00100000
+#define CONFIG_SYS_TEXT_BASE		0x00200000
 #endif
-#define CONFIG_SYS_INIT_SP_ADDR		0x00100000
+#define CONFIG_SYS_INIT_SP_ADDR		0x00400000
 #define CONFIG_SYS_LOAD_ADDR		0x00800800
 #define CONFIG_SPL_STACK		0xff718000
 
