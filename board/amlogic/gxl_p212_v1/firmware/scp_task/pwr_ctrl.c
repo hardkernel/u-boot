6e460fc28deb610088f10f116bf4f1c0161c10e0
Rongjun Chen <rongjun.chen@amlogic.com>
2018-09-25 14:14:30 +0800

    wifi: bcm: fix wifi resume open fail issue [1/1]
    PD#173849
    
    Problem:
    system suspend resume let irq num change, is will let wifi open fail
    
    Solution:
    channel the irq num
    
    Verify:
    verify by p212
    
    Change-Id: Ife2500ba1d10df55104a2928b481daf3e1976f1f
    Signed-off-by: Rongjun Chen <rongjun.chen@amlogic.com>

--- board/amlogic/gxl_p212_v1/firmware/scp_task/pwr_ctrl.c
+++ board/amlogic/gxl_p212_v1/firmware/scp_task/pwr_ctrl.c
@@ -161,7 +161,7 @@ void get_wakeup_source(void *response, unsigned int suspend_from)
 	gpio->gpio_in_ao = 0;
 	gpio->gpio_out_idx = -1;
 	gpio->gpio_out_ao = -1;
-	gpio->irq = IRQ_GPIO0_NUM;
+	gpio->irq = IRQ_GPIO1_NUM;
 	gpio->trig_type	= GPIO_IRQ_FALLING_EDGE;
 	p->gpio_info_count = ++i;
 #endif
@@ -251,8 +251,8 @@ static unsigned int detect_key(unsigned int suspend_from)
 				exit_reason = POWER_KEY_WAKEUP;
 		}
 #ifdef CONFIG_BT_WAKEUP
-		if (irq[IRQ_GPIO0] == IRQ_GPIO0_NUM) {
-			irq[IRQ_GPIO0] = 0xFFFFFFFF;
+		if (irq[IRQ_GPIO1] == IRQ_GPIO1_NUM) {
+			irq[IRQ_GPIO1] = 0xFFFFFFFF;
 			if (!(readl(PREG_PAD_GPIO4_I) & (0x01 << 18))
 				&& (readl(PREG_PAD_GPIO4_O) & (0x01 << 17))
 				&& !(readl(PREG_PAD_GPIO4_EN_N) & (0x01 << 17)))
