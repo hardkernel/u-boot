1dfe3932bf8ddec130b80fedc2d73f2ae08621a4
Joseph Chen <chenjh@rock-chips.com>
2018-10-07 12:55:51 +0800

    rockchip: rk1808: support decomp lz4 kernel image
    Change-Id: Ia8867efe104fd59974b83c10770aece61f4643d7
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- include/configs/rk1808_common.h
+++ include/configs/rk1808_common.h
@@ -40,6 +40,7 @@
 	"pxefile_addr_r=0x00600000\0" \
 	"fdt_addr_r=0x01f00000\0" \
 	"kernel_addr_r=0x02080000\0" \
+	"kernel_addr_c=0x04080000\0" \
 	"ramdisk_addr_r=0x0a200000\0"
 
 #include <config_distro_bootcmd.h>
