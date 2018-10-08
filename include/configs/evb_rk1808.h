babab61fd115004f40be8806e4dba594c07613f9
Joseph Chen <chenjh@rock-chips.com>
2018-10-08 14:30:52 +0800

    rockchip: rk1808: define CONFIG_SYS_MMC_ENV_DEV as 0
    Change-Id: I34584893af47880d73b4016923af44a5fb6b7b1d
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- include/configs/evb_rk1808.h
+++ include/configs/evb_rk1808.h
@@ -13,6 +13,8 @@
 			"stdout=serial,vidconsole\0" \
 			"stderr=serial,vidconsole\0"
 
+#define CONFIG_SYS_MMC_ENV_DEV		0
+
 #ifndef CONFIG_SPL_BUILD
 #undef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND RKIMG_BOOTCOMMAND
