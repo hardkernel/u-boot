6096696b6d1464512bf271d71f4fc4e979ae978d
Dingqiang Lin <jon.lin@rock-chips.com>
2018-10-29 11:57:30 +0800

    rkflash: drivers: rkflash_debug code is adjusted to use block desc standard api
    Change-Id: I47d6fc0f0a4e660d0fa88cd72dcf2bffb8bb090c
    Signed-off-by: Dingqiang Lin <jon.lin@rock-chips.com>

--- drivers/rkflash/rkflash_debug.c
+++ drivers/rkflash/rkflash_debug.c
@@ -8,6 +8,7 @@
 
 #include "rkflash_debug.h"
 #include "rkflash_blk.h"
+#include "boot_rkimg.h"
 
 void rkflash_print_hex(char *s, void *buf, u32 width, u32 len)
 {
@@ -42,20 +43,24 @@ static u8 pread[max_test_sector * 512];
 static u32 *pwrite32;
 void blk_stress_test(struct udevice *udev)
 {
-	struct blk_desc *block_dev = dev_get_uclass_platdata(udev);
-	struct rkflash_info *priv = dev_get_priv(udev->parent);
+	struct blk_desc *block_dev = rockchip_get_bootdev();
 	u16 i, j, loop = 0;
 	u32 test_end_lba;
-	u32 test_lba = 0;
+	u32 test_lba;
 	u16 test_sec_count = 1;
 	u16 print_flag;
 
-	if (!priv || !block_dev) {
+	if (!block_dev) {
 		printf("device unknown\n");
 		return;
 	}
 
-	test_end_lba = priv->density;
+	if (block_dev->if_type == IF_TYPE_SPINOR)
+		test_lba = 0x800;
+	else
+		test_lba = 0;
+
+	test_end_lba = block_dev->lba;
 	pwrite32 = (u32 *)pwrite;
 	for (i = 0; i < (max_test_sector * 512); i++)
 		pwrite[i] = i;
@@ -65,8 +70,7 @@ void blk_stress_test(struct udevice *udev)
 		test_sec_count = 1;
 		printf("test_end_lba = %x\n", test_end_lba);
 		printf("test_lba = %x\n", test_lba);
-		for (test_lba = 0;
-		     (test_lba + test_sec_count) < test_end_lba;) {
+		for (; (test_lba + test_sec_count) < test_end_lba;) {
 			pwrite32[0] = test_lba;
 			blk_dwrite(block_dev, test_lba, test_sec_count, pwrite);
 			blk_dread(block_dev, test_lba, test_sec_count, pread);
@@ -101,8 +105,7 @@ void blk_stress_test(struct udevice *udev)
 		printf("---------Test ftl check---------\n");
 
 		test_sec_count = 1;
-		for (test_lba = 0;
-		     (test_lba + test_sec_count) < test_end_lba;) {
+		for (; (test_lba + test_sec_count) < test_end_lba;) {
 			pwrite32[0] = test_lba;
 			blk_dread(block_dev, test_lba, test_sec_count, pread);
 			print_flag = test_lba & 0x7FF;
