c7b9ee6b6f206c31475cfd7a6a965be5c07d9331
Kever Yang <kever.yang@rock-chips.com>
2018-11-09 11:18:15 +0800

    rockchip: rk3399: add tpl support in Kconfig
    Rockchp platform suppose to use TPL(run in SRAM) as dram init and
    SPL(run in DDR SDRAM) as pre-loader, so that the SPL would not be
    limited by SRAM size.
    
    Change-Id: Ib4115dbf6679fd4649e694d069a6489346112a97
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- arch/arm/include/asm/arch-rockchip/boot0.h
+++ arch/arm/include/asm/arch-rockchip/boot0.h
@@ -72,6 +72,6 @@ _start:
 	ARM_VECTORS
 #endif
 
-#if defined(CONFIG_SPL_BUILD) && (CONFIG_ROCKCHIP_SPL_RESERVE_IRAM > 0)
+#if !defined(CONFIG_TPL_BUILD) && defined(CONFIG_SPL_BUILD) && (CONFIG_ROCKCHIP_SPL_RESERVE_IRAM > 0)
 	.space CONFIG_ROCKCHIP_SPL_RESERVE_IRAM	/* space for the ATF data */
 #endif
