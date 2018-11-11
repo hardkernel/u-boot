5570e167ca4a758703fed3c05df591958bd09707
ruixuan.li <ruixuan.li@amlogic.com>
2018-11-11 20:00:43 +0800

    tl1: bringup: modify cophase to 3 [1/1]
    PD#172587
    
    Problem:
    operate emmc error
    
    Solution:
    modify cophase to 3
    
    Verify:
    test pass on tl1_skt
    
    Change-Id: I8ccb596ef64e87884f9dd14cbdb7d7e39a6c6db9
    Signed-off-by: ruixuan.li <ruixuan.li@amlogic.com>

--- arch/arm/cpu/armv8/tl1/sdio.c
+++ arch/arm/cpu/armv8/tl1/sdio.c
@@ -153,10 +153,8 @@ __weak void sd_emmc_para_config(struct sd_emmc_global_regs *reg,
 		unsigned int clock, unsigned int port)
 {
 	unsigned int clk = reg->gclock;
-	cpu_id_t cpuid = get_cpu_id();
 
-	if ((cpuid.chip_rev == 0xB)
-		&& (port == SDIO_PORT_C)) {
+	if (port == SDIO_PORT_C) {
 		clk &= ~(3 << Cfg_co_phase);
 		clk |= (3 << Cfg_co_phase);
 	}
