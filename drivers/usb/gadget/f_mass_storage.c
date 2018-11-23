57521aaaf8e6098197d33443c6e2133815c14e4a
Lukasz Majewski <lukma@denx.de>
2018-11-23 17:36:19 +0100

    UPSTREAM: usb: composite: Move bitmap related operations to ./include/linux/bitmap.h
    The BITMAP related operations can now be moved to ./include/linux/bitmap.h
    file to mimic the Linux kernel directory tree.
    
    This change also allows to remove the lin_gadget_compat.h header file
    (which is a legacy code only for composite U-boot layer).
    It was also possible to remove #includes from several USB gadget drivers.
    
    Conflicts:
    	include/usb/lin_gadget_compat.h
    
    Change-Id: Id61d6f9cef89ca238f082f430f6d01ac1009aa07
    Signed-off-by: Lukasz Majewski <lukma@denx.de>
    Reviewed-by: Stefan Agner <stefan.agner@toradex.com>
    (cherry picked from commit 916fa097997a5e1b70768ce944de28e038d4bebf)

--- drivers/usb/gadget/f_mass_storage.c
+++ drivers/usb/gadget/f_mass_storage.c
@@ -258,7 +258,7 @@
 #include <linux/usb/gadget.h>
 #include <linux/usb/gadget.h>
 #include <linux/usb/composite.h>
-#include <usb/lin_gadget_compat.h>
+#include <linux/bitmap.h>
 #include <g_dnl.h>
 
 /*------------------------------------------------------------------------*/
