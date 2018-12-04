95c293298449c60e3d988b261a18945532d25f14
xiaobo gu <xiaobo.gu@amlogic.com>
2018-12-04 17:44:08 +0800

    ddr: tl1 enable lpddr4 support [2/2]
    PD#SWPL-2861
    
    Problem:
    tl1 need enable lpddr4 support
    
    Solution:
    update lpddr4 timing
    
    Verify:
    test pass on x309
    
    Change-Id: Ieec9bef008d06e9746600217dcb7647ffa837770
    Signed-off-by: xiaobo gu <xiaobo.gu@amlogic.com>

--- board/amlogic/tl1_skt_v1/firmware/timing.c
+++ board/amlogic/tl1_skt_v1/firmware/timing.c
@@ -75,7 +75,6 @@
 #define DDR_RFC_TYPE_LPDDR4_4Gbx1				11
 
 ddr_set_t __ddr_setting[] = {
-/*
 {
 	//lpddr4
 	.board_id				= CONFIG_BOARD_ID_MASK,
@@ -84,39 +83,40 @@ ddr_set_t __ddr_setting[] = {
 	.dram_rank_config		= CONFIG_DDR0_32BIT_RANK0_CH0,
 	.ddr_rfc_type			= DDR_RFC_TYPE_LPDDR4_4Gbx1,
 	.DramType				= CONFIG_DDR_TYPE_LPDDR4,
-	.DRAMFreq				= {600, 0, 0, 0},
+	.DRAMFreq				= {1200, 0, 0, 0},
 	.ddr_base_addr			= CFG_DDR_BASE_ADDR,
 	.ddr_start_offset		= CFG_DDR_START_OFFSET,
 	.DisabledDbyte			= 0xf0,
 	.Is2Ttiming				= 0,
-	.HdtCtrl				= 0x4,
-	.dram_cs0_size_MB		= 1024,//1024,
+	.HdtCtrl				= 0xa,
+	.dram_cs0_size_MB		= 2048,//1024,
 	.dram_cs1_size_MB		= 0,//1024,
 	.training_SequenceCtrl	= {0x131f,0x61}, //ddr3 0x21f 0x31f
 	.phy_odt_config_rank	= {0x30,0x30,0x30,0x30}, // // Odt pattern for accesses //targeting rank 0. [3:0] is used //for write ODT [7:4] is used for //read ODT
 	.dfi_odt_config			= 0x0808,
 	.PllBypassEn			= 0, //bit0-ps0,bit1-ps1
-	.ddr_rdbi_wr_enable		= 0,
-	.clk_drv_ohm			= 40,
+	.ddr_rdbi_wr_enable		= 0x3,
+	.clk_drv_ohm			= 60,
 	.cs_drv_ohm				= 40,
-	.ac_drv_ohm				= 40,
-	.soc_data_drv_ohm_p		= 40,
-	.soc_data_drv_ohm_n		= 40,
+	.ac_drv_ohm				= 60,
+	.soc_data_drv_ohm_p		= 48,
+	.soc_data_drv_ohm_n		= 48,
 	.soc_data_odt_ohm_p		= 0,
-	.soc_data_odt_ohm_n		= 120,
-	.dram_data_drv_ohm		= 40, //lpddr4 sdram only240/1-6
+	.soc_data_odt_ohm_n		= 60,
+	.dram_data_drv_ohm		= 48, //lpddr4 sdram only240/1-6
 	.dram_data_odt_ohm		= 120,
-	.dram_ac_odt_ohm		= 120,
+	.dram_ac_odt_ohm		=120,// 120,
+	.lpddr4_dram_vout_voltage_1_3_2_5_setting   =0,///1, 1/3vddq     0 2/5 vddq
 	.soc_clk_slew_rate		= 0x3ff,//0x253,
 	.soc_cs_slew_rate		= 0x100,//0x253,
 	.soc_ac_slew_rate		= 0x100,//0x253,
 	.soc_data_slew_rate		= 0x1ff,
-	.vref_output_permil		= 0,//200,
+	.vref_output_permil		= 260,//200,
 	.vref_receiver_permil	= 0,
 	.vref_dram_permil		= 0,
 	//.vref_reverse			= 0,
-	.ac_trace_delay			= {00,0x0,0,0,0,0,0x0,00},
-	.ac_pinmux				= {00,00},
+	//.ac_trace_delay			= {32-5,0,0,0,0,0,0x0,00},
+	//.ac_pinmux				= {00,00},
 	.ddr_dmc_remap			= {
 							[0] = ( 5 |  6 << 5 |  7 << 10 |  8<< 15 | 9<< 20 | 10 << 25 ),
 							[1] = ( 11|  0 << 5 |  0 << 10 | 15 << 15 | 16 << 20 | 17 << 25 ),
@@ -125,14 +125,14 @@ ddr_set_t __ddr_setting[] = {
 							[4] = ( 30| 12 << 5 | 13 << 10 |  14<< 15 |  0 << 20 |  0 << 25 ),
 	},
 	.ddr_lpddr34_ca_remap	= {00,00},
-//	.ddr_lpddr34_dq_remap	= {3,2,0,1,7,6,5,4,14,13,12,15,8,9,11,10,20,21,22,23,16,17,19,18,24,25,28,26,31,30,27,29},
+	//.ddr_lpddr34_dq_remap	= {4,5,6,7,0,2,3,1,  15,14,13,12,11,9,10,8, 4,5,6,7,0,2,3,1,  15,14,13,12,11,9,10,8,},
+	.ddr_lpddr34_dq_remap	= {4,5,6,7,0,2,3,1,  15,14,13,12,11,9,10,8,   20,18,17,16,23,22,19,21,   30,31,25,24,26,28,29,27},
 	.dram_rtt_nom_wr_park	= {00,00},
 	.pll_ssc_mode			= (1<<20) | (1<<8) | (2<<4) | 0,//center_ssc_1000ppm
 	.ddr_func				= DDR_FUNC,
 	.magic					= DRAM_CFG_MAGIC,
 	.diagnose				= CONFIG_DIAGNOSE_DISABLE,
 },
-*/
 {
 	/// tl1 ref(x301) ddr3
 	.board_id				=CONFIG_BOARD_ID_MASK,
