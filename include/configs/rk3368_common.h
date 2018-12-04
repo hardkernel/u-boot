342ca48f820b3c062652e2cf967e740dac00d8ee
Joseph Chen <chenjh@rock-chips.com>
2018-12-04 17:28:03 +0800

    rk3368: set "kernel_addr_r" as 0x00280000
    load kernel as close as possible to the start of system RAM in
    order to make full use of memory for kernel.
    
    Change-Id: I199c01bc60dbb3107105e6ea669f64839ead0b80
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- include/configs/rk3368_common.h
+++ include/configs/rk3368_common.h
@@ -52,7 +52,7 @@
 	"scriptaddr=0x00500000\0" \
 	"pxefile_addr_r=0x00600000\0" \
 	"fdt_addr_r=0x01f00000\0" \
-	"kernel_addr_r=0x02080000\0" \
+	"kernel_addr_r=0x00280000\0" \
 	"ramdisk_addr_r=0x0a200000\0"
 
 #include <config_distro_bootcmd.h>
