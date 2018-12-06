36ba3f8d39ecc8eca0b0ed34154f08be164b93a3
Joseph Chen <chenjh@rock-chips.com>
2018-12-06 16:05:24 +0800

    lib: initcall: add time cost verbose
    This is more helpful than bootstage timing record since it
    is quite possible for every developer to enable inicall
    verbose for debug and find abnormal time cost as early as
    possible.
    
    It looks like:
      initcall: 000000000061015c
                                                                      #   183 us
      initcall: 000000000ff3e614
                                                                      #     0 us
      initcall: 000000000ff3e408
                                                                      #     0 us
      initcall: 00000000006105fc (relocated to 000000000ff3e5fc)
                                                                      #  6401 us
      initcall: 0000000000610420 (relocated to 000000000ff3e420)
                                                                      #     1 us
      initcall: 0000000000604444 (relocated to 000000000ff32444)
                                                                      #     5 us
    Change-Id: Ib59f3e5052f921d0046a6fa73cbfb968000fd34e
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- lib/initcall.c
+++ lib/initcall.c
@@ -10,9 +10,18 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+#define SYS_TICKS_TO_US(ticks)	 ((ticks) / (COUNTER_FREQUENCY / 1000000))
+
+#ifdef DEBUG
+static inline void call_get_ticks(ulong *ticks) { *ticks = get_ticks(); }
+#else
+static inline void call_get_ticks(ulong *ticks) { }
+#endif
+
 int initcall_run_list(const init_fnc_t init_sequence[])
 {
 	const init_fnc_t *init_fnc_ptr;
+	__maybe_unused ulong start = 0, end = 0;
 
 	for (init_fnc_ptr = init_sequence; *init_fnc_ptr; ++init_fnc_ptr) {
 		unsigned long reloc_ofs = 0;
@@ -28,7 +37,11 @@ int initcall_run_list(const init_fnc_t init_sequence[])
 			debug(" (relocated to %p)\n", (char *)*init_fnc_ptr);
 		else
 			debug("\n");
+		call_get_ticks(&start);
 		ret = (*init_fnc_ptr)();
+		call_get_ticks(&end);
+		if (start != end)
+			debug("\t\t\t\t\t\t\t\t#%6ld us\n", SYS_TICKS_TO_US(end - start));
 		if (ret) {
 			printf("initcall sequence %p failed at call %p (err=%d)\n",
 			       init_sequence,
