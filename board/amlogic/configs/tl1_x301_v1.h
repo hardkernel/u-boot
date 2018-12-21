56b84891c69ec2c6221af07e753d0c30c6cb3507
Evoke Zhang <evoke.zhang@amlogic.com>
2018-12-21 13:36:43 +0800

    x301: set model_name FHD2HDMI as default [1/1]
    PD#SWPL-3481
    
    Problem:
    in most cases used hdmi conversion board for lvds 1080p
    
    Solution:
    use FHD2HDMI model_name as default
    
    Verify:
    x301
    
    Change-Id: Ia116844e3db073a973823937fc243a7d05470497
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- board/amlogic/configs/tl1_x301_v1.h
+++ board/amlogic/configs/tl1_x301_v1.h
@@ -81,8 +81,8 @@
         "upgrade_step=0\0"\
         "jtag=disable\0"\
         "loadaddr=1080000\0"\
-        "model_name=FHD\0" \
-        "panel_type=lvds_0\0" \
+        "model_name=FHD2HDMI\0" \
+        "panel_type=lvds_1\0" \
         "outputmode=1080p60hz\0" \
         "hdmimode=1080p60hz\0" \
         "cvbsmode=576cvbs\0" \
