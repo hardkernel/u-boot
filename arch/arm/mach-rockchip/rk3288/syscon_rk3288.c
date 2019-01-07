de867820fcdbdc960d80f6e12968088f5b5ad1ad
Wyon Bi <bivvy.bi@rock-chips.com>
2019-01-07 21:06:44 +0800

    rockchip: rk3288: scan sub-nodes of the syscon node
    Change-Id: I74eec80993664173e57e0c03f662790cf9e66b5a
    Signed-off-by: Wyon Bi <bivvy.bi@rock-chips.com>

--- arch/arm/mach-rockchip/rk3288/syscon_rk3288.c
+++ arch/arm/mach-rockchip/rk3288/syscon_rk3288.c
@@ -22,6 +22,9 @@ U_BOOT_DRIVER(syscon_rk3288) = {
 	.name = "rk3288_syscon",
 	.id = UCLASS_SYSCON,
 	.of_match = rk3288_syscon_ids,
+#if !CONFIG_IS_ENABLED(OF_PLATDATA)
+	.bind = dm_scan_fdt_dev,
+#endif
 };
 
 #if CONFIG_IS_ENABLED(OF_PLATDATA)
