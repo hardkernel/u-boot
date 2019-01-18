1b0a5f9d2c0ffd70e9c2b5ae3cc614e6864afc20
Joseph Chen <chenjh@rock-chips.com>
2019-01-18 14:57:10 +0800

    include: rk3036: move fdt to 131M and ramdisk to 162M
    It avoids fdt overlap with large boot.img/recovery.img, moving
    fdt ahead of boot.img/recovery.img and moving ramdisk backward
    is safer.
    
    Change-Id: I6445227d6e36c4625a0c6badaf00c0cad0c9211c
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- include/configs/rk3066_common.h
+++ include/configs/rk3066_common.h
@@ -72,9 +72,9 @@
 #define ENV_MEM_LAYOUT_SETTINGS \
 	"scriptaddr=0x60000000\0" \
 	"pxefile_addr_r=0x60100000\0" \
-	"fdt_addr_r=0x61f00000\0" \
+	"fdt_addr_r=0x68300000\0" \
 	"kernel_addr_r=0x62000000\0" \
-	"ramdisk_addr_r=0x64000000\0"
+	"ramdisk_addr_r=0x6a200000\0"
 
 #include <config_distro_bootcmd.h>
 
