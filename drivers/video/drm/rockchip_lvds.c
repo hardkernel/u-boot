4888f8a48ab3553e384a7696c98b863ca15bbad6
Wyon Bi <bivvy.bi@rock-chips.com>
2019-02-18 17:27:46 +0800

    video/drm: lvds: Add support for MEDIA_BUS_FMT_RGB666_1X7X3_JEIDA bus format
    Change-Id: Id8b17e482036ce7c8eb543a673eb1b745958c7c3
    Signed-off-by: Wyon Bi <bivvy.bi@rock-chips.com>

--- drivers/video/drm/rockchip_lvds.c
+++ drivers/video/drm/rockchip_lvds.c
@@ -100,11 +100,14 @@ static int rockchip_lvds_connector_init(struct display_state *state)
 	lvds->phy = conn_state->phy;
 
 	switch (panel->bus_format) {
-	case MEDIA_BUS_FMT_RGB666_1X7X3_SPWG:	/* jeida-18 */
+	case MEDIA_BUS_FMT_RGB666_1X7X3_JEIDA:	/* jeida-18 */
 		lvds->format = LVDS_6BIT_MODE;
 		break;
 	case MEDIA_BUS_FMT_RGB888_1X7X4_JEIDA:	/* jeida-24 */
 		lvds->format = LVDS_8BIT_MODE_FORMAT_2;
+		break;
+	case MEDIA_BUS_FMT_RGB666_1X7X3_SPWG:	/* vesa-18 */
+		lvds->format = LVDS_8BIT_MODE_FORMAT_3;
 		break;
 	case MEDIA_BUS_FMT_RGB888_1X7X4_SPWG:	/* vesa-24 */
 	default:
