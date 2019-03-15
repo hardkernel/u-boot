1e312760bf711d1cb0248e15af07c1b1bd2d4d58
Joy Cho <joy.cho@hardkernel.com>
2019-03-15 10:55:35 +0900

    command: bmp: fix a warning
    Change-Id: Iac400b3e3d4cd3f7d195c02b0d8750efd3714bfe

--- common/cmd_bmp.c
+++ common/cmd_bmp.c
@@ -63,7 +63,7 @@ bmp_image_t *gunzip_bmp(unsigned long addr, unsigned long *lenp,
 	bmp = dst;
 
 	/* align to 32-bit-aligned-address + 2 */
-	bmp = (bmp_image_t *)((((unsigned int)dst + 1) & ~3) + 2);
+	bmp = (bmp_image_t *)((((phys_addr_t)dst + 1) & ~3) + 2);
 
 	if (gunzip(bmp, CONFIG_SYS_VIDEO_LOGO_MAX_SIZE, (uchar *)addr, &len) != 0) {
 		free(dst);
