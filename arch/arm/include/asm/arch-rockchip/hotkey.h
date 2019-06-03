866ecb69e8cb1422418857074bb11e13d5c32160
Joseph Chen <chenjh@rock-chips.com>
2019-06-03 18:18:17 +0800

    rockchip: hotkey: add ctrl+i and ctrl+p
    ctrl+i: enable initcall debug;
    ctrl+p: dump cmdline;
    
    Change-Id: Ie31f67a2a3fcb87c4057d56b62fbc1cc59397b86
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- arch/arm/include/asm/arch-rockchip/hotkey.h
+++ arch/arm/include/asm/arch-rockchip/hotkey.h
@@ -9,7 +9,9 @@
 enum hotkey_t {
 	HK_INVAL,
 	HK_BROM_DNL,
+	HK_CMDLINE,
 	HK_FASTBOOT,
+	HK_INITCALL,
 	HK_ROCKUSB_DNL,
 	HK_SYSMEM,
 };
--- arch/arm/mach-rockchip/hotkey.c
+++ arch/arm/mach-rockchip/hotkey.c
@@ -14,15 +14,21 @@ DECLARE_GLOBAL_DATA_PTR;
 #define CTRL_B		0x02
 #define CTRL_D		0x04
 #define CTRL_F		0x06
+#define CTRL_I		0x09
 #define CTRL_M		0x0d
+#define CTRL_P		0x10
 
 bool is_hotkey(enum hotkey_t id)
 {
 	switch (id) {
 	case HK_BROM_DNL:
 		return gd->console_evt == CTRL_B;
+	case HK_CMDLINE:
+		return gd->console_evt == CTRL_P;
 	case HK_FASTBOOT:
 		return gd->console_evt == CTRL_F;
+	case HK_INITCALL:
+		return gd->console_evt == CTRL_I;
 	case HK_ROCKUSB_DNL:
 		return gd->console_evt == CTRL_D;
 	case HK_SYSMEM:
@@ -42,6 +48,14 @@ void hotkey_run(enum hotkey_t id)
 			bidram_dump();
 			sysmem_dump();
 		}
+		break;
+	case HK_CMDLINE:
+		if (gd->console_evt == CTRL_P)
+			printf("cmdline: %s\n", env_get("bootargs"));
+		break;
+	case HK_INITCALL:
+		if (gd->console_evt == CTRL_I)
+			env_update("bootargs", "initcall_debug debug");
 		break;
 	default:
 		break;
