a931f195dda02961a1149233cddce86988398dda
Joy Cho <joy.cho@hardkernel.com>
2019-06-18 17:34:14 +0900

    board: odroidgo2: Turn on alive LED
    Change-Id: Ic4cec6a986f328f24109a10104e5f740f9c5084a

--- board/rockchip/odroidgo2/odroidgo2.c
+++ board/rockchip/odroidgo2/odroidgo2.c
@@ -7,5 +7,20 @@
 #include <common.h>
 #include <dm.h>
 #include <asm/io.h>
+#include <asm/gpio.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+#define ALIVE_LED_GPIO	17 /* GPIO0_C1 */
+void board_alive_led(void)
+{
+	gpio_request(ALIVE_LED_GPIO, "alive_led");
+	gpio_direction_output(ALIVE_LED_GPIO, 1);
+}
+
+int rk_board_late_init(void)
+{
+	board_alive_led();
+
+	return 0;
+}
