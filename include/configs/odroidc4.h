1823eb28b55e6d95789020a671872488c857b3dc
Luke Go <luke.go@hardkernel.com>
2019-06-19 11:46:06 +0900

    ODROID-C4: introduce new board ODROID-C4 and its board configuration
    Change-Id: I112d1c669784fd989405c6dfe788e084eb09785e

---
+++ include/configs/odroidc4.h
@@ -0,0 +1,17 @@
+/*
+ * include/configs/odroidc4.h
+ *
+ * (C) Copyright 2018 Hardkernel Co., Ltd
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef __ODROID_C4_H__
+#define __ODROID_C4_H__
+
+#define CONFIG_DEVICE_PRODUCT		"odroidc4"
+#define ODROID_BOARD_UUID		"9098004a-a1dd-11e8-98d0-529269fb1459"
+
+#include "odroid-g12-common.h"
+
+#endif
