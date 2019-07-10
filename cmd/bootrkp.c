14768307235d208fd8f1e3ec83ca0e94fe1bd357
Joseph Chen <chenjh@rock-chips.com>
2019-07-10 16:14:23 +0800

    cmd: bootrkp: clean up code
    Change-Id: Ic76a6cabca67263a792eece7ecc7ee46e521ef97
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- cmd/bootrkp.c
+++ cmd/bootrkp.c
@@ -5,23 +5,20 @@
  */
 
 #include <common.h>
+#include <boot_rkimg.h>
+#include <keymaster.h>
 #include <malloc.h>
 #include <android_bootloader.h>
 #include <attestation_key.h>
-#include <boot_rkimg.h>
-#include <keymaster.h>
-
-#define OEM_UNLOCK_ARG_SIZE 30
 
 static int do_boot_rockchip(cmd_tbl_t *cmdtp, int flag, int argc,
-		      char * const argv[])
+			    char *const argv[])
 {
+	char *boot_partname = PART_BOOT;
 	disk_partition_t part_info;
 	struct blk_desc *dev_desc;
-	int mode = 0;
-	char *boot_partname = PART_BOOT;
-	int ret = 0;
-	int i = 0;
+	int i, ret;
+	int mode;
 
 	dev_desc = rockchip_get_bootdev();
 	if (!dev_desc) {
@@ -30,51 +27,49 @@ static int do_boot_rockchip(cmd_tbl_t *cmdtp, int flag, int argc,
 	}
 
 #ifdef CONFIG_ANDROID_KEYMASTER_CA
-	disk_partition_t misc_part_info;
-
 	/* load attestation key from misc partition. */
-	ret = part_get_info_by_name(dev_desc, "misc",
-				    &misc_part_info);
+	ret = part_get_info_by_name(dev_desc, PART_MISC, &part_info);
 	if (ret < 0)
-		printf("%s Could not find misc partition\n", __func__);
+		printf("%s: Could not find misc partition\n", __func__);
 	else
-		load_attestation_key(dev_desc, &misc_part_info);
+		load_attestation_key(dev_desc, &part_info);
 #endif
 
 #ifdef CONFIG_FASTBOOT_OEM_UNLOCK
 	/* read oem unlock status and attach to bootargs */
-	uint8_t unlock = 0;
+	char oem_unlock[30] = {0};
 	TEEC_Result result;
-	char oem_unlock[OEM_UNLOCK_ARG_SIZE] = {0};
+	uint8_t unlock = 0;
+
 	result = trusty_read_oem_unlock(&unlock);
 	if (result) {
-		printf("read oem unlock status with error : 0x%x\n", result);
+		printf("%s: Read oem unlock status failed: %d\n",
+		       __func__, result);
 	} else {
-		snprintf(oem_unlock, OEM_UNLOCK_ARG_SIZE, "androidboot.oem_unlocked=%d", unlock);
+		snprintf(oem_unlock, sizeof(oem_unlock),
+			 "androidboot.oem_unlocked=%d", unlock);
 		env_update("bootargs", oem_unlock);
 	}
 #endif
 
 	mode = rockchip_get_boot_mode();
-	if (mode == BOOT_MODE_RECOVERY) {
+	if (mode == BOOT_MODE_RECOVERY)
 		boot_partname = PART_RECOVERY;
-		printf("%s boot from Recovery partition!\n", __func__);
-	}
 
 	for (i = 0; i < argc; i++) {
 		if (!strcmp(argv[i], "boot-recovery")) {
 			boot_partname = PART_RECOVERY;
-			printf("%s argv%d:%s boot from Recovery partition!\n",
-				__func__, i, argv[i]);
+			printf("Boot from Recovery partition\n");
 		}
 	}
 
 	ret = part_get_info_by_name(dev_desc, boot_partname, &part_info);
+	if (ret < 0) {
+		printf("%s: Could not find %s part\n", __func__, part_info.name);
+		return CMD_RET_FAILURE;
+	}
 
-	if(boot_rockchip_image(dev_desc, &part_info))
-		ret = CMD_RET_FAILURE;
-
-	return ret;
+	return boot_rockchip_image(dev_desc, &part_info) ? CMD_RET_FAILURE : 0;
 }
 
 U_BOOT_CMD(
@@ -86,33 +81,36 @@ U_BOOT_CMD(
 );
 
 static int do_rkimg_test(cmd_tbl_t *cmdtp, int flag, int argc,
-		      char * const argv[])
+			 char *const argv[])
 {
 	struct blk_desc *dev_desc;
-	u32* buffer;
-	int ret = 0;
+	u32 *buffer;
+	int ret;
 
 	dev_desc = blk_get_dev(argv[1], simple_strtoul(argv[2], NULL, 16));
+	if (!dev_desc) {
+		printf("%s: dev_desc is NULL!\n", __func__);
+		return CMD_RET_FAILURE;
+	}
 
+	/* Read one block from beginning of IDB data */
 	buffer = memalign(ARCH_DMA_MINALIGN, 1024);
-	/* Read one block from begining of IDB data */
 	ret = blk_dread(dev_desc, 64, 2, buffer);
 	if (ret != 2) {
-		printf("%s fail to read data from IDB\n", __func__);
+		printf("%s: Fail to read data from IDB\n", __func__);
 		free(buffer);
 		return CMD_RET_FAILURE;
 	}
 
-	if (buffer[0] == 0xFCDC8C3B){
-		printf("%s found IDB in SDcard\n", __func__);
-		ret = CMD_RET_SUCCESS;
+	if (buffer[0] == 0xFCDC8C3B) {
+		printf("Found IDB in SDcard\n");
 		if (0 == buffer[128 + 104 / 4]) /* TAG in IDB */
 			env_update("bootargs", "sdfwupdate");
 	}
 
 	free(buffer);
 
-	return ret;
+	return CMD_RET_SUCCESS;
 }
 
 U_BOOT_CMD(
