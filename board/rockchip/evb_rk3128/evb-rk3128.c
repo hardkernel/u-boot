68fff71bb34b08bb468e9177508271e0f25fdad8
Kever Yang <kever.yang@rock-chips.com>
2019-07-24 20:57:07 +0800

    rockchip: evb-rk3128: enable rk818 current limit early
    Some rk3128 board like fireprime is using rk818, and it may limit
    the current before change the pmic setting, the normal process
    need to enable the fuelguage and its dts which is too late,
    the U-Boot may HANG at regulator_power_on_init() in board_init().
    
    Change-Id: I7bd058d8bd8fac01377c3cef6b3681c1c6110ee7
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- board/rockchip/evb_rk3128/evb-rk3128.c
+++ board/rockchip/evb_rk3128/evb-rk3128.c
@@ -5,5 +5,24 @@
  */
 
 #include <common.h>
+#include <dm.h>
+#include <power/rk8xx_pmic.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+int board_early_init_r(void)
+{
+	struct udevice *pmic;
+	int ret;
+
+	ret = uclass_first_device_err(UCLASS_PMIC, &pmic);
+	if (ret)
+		return ret;
+
+	/* Increase USB input current to 2A */
+	ret = rk818_spl_configure_usb_input_current(pmic, 2000);
+	if (ret)
+		return ret;
+
+	return 0;
+}
