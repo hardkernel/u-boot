fbf99d9116409fd59686dfd3f891547b111d65cb
Joseph Chen <chenjh@rock-chips.com>
2019-07-30 15:15:45 +0800

    cmd: add "download" to enter rockusb/bootrom mode
    Effect order: rockusb => bootrom.
    
    Change-Id: Ie206f565e1c79e72c173c25f6970c93c8306332f
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

---
+++ cmd/download.c
@@ -0,0 +1,27 @@
+/*
+ * (C) Copyright 2019 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#include <common.h>
+#include <command.h>
+
+static int do_download(cmd_tbl_t *cmdtp, int flag,
+		       int argc, char * const argv[])
+{
+#ifdef CONFIG_CMD_ROCKUSB
+	run_command("rockusb 0 $devtype $devnum", 0);
+#endif
+#ifdef CONFIG_CMD_GO
+	printf("Enter rockusb failed, fallback to bootrom...\n");
+	flushc();
+	run_command("rbrom", 0);
+#endif
+	return 0;
+}
+
+U_BOOT_CMD(
+	download, 1, 1, do_download,
+	"enter rockusb/bootrom download mode", ""
+);
