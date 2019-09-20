7f1fb2f87ad034585fcf0f48a8164f91318395da
Joseph Chen <chenjh@rock-chips.com>
2019-09-20 15:27:36 +0800

    dm: i2c: add alias seq print
    Change-Id: Ia4a415041389388e550057e476aa30daed696c86
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- drivers/i2c/i2c-uclass.c
+++ drivers/i2c/i2c-uclass.c
@@ -489,9 +489,15 @@ static int i2c_post_probe(struct udevice *dev)
 {
 #if CONFIG_IS_ENABLED(OF_CONTROL)
 	struct dm_i2c_bus *i2c = dev_get_uclass_priv(dev);
+	int seq;
 
 	i2c->speed_hz = dev_read_u32_default(dev, "clock-frequency", 100000);
-	printf("I2c speed: %dHz\n", i2c->speed_hz);
+
+	if (dev_read_alias_seq(dev, &seq) >= 0)
+		printf("I2c%d speed: %dHz\n", seq, i2c->speed_hz);
+	else
+		printf("I2c speed: %dHz\n", i2c->speed_hz);
+
 	return dm_i2c_set_bus_speed(dev, i2c->speed_hz);
 #else
 	return 0;
