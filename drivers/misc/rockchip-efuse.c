492844a318dc286e332f639c4c7824811453725b
Finley Xiao <finley.xiao@rock-chips.com>
2019-11-26 16:48:16 +0800

    rockchip: efuse: Add rockchip_rk3288_efuse_secure_read() for rk3288
    The 256-bit and 1024-bit efuse are always secure.
    
    Change-Id: I763a745360952991e63785bcacf0b63e859e60f8
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>

--- drivers/misc/rockchip-efuse.c
+++ drivers/misc/rockchip-efuse.c
@@ -12,9 +12,11 @@
 #include <command.h>
 #include <display_options.h>
 #include <dm.h>
+#include <linux/arm-smccc.h>
 #include <linux/bitops.h>
 #include <linux/delay.h>
 #include <misc.h>
+#include <asm/arch/rockchip_smccc.h>
 
 #define T_CSB_P_S		0
 #define T_PGENB_P_S		0
@@ -262,6 +264,57 @@ static int rockchip_rk3288_efuse_read(struct udevice *dev, int offset,
 	return 0;
 }
 
+#ifndef CONFIG_SPL_BUILD
+static int rockchip_rk3288_efuse_secure_read(struct udevice *dev, int offset,
+					     void *buf, int size)
+{
+	struct rockchip_efuse_platdata *plat = dev_get_platdata(dev);
+	struct rockchip_efuse_regs *efuse =
+		(struct rockchip_efuse_regs *)plat->base;
+	u8 *buffer = buf;
+	int max_size = RK3288_NFUSES * RK3288_BYTES_PER_FUSE;
+	struct arm_smccc_res res;
+
+	if (size > (max_size - offset))
+		size = max_size - offset;
+
+	/* Switch to read mode */
+	sip_smc_secure_reg_write((ulong)&efuse->ctrl,
+				 RK3288_LOAD | RK3288_PGENB);
+	udelay(1);
+	while (size--) {
+		res = sip_smc_secure_reg_read((ulong)&efuse->ctrl);
+		sip_smc_secure_reg_write((ulong)&efuse->ctrl, res.a1 &
+					 (~(RK3288_A_MASK << RK3288_A_SHIFT)));
+		/* set addr */
+		res = sip_smc_secure_reg_read((ulong)&efuse->ctrl);
+		sip_smc_secure_reg_write((ulong)&efuse->ctrl, res.a1 |
+					 ((offset++ & RK3288_A_MASK) <<
+					  RK3288_A_SHIFT));
+		udelay(1);
+		/* strobe low to high */
+		res = sip_smc_secure_reg_read((ulong)&efuse->ctrl);
+		sip_smc_secure_reg_write((ulong)&efuse->ctrl,
+					 res.a1 | RK3288_STROBE);
+		ndelay(60);
+		/* read data */
+		res = sip_smc_secure_reg_read((ulong)&efuse->dout);
+		*buffer++ = res.a1;
+		/* reset strobe to low */
+		res = sip_smc_secure_reg_read((ulong)&efuse->ctrl);
+		sip_smc_secure_reg_write((ulong)&efuse->ctrl,
+					 res.a1 & (~RK3288_STROBE));
+		udelay(1);
+	}
+
+	/* Switch to standby mode */
+	sip_smc_secure_reg_write((ulong)&efuse->ctrl,
+				 RK3288_PGENB | RK3288_CSB);
+
+	return 0;
+}
+#endif
+
 static int rockchip_rk3328_efuse_read(struct udevice *dev, int offset,
 				      void *buf, int size)
 {
@@ -342,10 +395,12 @@ static const struct udevice_id rockchip_efuse_ids[] = {
 		.compatible = "rockchip,rk1808-efuse",
 		.data = (ulong)&rockchip_rk1808_efuse_read,
 	},
+#ifndef CONFIG_SPL_BUILD
 	{
-		.compatible = "rockchip,rockchip-efuse",
-		.data = (ulong)&rockchip_rk3288_efuse_read,
+		.compatible = "rockchip,rk3288-secure-efuse",
+		.data = (ulong)&rockchip_rk3288_efuse_secure_read,
 	},
+#endif
 	{
 		.compatible = "rockchip,rk3066a-efuse",
 		.data = (ulong)&rockchip_rk3288_efuse_read,
