45390edfba72a37acf54017d3be4758e6c40f680
Joseph Chen <chenjh@rock-chips.com>
2020-02-07 11:59:09 +0800

    rockchip: resource: optimise code
    - clean init_resource_list();
    - support create resource list and read file from memory
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: If308c1dc6f43d0250d81cfa6fb0898d59efe8975

--- arch/arm/include/asm/arch-rockchip/resource_img.h
+++ arch/arm/include/asm/arch-rockchip/resource_img.h
@@ -8,6 +8,25 @@
 #define __RESC_IMG_H_
 
 /*
+ * resource_image_check_header - check resource image header
+ *
+ * @rsce_hdr: resource file hdr
+ *
+ * return 0 on header okay, otherwise failed
+ */
+int resource_image_check_header(void *rsce_hdr);
+
+/*
+ * resource_create_ram_list - create resource file list by data from memory
+ *
+ * @dev_desc: blk dev descritpion
+ * @rsce_hdr: resource file hdr
+ *
+ * return 0 on header okay, otherwise failed
+ */
+int resource_create_ram_list(struct blk_desc *dev_desc, void *rsce_hdr);
+
+/*
  * rockchip_read_resource_file - read file from resource partition
  *
  * @buf: destination buf to store file data
