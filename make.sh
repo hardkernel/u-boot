c61a6715316f2e2f5e5a1f186b29f40430344147
Joseph Chen <chenjh@rock-chips.com>
2020-02-21 10:33:18 +0800

    make.sh: rename rv1109 to rv1126
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I22687b0475f4fe8acf39c49276813104eb7e9627

--- make.sh
+++ make.sh
@@ -361,8 +361,8 @@ select_chip_info()
 			&& RKCHIP=RK3368H
 		grep '^CONFIG_ROCKCHIP_RV1108=y' ${OUTDIR}/.config >/dev/null \
 			&& RKCHIP=RV110X
-		grep '^CONFIG_ROCKCHIP_RV1109=y' ${OUTDIR}/.config >/dev/null \
-			&& RKCHIP=RV1109
+		grep '^CONFIG_ROCKCHIP_RV1126=y' ${OUTDIR}/.config >/dev/null \
+			&& RKCHIP=RV1126
 	elif [ $count -gt 1 ]; then
 		# Grep the RK CHIP variant
 		grep '^CONFIG_ROCKCHIP_PX3SE=y' ${OUTDIR}/.config > /dev/null \
