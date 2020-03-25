7b515e87eeabc88ee46d29f61ead8ef74e0837df
Joseph Chen <chenjh@rock-chips.com>
2020-03-25 16:44:53 +0800

    rockchip: rv1126: syscon add pmugrf and scan fdt
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I288566a68876c02aae8470a0b9aca2974ef16fe6

--- arch/arm/mach-rockchip/rv1126/syscon_rv1126.c
+++ arch/arm/mach-rockchip/rv1126/syscon_rv1126.c
@@ -11,6 +11,7 @@
 
 static const struct udevice_id rv1126_syscon_ids[] = {
 	{ .compatible = "rockchip,rv1126-grf", .data = ROCKCHIP_SYSCON_GRF },
+	{ .compatible = "rockchip,rv1126-pmugrf", .data = ROCKCHIP_SYSCON_PMUGRF },
 	{ }
 };
 
@@ -18,5 +19,7 @@ U_BOOT_DRIVER(syscon_rv1126) = {
 	.name = "rv1126_syscon",
 	.id = UCLASS_SYSCON,
 	.of_match = rv1126_syscon_ids,
+#if !CONFIG_IS_ENABLED(OF_PLATDATA)
+	.bind = dm_scan_fdt_dev,
+#endif
 };
-
