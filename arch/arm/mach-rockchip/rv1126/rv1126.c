3edf1a0ecf69499b00440292ac95dc8192d36828
Jason Zhu <jason.zhu@rock-chips.com>
2020-03-28 17:21:32 +0800

    rockchip: rv1126: set the region 0 unsecure
    Signed-off-by: Jason Zhu <jason.zhu@rock-chips.com>
    Change-Id: I9fd0a35571b339cf1f171b9a188a36f9309fdb0e

--- arch/arm/mach-rockchip/rv1126/rv1126.c
+++ arch/arm/mach-rockchip/rv1126/rv1126.c
@@ -10,9 +10,7 @@
 DECLARE_GLOBAL_DATA_PTR;
 
 #define FIREWALL_APB_BASE	0xffa60000
-#define FW_DDR_BASE		FIREWALL_APB_BASE
 #define FW_DDR_CON_REG		0x80
-#define DDR_RGN(x)		(0x20 + (x) * 4)
 
 void board_debug_uart_init(void)
 {
@@ -22,13 +20,8 @@ void board_debug_uart_init(void)
 #if defined(CONFIG_SPL_BUILD)
 int arch_cpu_init(void)
 {
-	int i;
-
-	/* Close the firewall, so that some devices can access to DDR */
+	/* Just set region 0 to unsecure */
 	writel(0, FIREWALL_APB_BASE + FW_DDR_CON_REG);
-	for (i = 0; i < 9; i++) {
-		writel(0, FW_DDR_BASE + DDR_RGN(i));
-	}
 
 	return 0;
 }
