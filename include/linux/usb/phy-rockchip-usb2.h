6037440f1237dd5604f31ae36a5eb2f742c31021
Jianing Ren <jianing.ren@rock-chips.com>
2020-04-02 17:30:57 +0800

    include: usb: add naneng usb2.0 phy support
    There are there rockchip USB2.0 PHYs can support USB BC1.2,
    respectively Synopsys USB2.0 PHY, Innosilicon USB2.0 PHY and
    Naneng USB2.0 PHY. So we rename the file to phy-rockchip-usb2.h.
    
    Change-Id: I561efc178b5168965c41098dd5b749f3d5083851
    Signed-off-by: Jianing Ren <jianing.ren@rock-chips.com>

--- include/linux/usb/phy-rockchip-usb2.h
+++ include/linux/usb/phy-rockchip-usb2.h
@@ -4,12 +4,15 @@
  * SPDX-License-Identifier:    GPL-2.0+
  */
 
-#ifndef _PHY_ROCKCHIP_INNO_USB2_H
-#define _PHY_ROCKCHIP_INNO_USB2_H
+#ifndef _PHY_ROCKCHIP_USB2_H
+#define _PHY_ROCKCHIP_USB2_H
 
 extern int rockchip_chg_get_type(void);
 
-#if defined(CONFIG_PHY_ROCKCHIP_INNO_USB2) || defined(CONFIG_ROCKCHIP_USB2_PHY)
+#if defined(CONFIG_PHY_ROCKCHIP_INNO_USB2) ||\
+    defined(CONFIG_ROCKCHIP_USB2_PHY) ||\
+    defined(CONFIG_PHY_ROCKCHIP_NANENG_USB2)
+
 int rockchip_u2phy_vbus_detect(void);
 #else
 static inline int rockchip_u2phy_vbus_detect(void)
@@ -18,4 +21,4 @@ static inline int rockchip_u2phy_vbus_detect(void)
 }
 #endif
 
-#endif /* _PHY_ROCKCHIP_INNO_USB2_H */
+#endif /* _PHY_ROCKCHIP_USB2_H */
