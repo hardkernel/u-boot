eaa66b0d6a7c944700f8c75ae91690069378513f
Joseph Chen <chenjh@rock-chips.com>
2020-05-22 18:02:06 +0800

    tools: fit: add "/totalsize" property for itb size
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I1aa87d5c0e1dd39762c470b134c4d8a57cab4f29

--- tools/fit_image.c
+++ tools/fit_image.c
@@ -99,7 +99,8 @@ static int fit_add_file_data(struct image_tool_params *params, size_t size_inc,
 	/* for first image creation, add a timestamp at offset 0 i.e., root  */
 	if (params->datafile) {
 		time_t time = imagetool_get_source_date(params, sbuf.st_mtime);
-		ret = fit_set_timestamp(ptr, 0, time);
+		ret  = fit_set_timestamp(ptr, 0, time);
+		ret |= fit_set_totalsize(ptr, 0, sbuf.st_size);
 	}
 
 	if (!ret) {
