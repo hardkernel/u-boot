e49e7bf9b1615d68b8dd8786987268ec61ed4d68
Joseph Chen <chenjh@rock-chips.com>
2020-07-07 15:48:11 +0800

    spl: remove unused variable "rollback_index"
    We prefer use "gd->rollback_index".
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I45bc64e38224064797d8d8bd89d9d3881f8793b5

--- common/spl/spl_fit.c
+++ common/spl/spl_fit.c
@@ -436,7 +436,6 @@ static int spl_internal_load_simple_fit(struct spl_image_info *spl_image,
 		return -EINVAL;
 	}
 
-	spl_image->rollback_index = this_index;
 	printf("rollback index: %d >= %d(min), OK\n", this_index, min_index);
 #endif
 #endif
