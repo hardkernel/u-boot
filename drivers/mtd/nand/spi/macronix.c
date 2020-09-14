247c5a81b3ce004a415b26a55cc621d78a542104
Jon Lin <jon.lin@rock-chips.com>
2020-09-14 09:47:58 +0800

    mtd: spinand: Add initial support for the MX35LF4GE4AD
    Change-Id: Ib1228650e76dc82bc86fb28472616d0fefb269bf
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/macronix.c
+++ drivers/mtd/nand/spi/macronix.c
@@ -121,6 +121,15 @@ static const struct spinand_info macronix_spinand_table[] = {
 		     SPINAND_HAS_QE_BIT,
 		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout,
 				     mx35lf1ge4ab_ecc_get_status)),
+	SPINAND_INFO("MX35LF4GE4AD", 0x37,
+		     NAND_MEMORG(1, 4096, 128, 64, 2048, 2, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout,
+				     mx35lf1ge4ab_ecc_get_status)),
 };
 
 static int macronix_spinand_detect(struct spinand_device *spinand)
