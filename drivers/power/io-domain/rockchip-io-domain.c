230491661df12761fddf3e4ed2172e15fd04657d
Jianqun Xu <jay.xu@rock-chips.com>
2020-10-23 10:30:48 +0800

    io-domain: rockchip: add rk3568 support
    Change-Id: Ic3a984043e82bd65957239acc25de79e00e1a6b8
    Signed-off-by: Jianqun Xu <jay.xu@rock-chips.com>

--- drivers/power/io-domain/rockchip-io-domain.c
+++ drivers/power/io-domain/rockchip-io-domain.c
@@ -58,6 +58,10 @@
 #define RK3399_PMUGRF_CON0_VSEL		BIT(8)
 #define RK3399_PMUGRF_VSEL_SUPPLY_NUM	9
 
+#define RK3568_PMU_GRF_IO_VSEL0		(0x0124)
+#define RK3568_PMU_GRF_IO_VSEL1		(0x0128)
+#define RK3568_PMU_GRF_IO_VSEL2		(0x012C)
+
 struct rockchip_iodomain_priv;
 
 /**
@@ -79,6 +83,7 @@ struct rockchip_iodomain_priv {
 	struct regmap *regmap_base;
 	struct rockchip_iodomain_soc_data *sdata;
 	struct rockchip_iodomain_supply supplies[MAX_SUPPLIES];
+	int (*write)(struct rockchip_iodomain_supply *supply, int uV);
 };
 
 static int rockchip_ofdata_to_platdata(struct udevice *dev)
@@ -93,6 +98,47 @@ static int rockchip_ofdata_to_platdata(struct udevice *dev)
 		return PTR_ERR(regmap);
 
 	priv->regmap_base = regmap;
+
+	return 0;
+}
+
+static int rk3568_pmu_iodomain_write(struct rockchip_iodomain_supply *supply,
+				     int uV)
+{
+	struct rockchip_iodomain_priv *priv = supply->iod;
+	struct regmap *regmap = priv->regmap_base;
+	u32 is_3v3 = uV > MAX_VOLTAGE_1_8;
+	u32 val0, val1;
+	int b;
+
+	switch (supply->idx) {
+	case 0: /* pmuio1 */
+	case 1: /* pmuio2 */
+		b = supply->idx;
+		val0 = BIT(16 + b) | (is_3v3 ? 0 : BIT(b));
+		b = supply->idx + 4;
+		val1 = BIT(16 + b) | (is_3v3 ? BIT(b) : 0);
+
+		regmap_write(regmap, RK3568_PMU_GRF_IO_VSEL2, val0);
+		regmap_write(regmap, RK3568_PMU_GRF_IO_VSEL2, val1);
+		break;
+	case 2: /* vccio1 */
+	case 3: /* vccio2 */
+	case 4: /* vccio3 */
+	case 5: /* vccio4 */
+	case 6: /* vccio5 */
+	case 7: /* vccio6 */
+	case 8: /* vccio7 */
+		b = supply->idx - 1;
+		val0 = BIT(16 + b) | (is_3v3 ? 0 : BIT(b));
+		val1 = BIT(16 + b) | (is_3v3 ? BIT(b) : 0);
+
+		regmap_write(regmap, RK3568_PMU_GRF_IO_VSEL0, val0);
+		regmap_write(regmap, RK3568_PMU_GRF_IO_VSEL1, val1);
+		break;
+	default:
+		return -EINVAL;
+	};
 
 	return 0;
 }
@@ -441,6 +487,21 @@ static const struct rockchip_iodomain_soc_data soc_data_rk3399_pmu = {
 	.init = rk3399_pmu_iodomain_init,
 };
 
+static const struct rockchip_iodomain_soc_data soc_data_rk3568_pmu = {
+	.grf_offset = 0x124,
+	.supply_names = {
+		"pmuio0",
+		"pmuio1",
+		"vccio1",
+		"vccio2",
+		"vccio3",
+		"vccio4",
+		"vccio5",
+		"vccio6",
+		"vccio7",
+	},
+};
+
 static const struct rockchip_iodomain_soc_data soc_data_rv1108 = {
 	.grf_offset = 0x404,
 	.supply_names = {
@@ -521,6 +582,10 @@ static int rockchip_iodomain_probe(struct udevice *dev)
 
 	sdata = (struct rockchip_iodomain_soc_data *)dev_get_driver_data(dev);
 	priv->sdata = sdata;
+	if (sdata == &soc_data_rk3568_pmu)
+		priv->write = rk3568_pmu_iodomain_write;
+	else
+		priv->write = rockchip_iodomain_write;
 
 	if (!priv->regmap_base)
 		return -1;
@@ -613,6 +678,10 @@ static const struct udevice_id rockchip_iodomain_match[] = {
 	{
 		.compatible = "rockchip,rk3399-pmu-io-voltage-domain",
 		.data = (ulong)&soc_data_rk3399_pmu
+	},
+	{
+		.compatible = "rockchip,rk3568-pmu-io-voltage-domain",
+		.data = (ulong)&soc_data_rk3568_pmu
 	},
 	{
 		.compatible = "rockchip,rv1108-io-voltage-domain",
