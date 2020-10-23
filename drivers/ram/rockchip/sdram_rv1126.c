e8885e2486caea6bd420f2af4751e670a43357f1
CanYang He <hcy@rock-chips.com>
2020-10-23 11:17:28 +0800

    drivers: ram: rv1126: dram 32bit interface use pageclose
    after system test, 32bit interface use pageclose can improve
    performance, 16bit interface not improve.
    
    Change-Id: Iecac7aae1e5f8ec4f162200d80be16f1b91180f5
    Signed-off-by: CanYang He <hcy@rock-chips.com>

--- drivers/ram/rockchip/sdram_rv1126.c
+++ drivers/ram/rockchip/sdram_rv1126.c
@@ -2194,6 +2194,12 @@ static int sdram_init_(struct dram_info *dram,
 	pctl_cfg(dram->pctl, &sdram_params->pctl_regs,
 		 dram->sr_idle, dram->pd_idle);
 
+	if (sdram_params->ch.cap_info.bw == 2)
+		/* 32bit interface use pageclose */
+		setbits_le32(pctl_base + DDR_PCTL2_SCHED, 1 << 2);
+	else
+		clrbits_le32(pctl_base + DDR_PCTL2_SCHED, 1 << 2);
+
 #ifdef CONFIG_ROCKCHIP_DRAM_EXTENDED_TEMP_SUPPORT
 	u32 tmp, trefi;
 
