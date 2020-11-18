2d25c32e077904ed34e65f00257be2f5d360d141
Joseph Chen <chenjh@rock-chips.com>
2020-11-18 16:25:45 +0800

    rockchip: dts: rk3568: Resync from kernel-4.19
    Resync from kernel-4.19:
    (2f153f1fa73c arm64: dts: rockchip: rk3568: add thermal-zone for pvtm)
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I36fdfc366f4d44f3226b6f8b35ee496701fe021e

--- include/dt-bindings/soc/rockchip,boot-mode.h
+++ include/dt-bindings/soc/rockchip,boot-mode.h
@@ -1,7 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef __ROCKCHIP_BOOT_MODE_H
 #define __ROCKCHIP_BOOT_MODE_H
 
-/* high 24 bits is tag, low 8 bits is type */
+/*high 24 bits is tag, low 8 bits is type*/
 #define REBOOT_FLAG		0x5242C300
 /* normal boot */
 #define BOOT_NORMAL		(REBOOT_FLAG + 0)
@@ -9,6 +10,10 @@
 #define BOOT_BL_DOWNLOAD	(REBOOT_FLAG + 1)
 /* enter recovery */
 #define BOOT_RECOVERY		(REBOOT_FLAG + 3)
+/* reboot by panic */
+#define BOOT_PANIC		(REBOOT_FLAG + 7)
+/* reboot by watchdog */
+#define BOOT_WATCHDOG		(REBOOT_FLAG + 8)
 /* enter fastboot mode */
 #define BOOT_FASTBOOT		(REBOOT_FLAG + 9)
 /* enter charging mode */
