cc7c06a279508f59d81b6ae8d097ab81215636d1
Joseph Chen <chenjh@rock-chips.com>
2021-03-02 09:40:54 +0800

    include: global_data: add GD_FLG_KDTB_READY flag
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I7a9c1b58e20838110a73060839dd8b1bb4854932

--- include/asm-generic/global_data.h
+++ include/asm-generic/global_data.h
@@ -180,6 +180,7 @@ typedef struct global_data {
 #define GD_FLG_ENV_DEFAULT	0x02000 /* Default variable flag	   */
 #define GD_FLG_SPL_EARLY_INIT	0x04000 /* Early SPL init is done	   */
 #define GD_FLG_LOG_READY	0x08000 /* Log system is ready for use	   */
+#define GD_FLG_KDTB_READY	0x10000 /* Kernel dtb is ready for use	   */
 
 #ifdef CONFIG_ARCH_ROCKCHIP
 /* BL32 is enabled */
