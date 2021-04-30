54f3fd85dd86947ad682d8f067019d0a0113c305
Jason Zhu <jason.zhu@rock-chips.com>
2021-04-30 17:00:55 +0800

    common: android: delete reset_cpu_if_android_ab()
    Signed-off-by: Jason Zhu <jason.zhu@rock-chips.com>
    Change-Id: I6910c9c0b320bf3578d6e7c19ed35323039e6cfb

--- common/android_bootloader.c
+++ common/android_bootloader.c
@@ -31,22 +31,6 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#if defined(CONFIG_ANDROID_AB) && defined(CONFIG_ANDROID_AVB)
-static void reset_cpu_if_android_ab(void)
-{
-	printf("Reset in AB system.\n");
-	flushc();
-	/*
-	 * Since we use the retry-count in ab system, then can
-	 * try reboot if verify fail until the retry-count is
-	 * equal to zero.
-	 */
-	reset_cpu(0);
-}
-#else
-static inline void reset_cpu_if_android_ab(void) {}
-#endif
-
 int android_bootloader_message_load(
 	struct blk_desc *dev_desc,
 	const disk_partition_t *part_info,
@@ -1020,7 +1004,6 @@ int android_bootloader_boot_flow(struct blk_desc *dev_desc,
 		if (android_slot_verify(boot_partname, &load_address,
 					slot_suffix)) {
 			printf("AVB verify failed\n");
-			reset_cpu_if_android_ab();
 
 			return -1;
 		}
@@ -1043,7 +1026,6 @@ int android_bootloader_boot_flow(struct blk_desc *dev_desc,
 			if (android_slot_verify(boot_partname, &load_address,
 						slot_suffix)) {
 				printf("AVB verify failed\n");
-				reset_cpu_if_android_ab();
 
 				return -1;
 			}
