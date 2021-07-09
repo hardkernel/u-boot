7545a1ec89b80315ac3032a3dbe08f8aace93d13
Joseph Chen <chenjh@rock-chips.com>
2021-07-09 18:07:15 +0800

    scripts: stacktrace: use '{}' for variables
    Rename "SYMBOL_FILE" to "SYM_FILE".
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I8e7affe51cab41b60306745777f0efcc37ae0ecd

--- scripts/stacktrace.sh
+++ scripts/stacktrace.sh
@@ -6,24 +6,23 @@
 #
 
 set -e
-ARGS_N=$#
+
 INPUT_FILE=$1
 TPL_SPL=$2
 
-
-if [ "$TPL_SPL" = "tpl" ]; then
-	SYMBOL_FILE=`find -name u-boot-tpl.sym`
+if [ "${TPL_SPL}" = "tpl" ]; then
+	SYM_FILE=`find -name u-boot-tpl.sym`
 	ELF_FILE="elf tpl"
-elif [ "$TPL_SPL" = "spl" ]; then
-	SYMBOL_FILE=`find -name u-boot-spl.sym`
+elif [ "${TPL_SPL}" = "spl" ]; then
+	SYM_FILE=`find -name u-boot-spl.sym`
 	ELF_FILE="elf spl"
 else
-	SYMBOL_FILE=`find -name u-boot.sym`
+	SYM_FILE=`find -name u-boot.sym`
 	ELF_FILE="elf"
 fi
 
 echo
-if [ $ARGS_N -eq 0 ]; then
+if [ $# -eq 0 ]; then
 	echo "Usage: "
 	echo "	./scripts/stacktrace.sh <file> <type>"
 	echo
@@ -37,34 +36,34 @@ if [ $ARGS_N -eq 0 ]; then
 	echo "	./scripts/stacktrace.sh ./dump.txt spl"
 	echo
 	exit 1
-elif [ ! -f $INPUT_FILE ]; then
-	echo "Can't find input file: $INPUT_FILE"
+elif [ ! -f ${INPUT_FILE} ]; then
+	echo "ERROR: No ${INPUT_FILE}"
 	exit 1
-elif [ "$SYMBOL_FILE" = '' ] || [ ! -f $SYMBOL_FILE ]; then
-	echo "Can't find symbol file: ${SYMBOL_FILE}"
+elif [ "${SYM_FILE}" = '' ] || [ ! -f ${SYM_FILE} ]; then
+	echo "ERROR: No ${SYM_FILE}"
 	exit 1
 fi
 
-echo "SYMBOL File: ${SYMBOL_FILE}"
+echo "SYMBOL File: ${SYM_FILE}"
 echo
 # Parse PC and LR
 echo "Call trace:"
-grep '\[< ' $INPUT_FILE | grep '>\]' | grep [PC,LR] | while read line
+grep '\[< ' ${INPUT_FILE} | grep '>\]' | grep [PC,LR] | while read line
 do
 	echo -n " ${line}  "
 
-	frame_pc_str=`echo $line | awk '{ print "0x"$3 }'`
-	frame_pc_dec=`echo $line | awk '{ print strtonum("0x"$3); }'`
+	frame_pc_str=`echo ${line} | awk '{ print "0x"$3 }'`
+	frame_pc_dec=`echo ${line} | awk '{ print strtonum("0x"$3); }'`
 	frame_pc_hex=`echo "obase=16;${frame_pc_dec}"|bc |tr '[A-Z]' '[a-z]'`
 
-	f_pc_dec=`cat ${SYMBOL_FILE} | sort | awk '/\.text/ { if (strtonum("0x"$1) > '$frame_pc_str') { print fpc; exit; } fpc=strtonum("0x"$1); }'`
+	f_pc_dec=`cat ${SYM_FILE} | sort | awk '/\.text/ { if (strtonum("0x"$1) > '${frame_pc_str}') { print fpc; exit; } fpc=strtonum("0x"$1); }'`
 	f_pc_hex=`echo "obase=16;${f_pc_dec}"|bc |tr '[A-Z]' '[a-z]'`
 	f_offset_dec=$((frame_pc_dec-f_pc_dec))
 	f_offset_hex=`echo "obase=16;${f_offset_dec}"|bc |tr '[A-Z]' '[a-z]'`
 
-	cat ${SYMBOL_FILE} | sort |
-	awk -v foffset=$f_offset_hex '/\.text/ {
-		if (strtonum("0x"$1) > '$frame_pc_str') {
+	cat ${SYM_FILE} | sort |
+	awk -v foffset=${f_offset_hex} '/\.text/ {
+		if (strtonum("0x"$1) > '${frame_pc_str}') {
 			printf("%s+0x%s/0x%x      ", fname, foffset, fsize);
 			exit
 		}
@@ -73,30 +72,30 @@ do
 		fpc=strtonum("0x"$1);
 	}'
 
-	func_path=`./make.sh $frame_pc_str | awk '{ print $1 }' | sed -n "/home/p"`
+	func_path=`./make.sh ${frame_pc_str} | awk '{ print $1 }' | sed -n "/home/p"`
 	func_path=`echo ${func_path##*boot/}`
-	echo $func_path
+	echo ${func_path}
 done
 echo
 
 # Parse stack
 echo "Stack:"
-grep '\[< ' $INPUT_FILE | grep '>\]' | grep -v [PC,LR] | while read line
+grep '\[< ' ${INPUT_FILE} | grep '>\]' | grep -v [PC,LR] | while read line
 do
 	echo -n "       ${line}  "
 
-	frame_pc_str=`echo $line | awk '{ print "0x"$2 }'`
-	frame_pc_dec=`echo $line | awk '{ print strtonum("0x"$2); }'`
+	frame_pc_str=`echo ${line} | awk '{ print "0x"$2 }'`
+	frame_pc_dec=`echo ${line} | awk '{ print strtonum("0x"$2); }'`
 	frame_pc_hex=`echo "obase=16;${frame_pc_dec}"|bc |tr '[A-Z]' '[a-z]'`
 
-	f_pc_dec=`cat ${SYMBOL_FILE} | sort | awk '/\.text/ { if (strtonum("0x"$1) > '$frame_pc_str') { print fpc; exit; } fpc=strtonum("0x"$1); }'`
+	f_pc_dec=`cat ${SYM_FILE} | sort | awk '/\.text/ { if (strtonum("0x"$1) > '${frame_pc_str}') { print fpc; exit; } fpc=strtonum("0x"$1); }'`
 	f_pc_hex=`echo "obase=16;${f_pc_dec}"|bc |tr '[A-Z]' '[a-z]'`
 	f_offset_dec=$((frame_pc_dec-f_pc_dec))
 	f_offset_hex=`echo "obase=16;${f_offset_dec}"|bc |tr '[A-Z]' '[a-z]'`
 
-	cat ${SYMBOL_FILE} | sort |
-	awk -v foffset=$f_offset_hex '/\.text/ {
-		if (strtonum("0x"$1) > '$frame_pc_str') {
+	cat ${SYM_FILE} | sort |
+	awk -v foffset=${f_offset_hex} '/\.text/ {
+		if (strtonum("0x"$1) > '${frame_pc_str}') {
 			printf("%s+0x%s/0x%x\n", fname, foffset, fsize);
 			exit
 		}
@@ -109,9 +108,9 @@ echo
 
 # PC instruction
 echo " PC Surrounding Instructions:"
-line=`grep '\[< ' $INPUT_FILE | grep '>\]' | grep [PC]`
-frame_pc_str=`echo $line | awk '{ print "0x"$3 }'`
-frame_pc_dec=`echo $line | awk '{ print strtonum("0x"$3); }'`
+line=`grep '\[< ' ${INPUT_FILE} | grep '>\]' | grep [PC]`
+frame_pc_str=`echo ${line} | awk '{ print "0x"$3 }'`
+frame_pc_dec=`echo ${line} | awk '{ print strtonum("0x"$3); }'`
 frame_pc_hex=`echo "obase=16;${frame_pc_dec}"|bc |tr '[A-Z]' '[a-z]'`
 PC_INSTR=`./make.sh ${ELF_FILE} | grep -5 -m 1 "${frame_pc_hex}:"`
 echo "${PC_INSTR}"
