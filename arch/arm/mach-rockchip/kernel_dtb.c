7952efb81a13d5d5fe3921e3b87b036a97bc536e
Joseph Chen <chenjh@rock-chips.com>
2021-07-28 14:52:51 +0800

    rockchip: kernel dtb: don't validate current fdt depend on magic check
    It's not safe to use fdt_check_header() to validate current fdt.
    
    Because this valid "current" fdt may be the old fdt that was loaded
    successfully at the last power on.
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: Id6e2663806a23ca90122e2881b0b77533c940e4b

--- arch/arm/mach-rockchip/kernel_dtb.c
+++ arch/arm/mach-rockchip/kernel_dtb.c
@@ -307,19 +307,9 @@ dtb_embed:
 		memcpy((void *)fdt_addr, gd->fdt_blob_kern,
 		       fdt_totalsize(gd->fdt_blob_kern));
 		printf("DTB: %s\n", CONFIG_EMBED_KERNEL_DTB_PATH);
-	} else if (ret == -EBADF) {
-		/*
-		 * If there is not embedded DTB, and the DTB in boot/recovery.img
-		 * is corrupted, just exit.
-		 *
-		 * Avoid data-abort while calling fdt-lib API for a corrupted DTB.
-		 */
-		return ret;
-	}
-
-	if (fdt_check_header((void *)fdt_addr)) {
+	} else {
 		printf("Failed to get kernel dtb, ret=%d\n", ret);
-		return ret;
+		return -ENOENT;
 	}
 
 dtb_okay:
