b4094279e5abb801546a724bc36e38504066b832
Nico Cheng <nico.cheng@rock-chips.com>
2021-09-08 14:28:15 +0800

    rockchip: dfu: support update A/B firmwares
    If the device boot from slot A, pass the slot B partitions information to
    PC. Then PC excutes command to update slot B firmwares. Open
    CONFIG_ANDROID_AB to enable it.
    
    Signed-off-by: Nico Cheng <nico.cheng@rock-chips.com>
    Change-Id: Idaa65922e2f6bbf83a8cf6e4c753f4aeacafde85

--- arch/arm/mach-rockchip/dfu_alt_info.c
+++ arch/arm/mach-rockchip/dfu_alt_info.c
@@ -7,12 +7,16 @@
 #include <common.h>
 #include <boot_rkimg.h>
 #include <memalign.h>
+#include <android_avb/rk_avb_ops_user.h>
 
 #define CONFIG_SET_DFU_ALT_BUF_LEN	(SZ_1K)
 static char *get_dfu_alt(char *interface, char *devstr)
 {
 	struct blk_desc *dev_desc;
 	char *alt_boot;
+#ifdef CONFIG_ANDROID_AB
+	char current_slot[3] = {0};
+#endif
 
 	dev_desc = rockchip_get_bootdev();
 	if (!dev_desc) {
@@ -28,9 +32,23 @@ static char *get_dfu_alt(char *interface, char *devstr)
 #endif
 #ifdef CONFIG_MTD_BLK
 	case IF_TYPE_MTD:
+#ifdef CONFIG_ANDROID_AB
+		rk_avb_get_current_slot(current_slot);
+
+		if (!strcmp(current_slot, "_a")) {
+			alt_boot = DFU_ALT_BOOT_MTD_B;
+			printf("\ncurrent boot from slot A!\n");
+		} else if (!strcmp(current_slot, "_b")) {
+			alt_boot = DFU_ALT_BOOT_MTD_A;
+			printf("\ncurrent boot from slot B!\n");
+		} else {
+			return NULL;
+		}
+#else
 		alt_boot = DFU_ALT_BOOT_MTD;
-		break;
 #endif
+		break;
+#endif /* CONFIG_MTD_BLK */
 	default:
 		printf("[dfu ERROR]:Boot device type is invalid!\n");
 		return NULL;
