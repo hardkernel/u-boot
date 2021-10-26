01dba696feedc6e68d034c997cfe2ec97500ab3d
Liang Chen <cl@rock-chips.com>
2021-10-26 09:42:10 +0800

    rockchip: add soc version for px30s
    Change-Id: Ia567a7be7c20d2f399a0124f12afcb822150cccf
    Signed-off-by: Liang Chen <cl@rock-chips.com>

--- arch/arm/include/asm/arch-rockchip/cpu.h
+++ arch/arm/include/asm/arch-rockchip/cpu.h
@@ -11,6 +11,7 @@
 #include <dm/device.h>
 
 #define ROCKCHIP_CPU_MASK       0xffff0000
+#define ROCKCHIP_CPU_PX30	0x33260000
 #define ROCKCHIP_CPU_RK312X     0x31260000
 #define ROCKCHIP_CPU_RK3288     0x32880000
 #define ROCKCHIP_CPU_RK3308	0x33080000
@@ -18,6 +19,8 @@
 #define ROCKCHIP_CPU_RK3568	0x35680000
 
 #define ROCKCHIP_SOC_MASK	(ROCKCHIP_CPU_MASK | 0xff)
+#define ROCKCHIP_SOC_PX30	(ROCKCHIP_CPU_PX30 | 0x00)
+#define ROCKCHIP_SOC_PX30S	(ROCKCHIP_CPU_PX30 | 0x01)
 #define ROCKCHIP_SOC_RK3126     (ROCKCHIP_CPU_RK312X | 0x00)
 #define ROCKCHIP_SOC_RK3126B    (ROCKCHIP_CPU_RK312X | 0x10)
 #define ROCKCHIP_SOC_RK3126C    (ROCKCHIP_CPU_RK312X | 0x20)
@@ -45,7 +48,18 @@ static inline unsigned long rockchip_get_cpu_version(void)
 
 static inline int rockchip_soc_id(void)
 {
-#if defined(CONFIG_ROCKCHIP_RK3288)
+#if defined(CONFIG_ROCKCHIP_PX30)
+	u32 v = readl(0xFF630004);
+
+	/* The CHIP_ID is stored in DDRGRF CON1[15:14]
+	 * PX30:  0x00
+	 * PX30S: 0x03
+	 */
+	if (((v >> 14) & 0x03) == 0x03)
+		return ROCKCHIP_SOC_PX30S;
+	else
+		return ROCKCHIP_SOC_PX30;
+#elif defined(CONFIG_ROCKCHIP_RK3288)
 	/* RK3288W HDMI Revision ID is 0x1A */
 	if (readl(0xFF980004) == 0x1A)
 		return ROCKCHIP_SOC_RK3288W;
@@ -85,6 +99,8 @@ static inline bool soc_is_##id(void) \
 	return false; \
 }
 
+ROCKCHIP_SOC(px30, PX30)
+ROCKCHIP_SOC(px30s, PX30S)
 ROCKCHIP_SOC(rk3126, RK3126)
 ROCKCHIP_SOC(rk3126b, RK3126B)
 ROCKCHIP_SOC(rk3126c, RK3126C)
