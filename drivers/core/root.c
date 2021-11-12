dfe24df30ae9908b9af6d7bd818a8619d02632cb
Joseph Chen <chenjh@rock-chips.com>
2021-11-12 13:22:03 +0000

    dm: firmware: bind "firmware" into DM with OF_LIVE disabled
    Do the same as commit:
    (5120375c5f dm: firmware: bind "firmware" into DM)
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: Ic574a44bf3abcb49b2559f6a5897167f638d1bab

--- drivers/core/root.c
+++ drivers/core/root.c
@@ -241,6 +241,7 @@ static int dm_scan_fdt_live(struct udevice *parent,
 			debug("%s: ret=%d\n", np->name, ret);
 		}
 
+		/* There is no compatible in "/firmware", bind it by default. */
 		if (!pre_reloc_only && !strcmp(np->name, "firmware"))
 			ret = device_bind_driver_to_node(gd->dm_root,
 				"firmware", np->name, np_to_ofnode(np), NULL);
@@ -271,6 +272,7 @@ static int dm_scan_fdt_node(struct udevice *parent, const void *blob,
 			    int offset, bool pre_reloc_only)
 {
 	int ret = 0, err;
+	const char *name;
 
 	for (offset = fdt_first_subnode(blob, offset);
 	     offset > 0;
@@ -288,6 +290,12 @@ static int dm_scan_fdt_node(struct udevice *parent, const void *blob,
 			debug("%s: ret=%d\n", fdt_get_name(blob, offset, NULL),
 			      ret);
 		}
+
+		/* There is no compatible in "/firmware", bind it by default. */
+		name = fdt_get_name(blob, offset, NULL);
+		if (name && !strcmp(name, "firmware"))
+			ret = device_bind_driver_to_node(parent, "firmware",
+					name, offset_to_ofnode(offset), NULL);
 	}
 
 	if (ret)
