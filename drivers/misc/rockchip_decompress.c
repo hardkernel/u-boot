41f62ac53730a9c89b1e30ee9f4ab14542c9fff2
Jason Zhu <jason.zhu@rock-chips.com>
2021-11-12 11:03:09 +0800

    misc: rockchip decompress: change the interface of clock
    Use the clk_get_by_index() so that the device clock node can be
    removed in spl.
    
    Signed-off-by: Jason Zhu <jason.zhu@rock-chips.com>
    Change-Id: I1620ebdb5a4cf587ce5b27c6a2c8cb622f9e5dcb

--- drivers/misc/rockchip_decompress.c
+++ drivers/misc/rockchip_decompress.c
@@ -247,7 +247,7 @@ static int rockchip_decom_probe(struct udevice *dev)
 	}
 #endif
 
-	ret = clk_get_by_name(dev, "dclk", &priv->dclk);
+	ret = clk_get_by_index(dev, 1, &priv->dclk);
 	if (ret < 0)
 		return ret;
 
