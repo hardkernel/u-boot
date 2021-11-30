dd5acf09337c40556b55f0d262c46a20dad6b4f3
Jason Zhu <jason.zhu@rock-chips.com>
2021-11-30 19:28:31 +0800

    rockchip: fit_misc: set synchronous decompression in spl
    Signed-off-by: Jason Zhu <jason.zhu@rock-chips.com>
    Change-Id: I40f0374c9f96f5518633c0215e5e4042b602f15b

--- arch/arm/mach-rockchip/fit_misc.c
+++ arch/arm/mach-rockchip/fit_misc.c
@@ -93,7 +93,7 @@ static int fit_gunzip_image(void *fit, int node, ulong *load_addr,
 #if CONFIG_IS_ENABLED(MISC_DECOMPRESS)
 	ret = misc_decompress_process((ulong)(*load_addr),
 				      (ulong)(*src_addr), (ulong)(*src_len),
-				      DECOM_GZIP, false, &len, flags);
+				      DECOM_GZIP, true, &len, flags);
 #else
 	ret = gunzip((void *)(*load_addr), ALIGN(len, FIT_MAX_SPL_IMAGE_SZ),
 		     (void *)(*src_addr), (void *)(&len));
