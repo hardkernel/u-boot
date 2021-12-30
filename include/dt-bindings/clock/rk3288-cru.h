9ab8649d28d0f1d652fc9d4003fd5711c1fa2bb8
Elaine Zhang <zhangqing@rock-chips.com>
2021-12-30 15:00:58 +0800

    rockchip: dts: rk3288: Resync from kernel-4.19
    Resync from kernel-4.19:
    (e7b6652a08486 UPSTREAM: USB-PD tcpm: bad warning+size, PPS adapters)
    
    Change-Id: Ie837a585fa4d9320e70997b0ba0faffb34dfafa4
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- include/dt-bindings/clock/rk3288-cru.h
+++ include/dt-bindings/clock/rk3288-cru.h
@@ -77,10 +77,20 @@
 #define SCLK_PVTM_GPU		124
 #define SCLK_CRYPTO		125
 #define SCLK_MIPIDSI_24M	126
+#define SCLK_VIP_OUT		127
+#define SCLK_DDRCLK		128
+#define SCLK_I2S_SRC		129
+#define SCLK_TSPOUT		130
+#define SCLK_TSP		131
+#define SCLK_HSADC0_TSP		132
+#define SCLK_HSADC1_TSP		133
+#define SCLK_27M_TSP		134
 
 #define SCLK_MAC_PLL		150
 #define SCLK_MAC		151
 #define SCLK_MACREF_OUT		152
+#define SCLK_TESTOUT_SRC	153
+#define SCLK_TESTOUT		154
 
 #define DCLK_VOP0		190
 #define DCLK_VOP1		191
@@ -158,6 +168,13 @@
 #define PCLK_DDRUPCTL1		366
 #define PCLK_PUBL1		367
 #define PCLK_WDT		368
+#define PCLK_EFUSE256		369
+#define PCLK_EFUSE1024		370
+#define PCLK_ISP_IN		371
+#define PCLK_VIP		372
+#define PCLK_VIP_IN		373
+#define PCLK_PD_ALIVE		374
+#define PCLK_PD_PMU		375
 
 /* hclk gates */
 #define HCLK_GPS		448
@@ -191,8 +208,10 @@
 #define HCLK_VCODEC		476
 #define HCLK_CPU		477
 #define HCLK_PERI		478
+#define HCLK_USB_PERI		479
+#define HCLK_VIO		480
 
-#define CLK_NR_CLKS		(HCLK_PERI + 1)
+#define CLK_NR_CLKS		(HCLK_VIO + 1)
 
 /* soft-reset indices */
 #define SRST_CORE0		0
