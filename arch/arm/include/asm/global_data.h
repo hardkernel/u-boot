39cedad5fb73871e3ff1b11090f315350669c656
Joseph Chen <chenjh@rock-chips.com>
2022-02-21 06:45:45 +0000

    include: global_data: fix compiler error if disable I/D cache
    common/spl/spl.c: In function ‘spl_dcache_enable’:
    common/spl/spl.c:266:10: error: ‘volatile struct arch_global_data’ has no member named ‘tlb_size’
      gd->arch.tlb_size = PGTABLE_SIZE;
              ^
    common/spl/spl.c:267:10: error: ‘volatile struct arch_global_data’ has no member named ‘tlb_addr’
      gd->arch.tlb_addr = (ulong)memalign(SZ_16K, ALIGN(PGTABLE_SIZE, SZ_4K));
              ^
    common/spl/spl.c:268:15: error: ‘volatile struct arch_global_data’ has no member named ‘tlb_addr’
      if (!gd->arch.tlb_addr) {
                   ^
      CC      tpl/arch/arm/lib/spl.o
      CC      spl/common/fdt_support.o
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: Ifd6cf6cd8309867e1b51ce5c1f32dcfdb600c515

--- arch/arm/include/asm/global_data.h
+++ arch/arm/include/asm/global_data.h
@@ -36,7 +36,8 @@ struct arch_global_data {
 	unsigned int tbl;
 	unsigned long lastinc;
 	unsigned long long timer_reset_value;
-#if !(defined(CONFIG_SYS_ICACHE_OFF) && defined(CONFIG_SYS_DCACHE_OFF))
+#if !(defined(CONFIG_SYS_ICACHE_OFF) && defined(CONFIG_SYS_DCACHE_OFF) && \
+      defined(CONFIG_SPL_SYS_DCACHE_OFF))
 	unsigned long tlb_addr;
 	unsigned long tlb_size;
 #if defined(CONFIG_ARM64)
