79475ff58f34f65a7c16733c98aa6102157116ad
David Wu <david.wu@rock-chips.com>
2022-03-02 17:37:28 +0800

    include: arch-rockchip: rv1106: Add grf struct
    Change-Id: I165996d86bfe12ce58799f6a31d8e00cf8a65971
    Signed-off-by: David Wu <david.wu@rock-chips.com>

--- arch/arm/include/asm/arch-rockchip/grf_rv1106.h
+++ arch/arm/include/asm/arch-rockchip/grf_rv1106.h
@@ -8,4 +8,23 @@
 
 #include <common.h>
 
+struct rv1106_grf {
+	unsigned int reserved0[0x060000 / 4];
+	unsigned int gmac_con0;
+	unsigned int gmac_clk_con;
+	unsigned int gmac_st;
+	unsigned int tsadc_con;
+	unsigned int otp_con;
+	unsigned int rga_noc;
+	unsigned int vo_mem;
+	unsigned int sdio_con0;
+	unsigned int sdio_co1;
+	unsigned int sdio_ram_clhgat_dis;
+	unsigned int macphy_con0;
+	unsigned int macphy_con1;
+	unsigned int vop_qos;
+	unsigned int vop_pipe_bypass;
+};
+
+check_member(rv1106_grf, vop_pipe_bypass, 0x60034);
 #endif
