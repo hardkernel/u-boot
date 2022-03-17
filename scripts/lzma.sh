75c994bd24d78363c9cefc8d7a2e3454d50be87a
Joseph Chen <chenjh@rock-chips.com>
2022-03-17 11:23:29 +0000

    scripts: lzma.sh: remove output message
    It will called by make fit script.
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I6c5fd6813d224d376247f738166bc9bbacabbe77

--- scripts/lzma.sh
+++ scripts/lzma.sh
@@ -19,6 +19,3 @@ ch3=`printf "%08x\n" $dec_size | sed 's/\(..\)/\1 /g' | awk '{ printf $1 }'`
 ch3=`printf '%03o' $((0x$ch3))`
 
 (cat ${BIN} | lzma -9 -k -f && printf \\$ch0\\$ch1\\$ch2\\$ch3) > ${BIN}.lzma
-
-echo "${BIN}.lzma is ready."
-
