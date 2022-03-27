0ac34d7f9add9e5c1bf196ada4f60b604975eaa6
Joseph Chen <chenjh@rock-chips.com>
2022-03-27 03:27:32 +0000

    cmd: script_update: don't use '&&' syntax
    We may disable some syntax feature of hush for the board
    that requires critical tiny image size.
    
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I6a7b2893f6fc5d03c57ee44363e51563098bab2b

--- cmd/script_update.c
+++ cmd/script_update.c
@@ -91,7 +91,6 @@ static int do_script(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		printf("\n$ %s\n", next_line);
 		run_command(next_line, 0);
 	}
-
 	free(buf);
 
 	return CMD_RET_SUCCESS;
@@ -108,10 +107,12 @@ static int do_sd_update(cmd_tbl_t *cmdtp, int flag,
 	if (!buf)
 		return CMD_RET_FAILURE;
 
-	snprintf(cmd, 128,
-		 "fatload mmc 1 0x%08lx sd_update.txt && script 0x%08lx",
-		 (ulong)buf, (ulong)buf);
+	snprintf(cmd, 128, "fatload mmc 1 0x%08lx sd_update.txt", (ulong)buf);
 	ret = run_command(cmd, 0);
+	if (!ret) {
+		snprintf(cmd, 128, "script 0x%08lx", (ulong)buf);
+		ret = run_command(cmd, 0);
+	}
 	free(buf);
 
 	return ret;
@@ -128,10 +129,16 @@ static int do_usb_update(cmd_tbl_t *cmdtp, int flag,
 	if (!buf)
 		return CMD_RET_FAILURE;
 
-	snprintf(cmd, 128,
-		 "usb reset && fatload usb 0 0x%08lx usb_update.txt && script 0x%08lx",
-		 (ulong)buf, (ulong)buf);
+	snprintf(cmd, 128, "usb reset");
 	ret = run_command(cmd, 0);
+	if (!ret) {
+		snprintf(cmd, 128, "fatload usb 0 0x%08lx usb_update.txt", (ulong)buf);
+		ret = run_command(cmd, 0);
+	}
+	if (!ret) {
+		snprintf(cmd, 128, "script 0x%08lx", (ulong)buf);
+		ret = run_command(cmd, 0);
+	}
 	free(buf);
 
 	return ret;
@@ -155,10 +162,12 @@ static int do_tftp_update(cmd_tbl_t *cmdtp, int flag,
 	if (dhcp)
 		run_command("dhcp", 0);
 
-	snprintf(cmd, 128,
-		 "tftp 0x%08lx tftp_update.txt && script 0x%08lx",
-		 (ulong)buf, (ulong)buf);
+	snprintf(cmd, 128, "tftp 0x%08lx tftp_update.txt", (ulong)buf);
 	ret = run_command(cmd, 0);
+	if (!ret) {
+		snprintf(cmd, 128, "script 0x%08lx", (ulong)buf);
+		ret = run_command(cmd, 0);
+	}
 	free(buf);
 
 	return ret;
