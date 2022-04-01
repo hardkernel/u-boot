5d6c9d4c9564e8c3503cb7a28c1203b2d1f5137c
Joseph Chen <chenjh@rock-chips.com>
2022-04-01 08:33:38 +0000

    scripts: renamed lzma.sh -> compress.sh to support more compression
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I1d01f34afc0dc03c5a0c6202253b9c5465dc9569

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -6,9 +6,10 @@
 #
 set -e
 
-BIN=$1
+ALGO=$1
+FILE=$2
 
-dec_size=`wc ${BIN} | awk '{ printf $3 }'`
+dec_size=`wc ${FILE} | awk '{ printf $3 }'`
 ch0=`printf "%08x\n" $dec_size | sed 's/\(..\)/\1 /g' | awk '{ printf $4 }'`
 ch0=`printf '%03o' $((0x$ch0))`
 ch1=`printf "%08x\n" $dec_size | sed 's/\(..\)/\1 /g' | awk '{ printf $3 }'`
@@ -18,4 +19,4 @@ ch2=`printf '%03o' $((0x$ch2))`
 ch3=`printf "%08x\n" $dec_size | sed 's/\(..\)/\1 /g' | awk '{ printf $1 }'`
 ch3=`printf '%03o' $((0x$ch3))`
 
-(cat ${BIN} | lzma -9 -k -f && printf \\$ch0\\$ch1\\$ch2\\$ch3) > ${BIN}.lzma
+(cat ${FILE} | ${ALGO} -9 -k -f && printf \\$ch0\\$ch1\\$ch2\\$ch3) > ${FILE}.${ALGO}
