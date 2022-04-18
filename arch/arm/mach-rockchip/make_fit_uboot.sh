f670e8c83fd67a9aaa3aba3bd3f45ab85a8d4a95
Joseph Chen <chenjh@rock-chips.com>
2022-04-18 06:54:18 +0000

    rockchip: make_fit_uboot.sh: Add kernel fdt node
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I39194ea8d37aaa610731f660e3261d7f729c5937

--- arch/arm/mach-rockchip/make_fit_uboot.sh
+++ arch/arm/mach-rockchip/make_fit_uboot.sh
@@ -8,8 +8,9 @@
 source ./${srctree}/arch/arm/mach-rockchip/fit_nodes.sh
 gen_header
 gen_uboot_node
-gen_fdt_node
 gen_mcu_node
+gen_kfdt_node
+gen_fdt_node
 gen_loadable_node
 gen_arm_configurations
 
