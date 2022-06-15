c8480b92b4d9a534288e5fe4e5220e840dfca86a
Guochun Huang <hero.huang@rock-chips.com>
2022-06-15 13:01:25 +0000

    video/drm: dsi2: fix DSI_VID_TX_CFG configuration errors
    Change-Id: Ib31f91fa7ab3b93388ad20410f5a773c10bc1315
    Signed-off-by: Guochun Huang <hero.huang@rock-chips.com>

--- drivers/video/drm/dw_mipi_dsi2.c
+++ drivers/video/drm/dw_mipi_dsi2.c
@@ -597,16 +597,23 @@ static void dw_mipi_dsi2_set_vid_mode(struct dw_mipi_dsi2 *dsi2)
 	u32 val = 0, mode;
 	int ret;
 
+	if (dsi2->mode_flags & MIPI_DSI_MODE_VIDEO_HFP)
+		val |= BLK_HFP_HS_EN;
+
+	if (dsi2->mode_flags & MIPI_DSI_MODE_VIDEO_HBP)
+		val |= BLK_HBP_HS_EN;
+
+	if (dsi2->mode_flags & MIPI_DSI_MODE_VIDEO_HSA)
+		val |= BLK_HSA_HS_EN;
+
 	if (dsi2->mode_flags & MIPI_DSI_MODE_VIDEO_BURST)
 		val |= VID_MODE_TYPE_BURST;
 	else if (dsi2->mode_flags & MIPI_DSI_MODE_VIDEO_SYNC_PULSE)
 		val |= VID_MODE_TYPE_NON_BURST_SYNC_PULSES;
-
 	else
 		val |= VID_MODE_TYPE_NON_BURST_SYNC_EVENTS;
 
 	dsi_write(dsi2, DSI2_DSI_VID_TX_CFG, val);
-
 
 	dsi_write(dsi2, DSI2_MODE_CTRL, VIDEO_MODE);
 	ret = readl_poll_timeout(dsi2->base + DSI2_MODE_STATUS,
@@ -950,20 +957,6 @@ static void dw_mipi_dsi2_tx_option_set(struct dw_mipi_dsi2 *dsi2)
 
 	if (dsi2->scrambling_en)
 		dsi_write(dsi2, DSI2_DSI_SCRAMBLING_CFG, SCRAMBLING_EN);
-
-	val = 0;
-	if (dsi2->mode_flags & MIPI_DSI_MODE_VIDEO_HFP)
-		val |= BLK_HFP_HS_EN;
-
-	if (dsi2->mode_flags & MIPI_DSI_MODE_VIDEO_HBP)
-		val |= BLK_HBP_HS_EN;
-
-	if (dsi2->mode_flags & MIPI_DSI_MODE_VIDEO_HSA)
-		val |= BLK_HSA_HS_EN;
-
-	dsi_write(dsi2, DSI2_DSI_VID_TX_CFG, val);
-
-	/* configure the maximum return packet size that periphera can send */
 }
 
 static void dw_mipi_dsi2_irq_enable(struct dw_mipi_dsi2 *dsi2, bool enable)
