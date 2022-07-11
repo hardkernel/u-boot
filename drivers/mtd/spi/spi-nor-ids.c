7277a49c4aed49e7f7e95f88db64eb9c4a36063e
Jon Lin <jon.lin@rock-chips.com>
2022-07-11 15:34:43 +0800

    mtd: spi-nor-ids: Enanble GD25LQ128 quad read feature
    Change-Id: I0d0a63559b7b30347e7cc6be9218cb7afac804b8
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -120,7 +120,7 @@ const struct flash_info spi_nor_ids[] = {
 	},
 	{
 		INFO("gd25lq128", 0xc86018, 0, 64 * 1024, 256,
-			SECT_4K | SPI_NOR_DUAL_READ |
+			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
 	{	INFO("gd25q256", 0xc84019, 0, 64 * 1024, 512,
