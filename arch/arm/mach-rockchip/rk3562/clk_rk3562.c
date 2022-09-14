9b43a31a11f5bff94914567d2c1b7ca3e44d8d19
Finley Xiao <finley.xiao@rock-chips.com>
2022-09-14 20:08:54 +0800

    clk: rockchip: Add rk3562 clock driver
    Add basic clock for rk3562 which including cpu, mmc, i2c,
    pwm ...clocks init.
    
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Change-Id: Id106546cd8c6a44ba1eedaaa66086e4decbc960e

--- arch/arm/mach-rockchip/rk3562/clk_rk3562.c
+++ arch/arm/mach-rockchip/rk3562/clk_rk3562.c
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2020 Rockchip Electronics Co., Ltd.
+ * (C) Copyright 2022 Rockchip Electronics Co., Ltd.
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -16,6 +16,14 @@ int rockchip_get_clk(struct udevice **devp)
 			DM_GET_DRIVER(rockchip_rk3562_cru), devp);
 }
 
+#if CONFIG_IS_ENABLED(CLK_SCMI)
+int rockchip_get_scmi_clk(struct udevice **devp)
+{
+	return uclass_get_device_by_driver(UCLASS_CLK,
+			DM_GET_DRIVER(scmi_clock), devp);
+}
+#endif
+
 void *rockchip_get_cru(void)
 {
 	struct rk3562_clk_priv *priv;
@@ -29,26 +37,5 @@ void *rockchip_get_cru(void)
 	priv = dev_get_priv(dev);
 
 	return priv->cru;
-}
-
-static int rockchip_get_pmucruclk(struct udevice **devp)
-{
-	return uclass_get_device_by_driver(UCLASS_CLK,
-			DM_GET_DRIVER(rockchip_rk3562_pmucru), devp);
-}
-
-void *rockchip_get_pmucru(void)
-{
-	struct rk3562_pmuclk_priv *priv;
-	struct udevice *dev;
-	int ret;
-
-	ret = rockchip_get_pmucruclk(&dev);
-	if (ret)
-		return ERR_PTR(ret);
-
-	priv = dev_get_priv(dev);
-
-	return priv->pmucru;
 }
 
