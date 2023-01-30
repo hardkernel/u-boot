3ce911631825141646d038f116284dea0ff0a788
Jon Lin <jon.lin@rock-chips.com>
2023-01-30 17:10:50 +0800

    mtd: spi-nor-ids: support New devices
    gd55b01ge
    
    Change-Id: I60f4b39da17cfb39962929e328ee99a814f6bd59
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -148,6 +148,11 @@ const struct flash_info spi_nor_ids[] = {
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_4B_OPCODES | SPI_NOR_HAS_LOCK)
 	},
+	{
+		INFO("gd55b01ge", 0xc8471b, 0, 64 * 1024, 2048,
+			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			SPI_NOR_4B_OPCODES | SPI_NOR_HAS_LOCK)
+	},
 #endif
 #ifdef CONFIG_SPI_FLASH_ISSI		/* ISSI */
 	/* ISSI */
