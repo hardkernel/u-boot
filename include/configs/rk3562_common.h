08735c34a63f788bc8d84360798ae4e6cc8062fc
Xuhui Lin <xuhui.lin@rock-chips.com>
2023-02-10 17:00:32 +0800

    rockchip: rk3562: Add secure-otp support
    Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>
    Change-Id: I8b17c4a5585297c6088229c276deaf1c66018466

--- include/configs/rk3562_common.h
+++ include/configs/rk3562_common.h
@@ -34,6 +34,15 @@
 #define GICD_BASE			0xfe901000
 #define GICC_BASE			0xfe902000
 
+/* secure otp */
+#define OTP_UBOOT_ROLLBACK_OFFSET	0x350
+#define OTP_UBOOT_ROLLBACK_WORDS	2	/* 64 bits, 2 words */
+#define OTP_ALL_ONES_NUM_BITS		32
+#define OTP_SECURE_BOOT_ENABLE_ADDR	0x20
+#define OTP_SECURE_BOOT_ENABLE_SIZE	1
+#define OTP_RSA_HASH_ADDR		0x180
+#define OTP_RSA_HASH_SIZE		32
+
 /* MMC/SD IP block */
 #define CONFIG_BOUNCE_BUFFER
 
