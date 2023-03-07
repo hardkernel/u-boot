172237e267aac3321fda413b6879513c872bc88c
Jon Lin <jon.lin@rock-chips.com>
2023-03-07 10:52:52 +0800

    Revert "dm: regulator: skip probed pre-reloc regulators"
    This reverts commit 9a7b7f26ec66fca6fc43ff0ea333e16b2f6317f8.
    
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>
    Change-Id: Icd35e4a5b67e6fa701c6e87aba3e4434f7dd22a9

--- drivers/core/device.c
+++ drivers/core/device.c
@@ -97,8 +97,6 @@ static int device_bind_common(struct udevice *parent, const struct driver *drv,
 					debug("%s do not delete uboot dev: %s\n",
 					      __func__, dev->name);
 					return 0;
-				} else if (drv->id == UCLASS_REGULATOR) {
-					/* stay in dm tree, in order to handle exclusion */
 				} else {
 					list_del_init(&dev->uclass_node);
 				}
