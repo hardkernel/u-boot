d9939fc9e4c4eb810ee1df43e7be111ed3c7e3aa
Algea Cao <algea.cao@rock-chips.com>
2023-03-31 16:27:07 +0800

    drm: bridge: dw-hdmi: Read edid in 8-bytes steps
    This saves about 200 milliseconds compared to the
    original method of reading one byte at a time.
    
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>
    Change-Id: I8b0fdfffaf950089f7c8c80fb874b51bc4159894

--- drivers/video/drm/dw_hdmi.c
+++ drivers/video/drm/dw_hdmi.c
@@ -28,6 +28,7 @@
 #define HDCP_PRIVATE_KEY_SIZE   280
 #define HDCP_KEY_SHA_SIZE       20
 #define HDMI_HDCP1X_ID		5
+#define HDMI_EDID_BLOCK_LEN	128
 /*
  * Unless otherwise noted, entries in this table are 100% optimization.
  * Values can be obtained from hdmi_compute_n() but that function is
@@ -413,6 +414,7 @@ static int dw_hdmi_i2c_read(struct dw_hdmi *hdmi,
 {
 	struct dw_hdmi_i2c *i2c = hdmi->i2c;
 	int interrupt = 0, i = 20;
+	bool read_edid = false;
 
 	if (!i2c->is_regaddr) {
 		printf("set read register address to 0\n");
@@ -420,14 +422,36 @@ static int dw_hdmi_i2c_read(struct dw_hdmi *hdmi,
 		i2c->is_regaddr = true;
 	}
 
-	while (length--) {
-		hdmi_writeb(hdmi, i2c->slave_reg++, HDMI_I2CM_ADDRESS);
-		if (i2c->is_segment)
-			hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_READ_EXT,
-				    HDMI_I2CM_OPERATION);
-		else
-			hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_READ,
-				    HDMI_I2CM_OPERATION);
+	/* edid reads are in 128 bytes. scdc reads are in 1 byte */
+	if (length == HDMI_EDID_BLOCK_LEN)
+		read_edid = true;
+
+	while (length > 0) {
+		hdmi_writeb(hdmi, i2c->slave_reg, HDMI_I2CM_ADDRESS);
+
+		if (read_edid) {
+			i2c->slave_reg += 8;
+			length -= 8;
+		} else {
+			i2c->slave_reg++;
+			length--;
+		}
+
+		if (i2c->is_segment) {
+			if (read_edid)
+				hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_READ8_EXT,
+					    HDMI_I2CM_OPERATION);
+			else
+				hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_READ_EXT,
+					    HDMI_I2CM_OPERATION);
+		} else {
+			if (read_edid)
+				hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_READ8,
+					    HDMI_I2CM_OPERATION);
+			else
+				hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_READ,
+					    HDMI_I2CM_OPERATION);
+		}
 
 		while (i--) {
 			udelay(1000);
@@ -443,6 +467,10 @@ static int dw_hdmi_i2c_read(struct dw_hdmi *hdmi,
 		if (!interrupt) {
 			printf("[%s] i2c read reg[0x%02x] no interrupt\n",
 			       __func__, i2c->slave_reg);
+			hdmi_writeb(hdmi, 0, HDMI_I2CM_SOFTRSTZ);
+			hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_BUS_CLEAR,
+				    HDMI_I2CM_OPERATION);
+			udelay(1000);
 			return -EAGAIN;
 		}
 
@@ -450,11 +478,19 @@ static int dw_hdmi_i2c_read(struct dw_hdmi *hdmi,
 		if (interrupt & HDMI_IH_I2CM_STAT0_ERROR) {
 			printf("[%s] read reg[0x%02x] data error:0x%02x\n",
 			       __func__, i2c->slave_reg, interrupt);
+			hdmi_writeb(hdmi, 0, HDMI_I2CM_SOFTRSTZ);
+			hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_BUS_CLEAR,
+				    HDMI_I2CM_OPERATION);
+			udelay(1000);
 			return -EIO;
 		}
 
 		i = 20;
-		*buf++ = hdmi_readb(hdmi, HDMI_I2CM_DATAI);
+		if (read_edid)
+			for (i = 0; i < 8; i++)
+				*buf++ = hdmi_readb(hdmi, HDMI_I2CM_READ_BUFF0 + i);
+		else
+			*buf++ = hdmi_readb(hdmi, HDMI_I2CM_DATAI);
 	}
 	i2c->is_segment = false;
 
@@ -494,8 +530,22 @@ static int dw_hdmi_i2c_write(struct dw_hdmi *hdmi,
 				break;
 		}
 
+		if (!interrupt) {
+			printf("[%s] i2c write reg[0x%02x] no interrupt\n",
+			       __func__, i2c->slave_reg);
+			hdmi_writeb(hdmi, 0, HDMI_I2CM_SOFTRSTZ);
+			hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_BUS_CLEAR,
+				    HDMI_I2CM_OPERATION);
+			udelay(1000);
+			return -EAGAIN;
+		}
+
 		if ((interrupt & m_I2CM_ERROR) || (i == -1)) {
 			printf("[%s] write data error\n", __func__);
+			hdmi_writeb(hdmi, 0, HDMI_I2CM_SOFTRSTZ);
+			hdmi_writeb(hdmi, HDMI_I2CM_OPERATION_BUS_CLEAR,
+				    HDMI_I2CM_OPERATION);
+			udelay(1000);
 			return -EIO;
 		} else if (interrupt & m_I2CM_DONE) {
 			printf("[%s] write offset %02x success\n",
