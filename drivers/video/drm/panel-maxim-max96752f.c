5202c1e0978d8201d2d31a4d6919deb13dc42f85
Wyon Bi <bivvy.bi@rock-chips.com>
2023-05-26 17:40:21 +0800

    video/drm: panel-maxim-max96752f: Add support for ogm 101fhbllm01
    Signed-off-by: Wyon Bi <bivvy.bi@rock-chips.com>
    Change-Id: I0a89189a700a15e7f61f0c98a23f01d65188bb8b

--- drivers/video/drm/panel-maxim-max96752f.c
+++ drivers/video/drm/panel-maxim-max96752f.c
@@ -219,8 +219,35 @@ static const struct panel_desc boe_av156fht_l83 = {
 	.backlight_disable	= boe_av156fht_l83_panel_backlight_disable,
 };
 
+static int ogm_101fhbllm01_prepare(struct max96752f *max96752f)
+{
+	maxim_deserializer_write(max96752f, 0x01ce, 0x5e);
+
+	maxim_deserializer_write(max96752f, 0x0203, 0x84);	/* GPIO1 -> BL_PWM */
+	maxim_deserializer_write(max96752f, 0x0206, 0x84);	/* GPIO2 -> TP_RST */
+	maxim_deserializer_write(max96752f, 0x0209, 0x83);	/* GPIO3 <- TP_INT */
+
+	maxim_deserializer_write(max96752f, 0x0001, 0x02);
+
+	return 0;
+}
+
+static int ogm_101fhbllm01_unprepare(struct max96752f *max96752f)
+{
+	maxim_deserializer_write(max96752f, 0x0001, 0x01);
+
+	return 0;
+}
+
+static const struct panel_desc ogm_101fhbllm01 = {
+	.name			= "ogm,101fhbllm01",
+	.prepare		= ogm_101fhbllm01_prepare,
+	.unprepare		= ogm_101fhbllm01_unprepare,
+};
+
 static const struct udevice_id max96752f_of_match[] = {
 	{ .compatible = "boe,av156fht-l83", .data = (ulong)&boe_av156fht_l83 },
+	{ .compatible = "ogm,101fhbllm01", .data = (ulong)&ogm_101fhbllm01 },
 	{}
 };
 
