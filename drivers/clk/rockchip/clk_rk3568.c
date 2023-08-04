19191d3b0588b505cfdf24ebf6c2c0f9dcc37042
Jonas Karlman <jonas@kwiboo.se>
2023-08-04 09:34:00 +0000

    UPSTREAM: clk: rockchip: rk3568: Add dummy support for GMAC speed clocks
    Pine64 Quartz64 boards DT reference SCLK_GMAC1_RGMII_SPEED in the
    assigned-clocks property of the gmac1 node. This result in a ENOENT
    error when driver core tries to set a parent for this clock.
    
    The clock speed in rgmii/rmii mode is changed using clk_set_rate of the
    tx_rx clock and not using clk_set_parent of the speed clock.
    
    Add dummy support for SCLK_GMAC1_RGMII_SPEED and similar clocks to clk
    driver to allow a driver for gmac node to probe.
    
    Change-Id: Iaf6ba930cc29ac3233cf5d046002f71a6879eb78
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>
    Reviewed-by: Kever Yang <kever.yang@rock-chips.com>
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- drivers/clk/rockchip/clk_rk3568.c
+++ drivers/clk/rockchip/clk_rk3568.c
@@ -3219,6 +3219,11 @@ static int rk3568_clk_set_parent(struct clk *clk, struct clk *parent)
 	case I2S3_MCLK_IOE:
 	case I2S3_MCLKOUT:
 		return rk3568_i2s3_set_parent(clk, parent);
+	case I2S1_MCLKOUT_TX:
+	case SCLK_GMAC0_RGMII_SPEED:
+	case SCLK_GMAC0_RMII_SPEED:
+	case SCLK_GMAC1_RGMII_SPEED:
+	case SCLK_GMAC1_RMII_SPEED:
 	default:
 		return -ENOENT;
 	}
