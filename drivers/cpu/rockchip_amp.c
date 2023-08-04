4a758d955aedd5227d6c2142800435ca15fa3429
Joseph Chen <chenjh@rock-chips.com>
2023-08-04 01:50:57 +0000

    cpu: rockchip amp: Power down primary cpu if amp-dispatcher requires
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I597aee367fac8ec2a7db8355e679ba522e8ee911

--- drivers/cpu/rockchip_amp.c
+++ drivers/cpu/rockchip_amp.c
@@ -46,7 +46,8 @@ DECLARE_GLOBAL_DATA_PTR;
 
 #define gicd_readl(offset)	readl((void *)GICD_BASE + (offset))
 #define gicd_writel(v, offset)	writel(v, (void *)GICD_BASE + (offset))
-#define LINXU_AMP_NODES		"/rockchip-amp/amp-cpus"
+#define ROCKCHIP_AMP_NODE		"/rockchip-amp"
+#define ROCKCHIP_AMP_CPUS_NODE		"/rockchip-amp/amp-cpus"
 
 typedef struct boot_args {
 	ulong arg0;
@@ -60,10 +61,11 @@ typedef struct boot_cpu {
 	u32 state;
 	u32 entry;
 	u32 linux_os;
+	u32 boot_on;
 } boot_cpu_t;
 
 static boot_cpu_t g_bootcpu;
-static u32 g_cpus_boot_by_linux[8];
+static u32 os_amp_dispatcher_cpu[8];
 
 static u32 fit_get_u32_default(const void *fit, int noffset,
 			       const char *prop, u32 def)
@@ -77,23 +79,32 @@ static u32 fit_get_u32_default(const void *fit, int noffset,
 	return fdt32_to_cpu(*val);
 }
 
-static int parse_cpus_boot_by_linux(void)
+static int parse_os_amp_dispatcher(void)
 {
 	const void *fdt = gd->fdt_blob;
 	int noffset, cpu, i = 0;
 	u64 mpidr;
 
-	memset(g_cpus_boot_by_linux, 0xff, sizeof(g_cpus_boot_by_linux));
-	noffset = fdt_path_offset(fdt, LINXU_AMP_NODES);
+	memset(os_amp_dispatcher_cpu, 0xff, sizeof(os_amp_dispatcher_cpu));
+
+	noffset = fdt_path_offset(fdt, ROCKCHIP_AMP_NODE);
+	if (noffset < 0)
+		return 0;
+
+	if (!fdtdec_get_is_enabled(fdt, noffset))
+		return 0;
+
+	noffset = fdt_path_offset(fdt, ROCKCHIP_AMP_CPUS_NODE);
 	if (noffset < 0)
 		return 0;
 
 	fdt_for_each_subnode(cpu, fdt, noffset) {
 		mpidr = fdtdec_get_uint64(fdt, cpu, "id", 0xffffffff);
-		if (mpidr == 0xffffffff)
+		if (mpidr == 0xffffffff) /* invalid */
 			continue;
-		g_cpus_boot_by_linux[i++] = mpidr;
-		printf("CPU[0x%llx] is required boot by Linux\n", mpidr);
+
+		os_amp_dispatcher_cpu[i++] = mpidr;
+		AMP_I("cpu[%llx] belong to os amp-dispatcher\n", mpidr);
 	}
 
 	return 0;
@@ -286,12 +297,21 @@ static int brought_up_amp(void *fit, int noffset,
 	AMP_I("   linux-os: %d\n\n", is_linux);
 #endif
 
+	/* If os amp-dispatcher owns this cpu, don't boot it */
+	for (i = 0; i < ARRAY_SIZE(os_amp_dispatcher_cpu); i++) {
+		if (cpu == os_amp_dispatcher_cpu[i]) {
+			boot_on = 0;
+			break;
+		}
+	}
+
 	/* this cpu is boot cpu ? */
 	if ((read_mpidr() & 0x0fff) == cpu) {
 		bootcpu->arch = arch;
 		bootcpu->entry = entry;
 		bootcpu->state = pe_state;
 		bootcpu->linux_os = is_linux;
+		bootcpu->boot_on = boot_on;
 		return 0;
 	}
 
@@ -313,14 +333,6 @@ static int brought_up_amp(void *fit, int noffset,
 		if (!sysmem_alloc_base_by_name(desc,
 				(phys_addr_t)load, data_size))
 			return -ENXIO;
-	}
-
-	/* If linux assign the boot-on state, use it */
-	for (i = 0; i < ARRAY_SIZE(g_cpus_boot_by_linux); i++) {
-		if (cpu == g_cpus_boot_by_linux[i]) {
-			boot_on = 0;
-			break;
-		}
 	}
 
 	if (!boot_on)
@@ -372,9 +384,25 @@ static int brought_up_all_amp(void *fit, const char *fit_uname_cfg)
 
 	/* === only boot cpu can reach here === */
 
+	if (!g_bootcpu.boot_on) {
+		AMP_I("Primary cpu[%x, self] with state 0x%x, entry 0x%08x ... Power down!\n",
+		      (u32)read_mpidr() & 0x0fff, g_bootcpu.state, g_bootcpu.entry);
+		dsb();
+		isb();
+		/* make sure I am off before os amp-dispatcher probe */
+		psci_cpu_off(0);
+
+		/* Never reach here */
+		while (1) {
+			AMP_E("Primary cpu[%x, self] power down failed\n",
+			      (u32)read_mpidr());
+			__asm("wfe");
+		}
+	}
+
 	if (!g_bootcpu.linux_os && g_bootcpu.entry) {
 		flush_dcache_all();
-		AMP_I("Brought up cpu[%x, self] with state 0x%x, entry 0x%08x ...",
+		AMP_I("Brought up primary cpu[%x, self] with state 0x%x, entry 0x%08x ...",
 		      (u32)read_mpidr() & 0x0fff, g_bootcpu.state, g_bootcpu.entry);
 		cleanup_before_linux();
 		printf("OK\n");
@@ -451,8 +479,11 @@ int amp_cpus_on(void)
 		goto out1;
 	}
 
-	/* prase linux info */
-	parse_cpus_boot_by_linux();
+	ret = parse_os_amp_dispatcher();
+	if (ret < 0) {
+		ret = -EINVAL;
+		goto out1;
+	}
 
 	/* Load loadables */
 	memset(&images, 0, sizeof(images));
