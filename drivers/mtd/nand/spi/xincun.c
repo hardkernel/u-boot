1ee4a0e5e0af790a0438c5182a355145cdce4186
Jon Lin <jon.lin@rock-chips.com>
2023-12-21 18:47:14 +0800

    mtd: spinand: XINCUN: Support new device XCSP1AAPK
    Change-Id: I8843a4ded33098711eb52fd17486db7be2ce0ce1
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/xincun.c
+++ drivers/mtd/nand/spi/xincun.c
@@ -94,6 +94,15 @@ static const struct spinand_info xincun_spinand_table[] = {
 					      &update_cache_variants),
 		     SPINAND_HAS_QE_BIT,
 		     SPINAND_ECCINFO(&xcsp2aapk_ooblayout, xcsp2aapk_ecc_get_status)),
+	SPINAND_INFO("XCSP1AAPK",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0x01),
+		     NAND_MEMORG(1, 2048, 128, 64, 1024, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&xcsp2aapk_ooblayout, xcsp2aapk_ecc_get_status)),
 };
 
 static const struct spinand_manufacturer_ops xincun_spinand_manuf_ops = {
