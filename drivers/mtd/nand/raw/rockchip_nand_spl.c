6b5b58a095d21ac71994c9b578aa866e12b09669
Jon Lin <jon.lin@rock-chips.com>
2023-12-22 11:15:15 +0800

    mtd: nand: rockchip: Support IS34ML08G088
    Change-Id: I282d3565f04d0386e0f31ef51fa4ca46a2dd906c
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/raw/rockchip_nand_spl.c
+++ drivers/mtd/nand/raw/rockchip_nand_spl.c
@@ -351,7 +351,7 @@ static int rockchip_nandc_probe(struct udevice *dev)
 	    id[1] == 0xDA || id[1] == 0xAC ||
 	    id[1] == 0xDC || id[1] == 0xA3 ||
 	    id[1] == 0xD3 || id[1] == 0x95 ||
-	    id[1] == 0x48) {
+	    id[1] == 0x48 || id[1] == 0x63) {
 		nand_page_size = 2048;
 		nand_page_num = 64;
 		nand_block_num = 1024;
@@ -380,8 +380,11 @@ static int rockchip_nandc_probe(struct udevice *dev)
 				nand_page_size = 4096;
 				nand_block_num = 4096;
 			}
+		} else if (id[1] == 0x63 && id[3] == 0x19) { /* IS34ML08G088 */
+			nand_page_size = 4096;
+			nand_page_num = 64;
+			nand_block_num = 4096;
 		}
-
 		g_rk_nand->chipnr = 1;
 		g_rk_nand->databuf = kzalloc(nand_page_size, GFP_KERNEL);
 		if (!g_rk_nand)
